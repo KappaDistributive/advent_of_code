b4e8087d63a0bcc595a40d57ca0713bf02d5c395
Stefan Mesken <kappadistributive@gmail.com>
2020-12-13 20:12:28 +0100

    solve part 1 of 2015_17

---
+++ 2015/day_17.cpp
@@ -0,0 +1,67 @@
+#include <map>
+#include "../utils/input.hpp"
+
+template<class T>
+std::vector<std::vector<T>> subsequences(const std::vector<T>& sequence)
+{
+  std::vector<std::vector<T>> subsequences;
+
+  for (size_t mask{0}; mask < (1 << sequence.size()); mask++)
+  {
+    std::vector<T> subsequence;
+    for (size_t index{0}; index < sequence.size(); index++)
+    {
+      if ((1 << index) & mask)
+      {
+        subsequence.push_back(sequence[index]);
+      }
+    }
+    subsequences.push_back(subsequence);
+  }
+
+  return subsequences;
+}
+int part_one(const std::vector<std::string>& input)
+{
+  int result{0};
+  std::vector<int> containers;
+
+  for (auto line: input)
+  {
+    containers.push_back(std::stoi(line));
+  }
+
+  auto s = subsequences(containers);
+
+  for (auto subsequence: s)
+  {
+    int total_volume{0};
+    for (auto volume: subsequence)
+    {
+      total_volume += volume;
+    }
+    if (total_volume  == 150)
+    {
+      result += 1;
+    }
+  }
+
+  return result;
+}
+
+int part_two(const std::vector<std::string>& input)
+{
+  int result{0};
+  return result;
+}
+
+int main()
+{
+  utils::Reader reader(std::filesystem::path("../2015/data/input_17.txt"));
+  auto input = reader.get_lines();
+  
+  std::cout << "The answer to part one is: " << part_one(input) << std::endl;
+  std::cout << "The answer to part two is: " << part_two(input) << std::endl;
+
+  return 0;
+}
