fbdd56ff9fba76357f2b88326393623dc9daf96b
Stefan Mesken <kappadistributive@gmail.com>
2020-12-22 09:48:38 +0100

    solve part one of 2020_22

---
+++ 2020/day_22.cpp
@@ -0,0 +1,97 @@
+#include <array>
+#include <deque>
+#include <regex>
+
+#include "../utils/input.hpp"
+
+std::array<std::deque<int>, 2> prepare_input (const std::vector<std::string>& input)
+{
+  std::regex re{"^Player\\s(\\d+):$"};
+  std::smatch matches;
+  std::array<std::deque<int>, 2> decks;
+
+  int turn{0};
+
+  for (auto line: input)
+  {
+    if (std::regex_match(line, matches, re))
+    {
+      turn = std::stoi(matches[1].str());
+    }
+    else if (line.size() != 0)
+    {
+      switch (turn)
+      {
+        case 1:
+          decks[0].push_back(std::stoi(line));
+          break;
+        case 2:
+          decks[1].push_back(std::stoi(line));
+          break;
+        default:
+          throw std::invalid_argument("Invalid turn.");
+          break;
+      }
+    }
+  }
+
+  return decks;
+}
+
+int part_one(const std::vector<std::string>& input)
+{
+  auto decks = prepare_input(input);
+  bool player_one{true};
+
+  while (decks[0].size() != 0 && decks[1].size() != 0)
+  {
+    if (decks[0][0] > decks[1][0])
+    {
+      decks[0].push_back(decks[0][0]);
+      decks[0].push_back(decks[1][0]);
+    }
+    else
+    {
+      decks[1].push_back(decks[1][0]);
+      decks[1].push_back(decks[0][0]);
+    }
+    decks[0].pop_front();
+    decks[1].pop_front();
+
+    for (auto deck: decks)
+    {
+      for (auto card: deck)
+      {
+        std::cout << card << std::endl;
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+  std::deque<int>& winner = decks[0].size() > 0 ? decks[0] : decks[1];
+  int score{0};
+
+  for (size_t index{1}; index <= winner.size(); index++)
+  {
+    score += index * winner[winner.size() - index];
+  }
+  
+  return score;
+}
+
+int part_two(const std::vector<std::string>& input)
+{
+  return 3;
+}
+
+int main()
+{
+  utils::Reader reader(std::filesystem::path("../2020/data/input_22.txt"));
+  const std::vector<std::string> input = reader.get_lines();
+  
+  std::cout << "The answer to part one is: " << part_one(input) << std::endl;
+  std::cout << "The answer to part two is: " << part_two(input) << std::endl;
+ 
+  return 0;
+}
