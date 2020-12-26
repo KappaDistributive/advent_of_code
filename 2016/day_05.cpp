80c064ac21b3b0134b9e3fafd13ec06739876160
Stefan Mesken <kappadistributive@gmail.com>
2020-12-26 18:37:16 +0100

    solve 2016_05

--- 2016/day_05.cpp
+++ 2016/day_05.cpp
@@ -4,7 +4,16 @@
 const int PASSWORD_LENGTH = 8;
 const int LEADING_ZEROS = 5;
 
-std::pair<char, int> next_character(const std::string& input, const int offset)
+enum Part
+{
+  p_one,
+  p_two
+};
+
+template<Part p> std::tuple<size_t, char, int> next_character (const std::string& input, const int offet);
+
+template<>
+std::tuple<size_t, char, int> next_character<p_one> (const std::string& input, const int offset)
 {
   int shift{offset};
   std::string candidate;
@@ -31,7 +40,41 @@ std::pair<char, int> next_character(const std::string& input, const int offset)
     }
     shift++;
   }
-  return {candidate[LEADING_ZEROS], shift};
+  return {0, candidate[LEADING_ZEROS], shift};
+}
+
+template<>
+std::tuple<size_t, char, int> next_character<p_two> (const std::string& input, const int offset)
+{
+  int shift{offset};
+  std::string candidate;
+  bool done{false};
+
+  while (!done)
+  {
+    done = true;
+    candidate = md5(input + std::to_string(shift));
+    if ( candidate.size() < LEADING_ZEROS + 2)
+    {
+      done = false;
+    }
+    if (done)
+    {
+      if ( candidate[LEADING_ZEROS] >= '0' && candidate[LEADING_ZEROS] <= '7')
+      {
+        for (size_t index{0}; index < LEADING_ZEROS; index++)
+        {
+          if ( candidate[index] != '0')
+          {
+            done = false;
+            break;
+          }
+        }
+      }
+    }
+    shift++;
+  }
+  return {static_cast<size_t>(candidate[LEADING_ZEROS] - '0'), candidate[LEADING_ZEROS + 1], shift};
 }
 
 std::string part_one(const std::string& input)
@@ -42,7 +85,7 @@ std::string part_one(const std::string& input)
 
   while (result.size() < PASSWORD_LENGTH)
   {
-    auto [character, new_offset] = next_character(input, offset);
+    auto [_, character, new_offset] = next_character<p_one>(input, offset);
     offset = new_offset;
     result += character;
     if (verbose)
@@ -53,9 +96,30 @@ std::string part_one(const std::string& input)
   return result;
 }
 
-int part_two(const std::string& input)
+std::string part_two(const std::string& input)
 {
-  return 9;
+  std::string result;
+  for (size_t index{0}; index < PASSWORD_LENGTH; index++)
+  {
+    result += '_';
+  }
+  int offset{0};
+  bool verbose{true};
+
+  while (std::find(result.begin(), result.end(), '_') != result.end())
+  {
+    auto [position, character, new_offset] = next_character<p_two>(input, offset);
+    offset = new_offset;
+    if (result[position] == '_')
+    {
+      result[position] = character;
+      if (verbose)
+      {
+        std::cout << result << std::endl;
+      }
+    }
+  }
+  return result;
 }
 
 int main()
