b68871ed49f83ffa05451dceb3dc6c9a093d8ede
Stefan Mesken <kappadistributive@gmail.com>
2020-12-26 19:38:09 +0100

    solve 2016_06

--- 2016/day_06.cpp
+++ 2016/day_06.cpp
@@ -2,7 +2,16 @@
 
 #include "../utils/input.hpp"
 
-char mode(const std::vector<std::string>& input, size_t position)
+enum Part
+{
+  p_one,
+  p_two
+};
+
+template<Part p> char mode (const std::vector<std::string>& input, size_t position);
+
+template <>
+char mode<p_one> (const std::vector<std::string>& input, size_t position)
 {
   std::array<size_t, 1 + 'z' - 'a'> frequencies = std::array<size_t, 1 + 'z' - 'a'>{};
   for (auto word: input)
@@ -13,19 +22,43 @@ char mode(const std::vector<std::string>& input, size_t position)
   return 'a' + std::distance(frequencies.begin(), std::max_element(frequencies.begin(), frequencies.end()));
 }
 
+template <>
+char mode<p_two> (const std::vector<std::string>& input, size_t position)
+{
+  std::array<size_t, 1 + 'z' - 'a'> frequencies = std::array<size_t, 1 + 'z' - 'a'>{};
+  for (auto word: input)
+  {
+    assert (word[position] >= 'a' && word[position] <= 'z');
+    frequencies[word[position] - 'a']++;
+  }
+  for (size_t index{0}; index < frequencies.size(); index++)
+  {
+    if (frequencies[index] == 0)
+    {
+      frequencies[index] = input.size() + 1;
+    }
+  }
+  return 'a' + std::distance(frequencies.begin(), std::min_element(frequencies.begin(), frequencies.end()));
+}
+
 std::string part_one(const std::vector<std::string>& input)
 {
   std::string result;
   for (size_t index{0}; index < input[0].size(); index++)
   {
-    result.push_back(mode(input, index));
+    result.push_back(mode<p_one>(input, index));
   }
   return result;
 }
 
 std::string part_two(const std::vector<std::string>& input)
 {
-  return "not part one";
+  std::string result;
+  for (size_t index{0}; index < input[0].size(); index++)
+  {
+    result.push_back(mode<p_two>(input, index));
+  }
+  return result;
 }
 
 int main()
