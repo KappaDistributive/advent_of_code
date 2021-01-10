5a8f10199c2e28f048f3871c5a0a7429445c4070
Stefan Mesken <kappadistributive@gmail.com>
2021-01-10 10:03:35 +0100

    solve 2016_14

--- 2016/day_14.cpp
+++ 2016/day_14.cpp
@@ -74,7 +74,52 @@ size_t part_one(const std::string& input)
 
 int part_two(const std::string& input)
 {
-  return 22233;
+  size_t num_keys{0};
+  size_t offset{0};
+  std::map<std::pair<size_t, std::string>, int> candidates;
+  std::string candidate;
+  std::vector<size_t> keys;
+  bool final_phase{false};
+  size_t final_count{0};
+
+  while (!final_phase || final_count <= 1000)
+  {
+    candidate = input + std::to_string(offset);
+    for (size_t hash_counter{0}; hash_counter < 2017;  hash_counter++)
+    {
+      candidate = md5(candidate);
+    }
+    for (auto [key, value]: candidates)
+    {
+      if (value == -1 && key.first + 1000 >= offset && candidate.find(key.second) != std::string::npos)
+      {
+        candidates.insert_or_assign(key, offset);
+        keys.push_back(key.first);
+        if (num_keys == 63)
+        {
+          final_phase = true;
+        }
+        num_keys++;
+      }
+    }
+    auto triple = first_multiple(candidate, 3);
+    if (triple.has_value())
+    {
+      std::string pentuple;
+      for (size_t count{0}; count < 5; count++)
+      {
+        pentuple += triple.value();
+      }
+      candidates.insert_or_assign({offset, pentuple}, -1);
+    }
+    offset++;
+    if (final_phase)
+    {
+      final_count++;
+    }
+  }
+  std::sort(keys.begin(), keys.end());
+  return keys[63];
 }
 
 int main()
