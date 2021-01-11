93a6fa7c51337c424676a55c5b406046c1396629
Stefan Mesken <kappadistributive@gmail.com>
2021-01-11 21:14:09 +0100

    solve part one of 2017_02

---
+++ 2017/day_02.cpp
@@ -0,0 +1,45 @@
+#include "../utils/input.hpp"
+
+std::vector<std::vector<int>> prepare_input(const std::vector<std::string>& input)
+{
+  std::vector<std::vector<int>> data;
+  for (auto line: input)
+  {
+    std::vector<int> row;
+    for (auto entry: utils::split_string(line, '\t'))
+    {
+      row.push_back(std::stoi(entry));
+    }
+    data.push_back(row);
+  }
+  return data;
+}
+
+long part_one(const std::vector<std::string>& input)
+{
+  auto data = prepare_input(input);
+  long result{0};
+  for (auto row: data)
+  {
+    std::sort(row.begin(), row.end());
+    result += row.back() - row[0];
+  }
+  return result;
+}
+
+int part_two(const std::vector<std::string>& input)
+{
+  return 7;
+}
+
+int main()
+{
+  utils::Reader reader(std::filesystem::path("../2017/data/input_02.txt"));
+  auto input = reader.get_lines();
+  
+  auto answer_one =  part_one(input);
+  std::cout << "The answer to part one is: " << answer_one << std::endl;
+  auto answer_two =  part_two(input);
+  std::cout << "The answer to part two is: " << answer_two << std::endl;
+  return 0;
+}
