006c92767b4e0c58d6bc5734743c04d64d547c49
Stefan Mesken <kappadistributive@gmail.com>
2021-02-01 21:03:35 +0100

    solve 2018_02

--- 2018/day_02.cpp
+++ 2018/day_02.cpp
@@ -23,6 +23,25 @@ bool contains_multiple(const std::string& candidate)
   return false;
 }
 
+bool are_close(const std::string& lhs, const std::string& rhs)
+{
+  if (lhs.size() != rhs.size())
+  {
+    return false;
+  }
+  if (lhs == rhs)
+  {
+    return false;
+  }
+
+  size_t differences{0};
+  for (size_t index{0}; index < lhs.size(); index++)
+  {
+    differences += lhs[index] != rhs[index];
+  }
+
+  return differences == 1;
+}
 
 size_t part_one(const std::vector<std::string>& input)
 {
@@ -36,9 +55,32 @@ size_t part_one(const std::vector<std::string>& input)
   return twin_count * tripled_count;
 }
 
-int part_two(const std::vector<std::string>& input)
+std::string part_two(const std::vector<std::string>& input)
 {
-  return 6666;
+  std::string lhs, rhs;
+  bool searching{true};
+  for (size_t left_index{0}; left_index + 1 < input.size() && searching; left_index++)
+  {
+    for (size_t right_index{left_index+1}; right_index < input.size() && searching; right_index++)
+    {
+      if (are_close(input[left_index], input[right_index]))
+      {
+        lhs = input[left_index];
+        rhs = input[right_index];
+        assert (lhs.size() == rhs.size());
+        searching = false;
+      }
+    }
+  }
+  std::string result;
+  for (size_t index{0}; index < lhs.size(); index++)
+  {
+    if (lhs[index] == rhs[index])
+    {
+      result += lhs[index];
+    }
+  }
+  return result;
 }
 
 int main()
