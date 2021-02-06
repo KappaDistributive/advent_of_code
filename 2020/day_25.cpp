bd7e9e875e189fee92f8e95c9657217a838af56f
Stefan Mesken <kappadistributive@gmail.com>
2021-02-06 01:46:20 +0100

    linting

--- 2020/day_25.cpp
+++ 2020/day_25.cpp
@@ -1,37 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "../utils/input.hpp"
 
-long part_one(const std::vector<std::string>& input)
-{
-  const long subject_number{7};
-  const long card_public_key{std::stoi(input[0])};
-  const long door_public_key{std::stoi(input[1])};
-  const long divider{20201227};
+int64_t part_one(const std::vector<std::string>& input) {
+  const int64_t subject_number{7};
+  const int64_t card_public_key{std::stoi(input[0])};
+  const int64_t door_public_key{std::stoi(input[1])};
+  const int64_t divider{20201227};
 
-  long a{1}, b{1};
-  while (true)
-  {
+  int64_t a{1}, b{1};
+  while (true) {
     a = (a * subject_number) % divider;
     b = (b * card_public_key) % divider;
-    if (a == door_public_key)
-    {
+    if (a == door_public_key) {
       return b;
     }
   }
   return -1;
 }
 
-long part_two(const std::vector<std::string>& input)
-{
+int64_t part_two(const std::vector<std::string>& input) {
   return 17;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2020/data/input_25.txt"));
   auto input = reader.get_lines();
 
   std::cout << "The answer to part one is: " << part_one(input) << std::endl;
   std::cout << "The answer to part two is: " << part_two(input) << std::endl;
- 
+
   return 0;
 }
