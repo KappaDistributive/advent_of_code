acef9bb4fbb667f329c47d89332240700fdc3102
Stefan Mesken <kappadistributive@gmail.com>
2021-02-06 12:11:58 +0100

    solve part one of 2019_02

--- 2019/day_02.cpp
+++ 2019/day_02.cpp
@@ -10,7 +10,39 @@ std::vector<int> prepare_input(const std::vector<std::string>& input) {
 
 int part_one(const std::vector<std::string>& input) {
     auto intcodes = prepare_input(input);
-    return 7;
+    auto max_element = std::max_element(intcodes.begin(), intcodes.end());
+    assert (max_element != intcodes.end());
+    while (intcodes.size() < *max_element + 1)
+    {
+        intcodes.push_back(-1);
+    }
+    intcodes[1] = 12;
+    intcodes[2] = 2;
+    size_t position{0};
+    auto current_intcode = intcodes[0];
+
+    while (current_intcode != 99)
+    {
+        for (auto intcode: intcodes) std::cout << intcode << ", ";
+        std::cout << std::endl;
+        switch (current_intcode)
+        {
+            case 1:
+                intcodes[intcodes[position + 3]] = intcodes[intcodes[position + 1]] + intcodes[intcodes[position + 2]];
+                break;
+            case 2:
+                intcodes[intcodes[position + 3]] = intcodes[intcodes[position + 1]] * intcodes[intcodes[position + 2]];
+                break;
+            default:
+                throw std::runtime_error("Invalid intcode: " + std::to_string(current_intcode));
+        }
+        position += 4;
+        current_intcode = intcodes[position];
+    }
+
+    for (auto intcode: intcodes) std::cout << intcode << ", ";
+    std::cout << std::endl;
+    return intcodes[0];
 }
 
 int part_two(const std::vector<std::string>& input) {
