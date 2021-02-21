fe376c8f21e7c7ec2897f9613c234aa25b91fcba
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 00:29:06 +0100

    linting issues

--- 2016/day_09.cpp
+++ 2016/day_09.cpp
@@ -4,26 +4,21 @@
 
 static const std::regex RE{"\\((\\d+)x(\\d+)\\)"};
 
-size_t decompress(std::string input, bool recursive = false)
-{
+size_t decompress(std::string input, bool recursive = false) {
   std::smatch matches;
   std::regex_search(input, matches, RE);
 
-  if (matches[1].str().size() > 0)
-  {
+  if (matches[1].str().size() > 0) {
     size_t sequence_length = std::stoi(matches[1].str());
     size_t repetitions = std::stoi(matches[2].str());
     size_t size{0};
     std::string chunk = matches.suffix().str().substr(0, sequence_length);
     std::string tail = matches.suffix().str().substr(sequence_length);
     size += matches.prefix().str().size();
-    if (recursive)
-    {
+    if (recursive) {
       size += repetitions * decompress(chunk, recursive);
       return size + decompress(tail, recursive);
-    }
-    else
-    {
+    } else {
       size += repetitions * chunk.size();
       return size += decompress(tail);
     }
@@ -31,34 +26,30 @@ size_t decompress(std::string input, bool recursive = false)
   return input.size();
 }
 
-long part_one(const std::vector<std::string>& input)
-{
-  long result{0};
-  for (auto line: input)
-  {
+int64_t part_one(const std::vector<std::string>& input) {
+  int64_t result{0};
+  for (auto line: input) {
     result += decompress(line);
   }
   return result;
 }
 
-long part_two(const std::vector<std::string>& input)
-{
-  long result{0};
-  for (auto line: input)
-  {
+int64_t part_two(const std::vector<std::string>& input) {
+  int64_t result{0};
+  for (auto line: input) {
     result += decompress(line, true);
   }
   return result;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2016/data/input_09.txt"));
   auto input = reader.get_lines();
-  
+
   auto answer_one =  part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
   auto answer_two =  part_two(input);
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
+
