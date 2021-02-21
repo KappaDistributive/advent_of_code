6a90f94c96cccc8293b4e8735fa288a99e283f30
Stefan Mesken <kappadistributive@gmail.com>
2021-02-21 23:14:34 +0100

    linting issues

--- 2015/day_17.cpp
+++ 2015/day_17.cpp
@@ -2,17 +2,13 @@
 #include "../utils/input.hpp"
 
 template<class T>
-std::vector<std::vector<T>> subsequences(const std::vector<T>& sequence)
-{
+std::vector<std::vector<T>> subsequences(const std::vector<T>& sequence) {
   std::vector<std::vector<T>> subsequences;
 
-  for (size_t mask{0}; mask < (1 << sequence.size()); mask++)
-  {
+  for (size_t mask{0}; mask < (1 << sequence.size()); mask++) {
     std::vector<T> subsequence;
-    for (size_t index{0}; index < sequence.size(); index++)
-    {
-      if ((1 << index) & mask)
-      {
+    for (size_t index{0}; index < sequence.size(); index++) {
+      if ((1 << index) & mask) {
         subsequence.push_back(sequence[index]);
       }
     }
@@ -21,27 +17,22 @@ std::vector<std::vector<T>> subsequences(const std::vector<T>& sequence)
 
   return subsequences;
 }
-int part_one(const std::vector<std::string>& input)
-{
+int part_one(const std::vector<std::string>& input) {
   int result{0};
   std::vector<int> containers;
 
-  for (auto line: input)
-  {
+  for (auto line: input) {
     containers.push_back(std::stoi(line));
   }
 
   auto s = subsequences(containers);
 
-  for (auto subsequence: s)
-  {
+  for (auto subsequence: s) {
     int total_volume{0};
-    for (auto volume: subsequence)
-    {
+    for (auto volume: subsequence) {
       total_volume += volume;
     }
-    if (total_volume  == 150)
-    {
+    if (total_volume  == 150) {
       result += 1;
     }
   }
@@ -49,29 +40,24 @@ int part_one(const std::vector<std::string>& input)
   return result;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
   int result{-1};
   std::vector<int> containers;
 
-  for (auto line: input)
-  {
+  for (auto line: input) {
     containers.push_back(std::stoi(line));
   }
 
   auto s = subsequences(containers);
 
-  for (auto subsequence: s)
-  {
+  for (auto subsequence: s) {
     int total_volume{0};
     int containers_used{0};
-    for (auto volume: subsequence)
-    {
+    for (auto volume: subsequence) {
       containers_used += 1;
       total_volume += volume;
     }
-    if (total_volume  == 150 && (result == -1 || containers_used < result))
-    {
+    if (total_volume  == 150 && (result == -1 || containers_used < result)) {
       result = containers_used;
     }
   }
@@ -79,13 +65,13 @@ int part_two(const std::vector<std::string>& input)
   return result;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2015/data/input_17.txt"));
   auto input = reader.get_lines();
-  
+
   std::cout << "The answer to part one is: " << part_one(input) << std::endl;
   std::cout << "The answer to part two is: " << part_two(input) << std::endl;
 
   return 0;
 }
+
