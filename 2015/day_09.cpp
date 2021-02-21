0d337f8b2d8af13a83b26ff96243134c6a9da47b
Stefan Mesken <kappadistributive@gmail.com>
2021-02-21 22:56:34 +0100

    linting issues

--- 2015/day_09.cpp
+++ 2015/day_09.cpp
@@ -6,51 +6,43 @@
 
 #include "../utils/input.hpp"
 
-class Map
-{
-private:
+class Map {
+ private:
   std::regex re;
   std::vector<std::string> cities;
   std::vector<int> adjacency_matrix;
 
-public:
+ public:
   std::smatch matches;
-  Map(const std::vector<std::string>& input) : re("^(\\w+)\\sto\\s(\\w+)\\s=\\s(\\d+)$")
-  {
+  explicit Map(const std::vector<std::string>& input)
+      : re("^(\\w+)\\sto\\s(\\w+)\\s=\\s(\\d+)$") {
     // temporary distance storage
     std::map<std::pair<std::string, std::string>, int> distances;
-    for (auto line: input)
-    {
+    for (auto line: input) {
       std::regex_match(line,  matches, re);
       assert(matches.size() == 4);
       std::string start{matches[1].str()};
       std::string destination{matches[2].str()};
       int distance{std::stoi(matches[3].str())};
-      
-      if (std::find(cities.begin(), cities.end(), start) == cities.end())
-      {
+
+      if (std::find(cities.begin(), cities.end(), start) == cities.end()) {
         cities.push_back(start);
       }
-      if (std::find(cities.begin(), cities.end(), destination) == cities.end())
-      {
+      if (std::find(cities.begin(), cities.end(), destination) == cities.end()) {
         cities.push_back(destination);
       }
       distances.insert(std::make_pair(std::make_pair(start, destination), distance));
     }
 
     adjacency_matrix.reserve(cities.size() * cities.size());
-    for (int j{0}; j < cities.size(); j++)
-    {
-      for (int i{0}; i < cities.size(); i++)
-      {
-        try
-        {
+    for (int j{0}; j < cities.size(); j++) {
+      for (int i{0}; i < cities.size(); i++) {
+        try {
           auto distance = distances.at(std::make_pair(cities[i], cities[j]));
           adjacency_matrix[j * cities.size() + i] = distance;
           adjacency_matrix[i * cities.size() + j] = distance;
         }
-        catch (const std::out_of_range& e)
-        {
+        catch (const std::out_of_range& e) {
           adjacency_matrix[j * cities.size() + i] = -1;
         }
       }
@@ -61,22 +53,18 @@ public:
     return cities;
   }
 
-  int get_distance(std::string start, std::string destination) const
-  {
-    try
-    {
+  int get_distance(std::string start, std::string destination) const {
+    try {
       int i = std::find(cities.begin(), cities.end(), start) - cities.begin();
       int j = std::find(cities.begin(), cities.end(), destination) - cities.begin();
       return adjacency_matrix[j * cities.size() + i];
     }
-    catch (const std::out_of_range& e)
-    {
+    catch (const std::out_of_range& e) {
       return -1;
     }
   }
 
-  std::vector<std::string> hamiltonian_path(bool shortest = true)
-  {
+  std::vector<std::string> hamiltonian_path(bool shortest = true) {
     std::vector<int> permutation;
     int distance{-1};
     int candidate;
@@ -84,58 +72,46 @@ public:
     bool valid;
 
     std::vector<int> indices;
-    for (int index{0}; index < cities.size(); index++)
-    {
+    for (int index{0}; index < cities.size(); index++) {
       indices.push_back(index);
     }
 
     do {
       candidate = 0;
       valid = true;
-      for (size_t index{1}; index < indices.size(); index++)
-      {
+      for (size_t index{1}; index < indices.size(); index++) {
         next_distance = get_distance(cities[indices[index-1]], cities[indices[index]]);
-        if (next_distance < 0)
-        {
+        if (next_distance < 0) {
           valid = false;
           break;
         }
         candidate += next_distance;
       }
-      if (valid && (distance < 0 || (shortest && candidate < distance) || (!shortest && candidate > distance)))
-      {
+      if (valid && (distance < 0 || (shortest && candidate < distance) || (!shortest && candidate > distance))) {
         distance = candidate;
         permutation = indices;
       }
     } while (std::next_permutation(indices.begin(), indices.end()));
 
-    if (distance >= 0)
-    {
+    if (distance >= 0) {
       std::vector<std::string> path;
-      for (auto index: permutation)
-      {
+      for (auto index: permutation) {
         path.push_back(cities[index]);
       }
       return path;
-    }
-    else
-    {
+    } else {
       return std::vector<std::string>{};
     }
   }
 };
 
-std::ostream& operator<< (std::ostream& os, const Map& map)
-{
+std::ostream& operator<< (std::ostream& os, const Map& map) {
   auto cities = map.get_cities();
-  for (auto start: cities)
-  {
-    for (auto destination: cities)
-    {
+  for (auto start: cities) {
+    for (auto destination: cities) {
       auto distance = map.get_distance(start, destination);
-      if (distance >= 0)
-      {
-        std::cout << start << " -- " << distance << " --> " << destination << std::endl; 
+      if (distance >= 0) {
+        std::cout << start << " -- " << distance << " --> " << destination << std::endl;
       }
     }
   }
@@ -143,33 +119,28 @@ std::ostream& operator<< (std::ostream& os, const Map& map)
 }
 
 
-int part_one(const std::vector<std::string>& input)
-{
+int part_one(const std::vector<std::string>& input) {
   Map map(input);
   int result{0};
   auto path = map.hamiltonian_path();
-  for(size_t index{1}; index < path.size(); index++)
-  {
+  for (size_t index{1}; index < path.size(); index++) {
     result += map.get_distance(path[index-1], path[index]);
   }
   return result;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
 
   Map map(input);
   int result{0};
   auto path = map.hamiltonian_path(false);
-  for(size_t index{1}; index < path.size(); index++)
-  {
+  for (size_t index{1}; index < path.size(); index++) {
     result += map.get_distance(path[index-1], path[index]);
   }
   return result;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2015/data/input_09.txt"));
   auto input = reader.get_lines();
 
@@ -178,3 +149,4 @@ int main()
 
   return 0;
 }
+
