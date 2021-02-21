f04e5d987ae972a5fd0a92e4ae60df4d0bd0204c
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 00:19:21 +0100

    linting issues

--- 2016/day_04.cpp
+++ 2016/day_04.cpp
@@ -4,66 +4,52 @@
 
 #include "../utils/input.hpp"
 
-class Room
-{
-private:
+class Room {
+ private:
   const std::string code;
   std::regex re_sector_id{"^\\D*(\\d+)\\D*$"};
   std::regex re_checksum{"^.*\\[(\\w+)\\]$"};
 
-public:
-  explicit Room (const std::string& code)
-    : code(code)
-  {
+ public:
+  explicit Room(const std::string& code)
+    : code(code) {
   }
 
-  int get_sector_id() const
-  {
+  int get_sector_id() const {
     std::smatch matches;
     std::regex_match(code, matches, re_sector_id);
-    assert (matches.size() == 2);
+    assert(matches.size() == 2);
 
     return std::stoi(matches[1].str());
   }
 
-  std::string get_name() const
-  {
+  std::string get_name() const {
     std::string name;
 
-    for (auto character: code)
-    {
-      if (character == '[' || isdigit(character))
-      {
+    for (auto character: code) {
+      if (character == '[' || isdigit(character)) {
         break;
-      }
-      else if (character != '-')
-      {
+      } else if (character != '-') {
         name += character;
       }
     }
     return name;
   }
 
-  std::string calculate_checksum() const
-  {
+  std::string calculate_checksum() const {
     auto name = get_name();
     std::map<char, int> frequency;
 
-    for (auto character: name)
-    {
-      if (frequency.count(character) > 0)
-      {
+    for (auto character: name) {
+      if (frequency.count(character) > 0) {
         frequency.at(character)++;
-      }
-      else
-      {
+      } else {
         frequency.insert(std::make_pair(character, 1));
       }
     }
     std::vector<std::pair<char, int>> sorted_frequency;
 
-    for (auto [character, count]: frequency)
-    {
+    for (auto [character, count]: frequency) {
       sorted_frequency.push_back(std::make_pair(character, count));
     }
     std::sort(sorted_frequency.begin(), sorted_frequency.end(), [] (const std::pair<char, int>& lhs, const std::pair<char, int>& rhs)
@@ -73,78 +59,66 @@ public:
     );
 
     std::string checksum;
-    for (auto [character, _]: sorted_frequency)
-    {
+    for (auto [character, _]: sorted_frequency) {
       checksum += character;
     }
     return checksum.substr(0, 5);
   }
 
-  std::string get_checksum() const
-  {
+  std::string get_checksum() const {
     std::smatch matches;
     std::regex_match(code, matches, re_checksum);
-    assert (matches.size() == 2);
+    assert(matches.size() == 2);
     return matches[1].str();
   }
 
-  bool is_valid() const
-  {
+  bool is_valid() const {
     return calculate_checksum() == get_checksum();
   }
 };
 
-std::string rot(const std::string& input, int rotation)
-{
-  assert (rotation >= 0);
+std::string rot(const std::string& input, int rotation) {
+  assert(rotation >= 0);
   std::string result;
-  for (auto character: input)
-  {
-    assert (character >= 'a' && character <= 'z');
+  for (auto character: input) {
+    assert(character >= 'a' && character <= 'z');
     result += 'a' + ((character - 'a' + rotation) % ('z' + 1 - 'a'));
   }
   return result;
 }
 
-int part_one(const std::vector<std::string>& input)
-{
+int part_one(const std::vector<std::string>& input) {
   int result{0};
-  for (auto code: input)
-  {
+  for (auto code: input) {
     Room room(code);
-    if (room.is_valid())
-    {
+    if (room.is_valid()) {
       result += room.get_sector_id();
     }
   }
   return result;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
   int result{-1};
   bool verbose{false};
-  for (auto code: input)
-  {
+  for (auto code: input) {
     Room room(code);
-    if (verbose)
-    {
+    if (verbose) {
       std::cout << room.get_sector_id() << ": " << rot(room.get_name(), room.get_sector_id()) << std::endl;
     }
-    if (rot(room.get_name(), room.get_sector_id()) == "northpoleobjectstorage")
-    {
+    if (rot(room.get_name(), room.get_sector_id()) == "northpoleobjectstorage") {
       result = room.get_sector_id();
     }
   }
-  return result; 
+  return result;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2016/data/input_04.txt"));
   auto input = reader.get_lines();
-  
+
   std::cout << "The answer to part one is: " << part_one(input) << std::endl;
   std::cout << "The answer to part two is: " << part_two(input) << std::endl;
   return 0;
 }
+
