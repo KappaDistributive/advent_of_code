0df5ceabedd2d376697b892acea7b810a23a3aa1
Stefan Mesken <kappadistributive@gmail.com>
2021-02-21 23:09:43 +0100

    linting issues

--- 2015/day_15.cpp
+++ 2015/day_15.cpp
@@ -6,32 +6,29 @@
 
 using ingredient = std::array<int, 5>;
 
-std::ostream& operator<< (std::ostream& os, ingredient ingredient)
-{
-  os << "capacity: " << ingredient[0] 
-     << " durability: " << ingredient[1] 
+std::ostream& operator<< (std::ostream& os, ingredient ingredient) {
+  os << "capacity: " << ingredient[0]
+     << " durability: " << ingredient[1]
      << " flavor: " << ingredient[2]
      << " texture: " << ingredient[3]
      << " calories: " << ingredient[4];
   return os;
 }
 
-std::vector<ingredient> get_ingredients(const std::vector<std::string>& input)
-{
+std::vector<ingredient> get_ingredients(const std::vector<std::string>& input) {
   std::regex re{"^.*\\s(-?\\d+).*\\s(-?\\d+).*\\s(-?\\d+).*\\s(-?\\d+).*\\s(-?\\d+).*$"};
   std::smatch matches;
   std::vector<std::array<int, 5>> ingredients;
 
-  for (auto line: input)
-  {
+  for (auto line: input) {
     std::regex_match(line, matches, re);
     assert(matches.size() == 6);
     ingredients.push_back(
         {
-          std::stoi(matches[1].str()), 
-          std::stoi(matches[2].str()), 
-          std::stoi(matches[3].str()), 
-          std::stoi(matches[4].str()), 
+          std::stoi(matches[1].str()),
+          std::stoi(matches[2].str()),
+          std::stoi(matches[3].str()),
+          std::stoi(matches[4].str()),
           std::stoi(matches[5].str())
        }
     );
@@ -40,19 +37,15 @@ std::vector<ingredient> get_ingredients(const std::vector<std::string>& input)
   return ingredients;
 }
 
-int part_one(const std::vector<std::string>& input)
-{
+int part_one(const std::vector<std::string>& input) {
   int result{0}, first{0}, capacity, durability, flavor, texture;
 
   auto ingredients = get_ingredients(input);
-  assert (ingredients.size() == 4);
+  assert(ingredients.size() == 4);
 
-  for (int fourth{0}; fourth <= 100; fourth++)
-  {
-    for (int third{0}; third + fourth <= 100; third++)
-    {
-      for (int second{0}; second + third + fourth <= 100; second++)
-      {
+  for (int fourth{0}; fourth <= 100; fourth++) {
+    for (int third{0}; third + fourth <= 100; third++) {
+      for (int second{0}; second + third + fourth <= 100; second++) {
         first = 100 - second - third - fourth;
         capacity = first * ingredients[0][0] + second * ingredients[1][0] + third * ingredients[2][0] + fourth * ingredients[3][0];
         capacity *= (capacity > 0);
@@ -63,8 +56,7 @@ int part_one(const std::vector<std::string>& input)
         texture = first * ingredients[0][3] + second * ingredients[1][3] + third * ingredients[2][3] + fourth * ingredients[3][3];
         texture *= (texture > 0);
 
-        if (capacity * durability * flavor * texture > result)
-        {
+        if (capacity * durability * flavor * texture > result) {
           result = capacity * durability * flavor * texture;
         }
       }
@@ -73,24 +65,19 @@ int part_one(const std::vector<std::string>& input)
   return result;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
   int result{0}, first{0}, capacity, durability, flavor, texture, calories;
 
   auto ingredients = get_ingredients(input);
-  assert (ingredients.size() == 4);
+  assert(ingredients.size() == 4);
 
-  for (int fourth{0}; fourth <= 100; fourth++)
-  {
-    for (int third{0}; third + fourth <= 100; third++)
-    {
-      for (int second{0}; second + third + fourth <= 100; second++)
-      {
+  for (int fourth{0}; fourth <= 100; fourth++) {
+    for (int third{0}; third + fourth <= 100; third++) {
+      for (int second{0}; second + third + fourth <= 100; second++) {
         first = 100 - second - third - fourth;
 
         calories = first * ingredients[0][4] + second * ingredients[1][4] + third * ingredients[2][4] + fourth * ingredients[3][4];
-        if (calories != 500)
-        {
+        if (calories != 500) {
           continue;
         }
         capacity = first * ingredients[0][0] + second * ingredients[1][0] + third * ingredients[2][0] + fourth * ingredients[3][0];
@@ -102,8 +89,7 @@ int part_two(const std::vector<std::string>& input)
         texture = first * ingredients[0][3] + second * ingredients[1][3] + third * ingredients[2][3] + fourth * ingredients[3][3];
         texture *= (texture > 0);
 
-        if (capacity * durability * flavor * texture > result)
-        {
+        if (capacity * durability * flavor * texture > result) {
           result = capacity * durability * flavor * texture;
         }
       }
@@ -112,13 +98,13 @@ int part_two(const std::vector<std::string>& input)
   return result;
   }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2015/data/input_15.txt"));
   auto input = reader.get_lines();
-  
+
   std::cout << "The answer to part one is: " << part_one(input) << std::endl;
   std::cout << "The answer to part two is: " << part_two(input) << std::endl;
 
   return 0;
 }
+
