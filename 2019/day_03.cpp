906f11215c985c0062fd11ca745570db111d9734
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 04:42:11 +0100

    linting issues

--- 2019/day_03.cpp
+++ 2019/day_03.cpp
@@ -6,8 +6,7 @@
 
 #include "../utils/input.hpp"
 
-std::vector<std::pair<char, int>> parse_line(const std::string& line)
-{
+std::vector<std::pair<char, int>> parse_line(const std::string& line) {
     auto splits = utils::split_string(line, ',');
     std::vector<std::pair<char, int>> result;
     std::transform(splits.begin(), splits.end(), std::back_insert_iterator(result),
@@ -16,34 +15,27 @@ std::vector<std::pair<char, int>> parse_line(const std::string& line)
     return result;
 }
 
-void walk_the_line(const std::vector<std::pair<char, int>>& line, std::map<std::pair<int, int>, std::tuple<bool, bool, int, int>>& visits, bool first = true)
-{
+void walk_the_line(const std::vector<std::pair<char, int>>& line, std::map<std::pair<int, int>, std::tuple<bool, bool, int, int>>& visits, bool first = true) {
     std::pair<int, int> position{0, 0}, step{0, 0};
     int steps{0};
-    
-    for (auto [direction, distance]: line)
-    {
-        switch (direction)
-        {
+
+    for (auto [direction, distance]: line) {
+        switch (direction) {
             case 'U': step = {0, 1}; break;
             case 'D': step = {0, -1}; break;
             case 'L': step = {-1, 0}; break;
             case 'R': step = {1, 0}; break;
-            default: throw std::runtime_error("Invalid direction: " + direction); break;
+            default: throw std::runtime_error("Invalid direction: " + std::to_string(direction)); break;
         }
-        for (int index{0}; index < distance; index++)
-        {
+        for (int index{0}; index < distance; index++) {
             steps++;
             position.first += step.first;
             position.second += step.second;
             visits.emplace(position, std::tuple(false, false, -1, -1));
-            if (first && !std::get<0>(visits.at(position)))
-            {
+            if (first && !std::get<0>(visits.at(position))) {
                 std::get<0>(visits.at(position)) = true;
                 std::get<2>(visits.at(position)) = steps;
-            }
-            else if (!first && !std::get<1>(visits.at(position)))
-            {
+            } else if (!first && !std::get<1>(visits.at(position))) {
                 std::get<1>(visits.at(position)) = true;
                 std::get<3>(visits.at(position)) = steps;
             }
@@ -59,16 +51,14 @@ int part_one(const std::vector<std::string>& input) {
     auto first = lines[0];
     auto second = lines[1];
     std::map<std::pair<int, int>, std::tuple<bool, bool, int, int>> visits;
-    visits.insert(std::make_pair(std::make_pair(0,0), std::make_tuple(true, true, -1, -1)));
+    visits.insert(std::make_pair(std::make_pair(0, 0), std::make_tuple(true, true, -1, -1)));
 
     walk_the_line(first, visits, true);
     walk_the_line(second, visits, false);
 
     int result{std::numeric_limits<int>::max()};
-    for (auto [key, value]: visits)
-    {
-        if (std::get<0>(value) && std::get<1>(value) && key != std::make_pair(0, 0))
-        {
+    for (auto [key, value]: visits) {
+        if (std::get<0>(value) && std::get<1>(value) && key != std::make_pair(0, 0)) {
             result = std::min(result, abs(key.first) + abs(key.second));
         }
     }
@@ -83,17 +73,15 @@ int part_two(const std::vector<std::string>& input) {
     auto first = lines[0];
     auto second = lines[1];
     std::map<std::pair<int, int>, std::tuple<bool, bool, int, int>> visits;
-    visits.insert(std::make_pair(std::make_pair(0,0), std::make_tuple(true, true, 0, 0)));
+    visits.insert(std::make_pair(std::make_pair(0, 0), std::make_tuple(true, true, 0, 0)));
 
     walk_the_line(first, visits, true);
     walk_the_line(second, visits, false);
 
     int result{std::numeric_limits<int>::max()};
-    for (auto [key, value]: visits)
-    {
-        if (std::get<0>(value) && std::get<1>(value) && key != std::make_pair(0, 0))
-        {
-            assert (std::get<2>(value) > 0 && std::get<3>(value) > 0);
+    for (auto [key, value]: visits) {
+        if (std::get<0>(value) && std::get<1>(value) && key != std::make_pair(0, 0)) {
+            assert(std::get<2>(value) > 0 && std::get<3>(value) > 0);
             result = std::min(result, std::get<2>(value) + std::get<3>(value));
         }
     }
@@ -110,3 +98,4 @@ int main() {
     std::cout << "The answer to part two is: " << answer_two << std::endl;
     return 0;
 }
+ 
