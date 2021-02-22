4a347fa4d4ed98acdf195505a1a1e9e77e462b6b
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 04:43:06 +0100

    linting issues

--- 2019/day_04.cpp
+++ 2019/day_04.cpp
@@ -2,26 +2,19 @@
 
 #include "../utils/input.hpp"
 
-bool has_pair(const int& candidate, bool strict = false)
-{
+bool has_pair(const int& candidate, bool strict = false) {
     auto code = std::to_string(candidate);
 
-    for (size_t index{0}; index + 1 < code.size(); index++)
-    {
-        if (code[index] == code[index+1])
-        {
-            if (strict)
-            {
+    for (size_t index{0}; index + 1 < code.size(); index++) {
+        if (code[index] == code[index+1]) {
+            if (strict) {
                 if (
                     (index == 0 || code[index-1] != code[index])  && // check lower part
                     (index + 2 >= code.size() || code[index+2] != code[index]) // check upper part
-                   )
-                {
+                   ) {
                     return true;
                 }
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
@@ -30,14 +23,11 @@ bool has_pair(const int& candidate, bool strict = false)
     return false;
 }
 
-bool never_decreasing(const int& candidate)
-{
+bool never_decreasing(const int& candidate) {
     auto code = std::to_string(candidate);
 
-    for (size_t index{0}; index + 1 < code.size(); index++)
-    {
-        if (code[index+1] < code[index])
-        {
+    for (size_t index{0}; index + 1 < code.size(); index++) {
+        if (code[index+1] < code[index]) {
             return false;
         }
     }
@@ -48,8 +38,7 @@ bool never_decreasing(const int& candidate)
 size_t part_one(const int& min, const int& max) {
     size_t valid_passwords{0};
 
-    for (int candidate{min}; candidate <= max; candidate++)
-    {
+    for (int candidate{min}; candidate <= max; candidate++) {
         valid_passwords += has_pair(candidate) && never_decreasing(candidate);
     }
 
@@ -59,8 +48,7 @@ size_t part_one(const int& min, const int& max) {
 int part_two(const int& min, const int& max) {
     size_t valid_passwords{0};
 
-    for (int candidate{min}; candidate <= max; candidate++)
-    {
+    for (int candidate{min}; candidate <= max; candidate++) {
         valid_passwords += has_pair(candidate, true) && never_decreasing(candidate);
     }
 
@@ -70,7 +58,7 @@ int part_two(const int& min, const int& max) {
 int main() {
     utils::Reader reader(std::filesystem::path("../2019/data/input_04.txt"));
     auto input = utils::split_string(reader.get_lines()[0], '-');
-    assert (input.size() == 2);
+    assert(input.size() == 2);
 
     auto answer_one =  part_one(std::stoi(input[0]), std::stoi(input[1]));
     std::cout << "The answer to part one is: " << answer_one << std::endl;
@@ -78,3 +66,4 @@ int main() {
     std::cout << "The answer to part two is: " << answer_two << std::endl;
     return 0;
 }
+
