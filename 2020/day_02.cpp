e694912509fd9317117421b57b7c42316eb818ea
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 05:04:59 +0100

    linting issues

--- 2020/day_02.cpp
+++ 2020/day_02.cpp
@@ -1,17 +1,15 @@
 #include <cassert>
 #include <regex>
-  
+
 #include "../utils/input.hpp"
 
-class Rule
-{
-private:
+class Rule {
+ private:
   std::pair<int, int> range;
   char letter;
 
-public:
-  Rule(std::string line)
-  {
+ public:
+  explicit Rule(std::string line) {
     std::smatch matches;
     std::regex_match(line, matches, std::regex("^(\\d+)-(\\d+)\\s(\\w)\\:\\s(\\w+)$"));
     assert(matches.size() == 5);
@@ -20,54 +18,45 @@ public:
     letter = matches[3].str()[0];
   }
 
-  bool accepts(std::string line)
-  {
+  bool accepts(std::string line) {
     int count{0};
 
-    for (auto character: line)
-    {
+    for (auto character: line) {
       count += character == letter;
     }
 
     return std::get<0>(range) <= count && count <= std::get<1>(range);
   }
 
-  bool new_accepts(std::string line)
-  {
+  bool new_accepts(std::string line) {
     return (line[std::get<0>(range)-1] == letter) ^ (line[std::get<1>(range)-1] == letter);
   }
 
-  std::pair<int, int> get_range() const
-  {
+  std::pair<int, int> get_range() const {
     return range;
   }
 
-  char get_letter() const
-  {
+  char get_letter() const {
     return letter;
   }
 };
 
-std::vector<Rule> get_rules(std::vector<std::string> input)
-{
+std::vector<Rule> get_rules(std::vector<std::string> input) {
   std::vector<Rule> rules;
 
-  for (auto line: input)
-  {
+  for (auto line: input) {
     rules.push_back(Rule(line));
   }
 
   return rules;
 }
 
-std::vector<std::string> get_passwords(std::vector<std::string> input)
-{
+std::vector<std::string> get_passwords(std::vector<std::string> input) {
   std::regex re("^(\\d+)-(\\d+)\\s(\\w)\\:\\s(\\w+)$");
   std::smatch matches;
-  std::vector<std::string> passwords ;
+  std::vector<std::string> passwords;
 
-  for (auto line: input)
-  {
+  for (auto line: input) {
     std::regex_match(line, matches, re);
     assert(matches.size() == 5);
     passwords.push_back(matches[4]);
@@ -76,15 +65,13 @@ std::vector<std::string> get_passwords(std::vector<std::string> input)
   return passwords;
 }
 
-std::ostream& operator<< (std::ostream& os, const Rule& rule)
-{
+std::ostream& operator<< (std::ostream& os, const Rule& rule) {
   os << std::get<0>(rule.get_range()) << "-" << std::get<1>(rule.get_range()) << " " << rule.get_letter();
 
   return os;
 }
 
-int part_one(std::vector<std::string> input)
-{
+int part_one(std::vector<std::string> input) {
   auto rules = get_rules(input);
   auto passwords = get_passwords(input);
   bool valid_password;
@@ -92,15 +79,13 @@ int part_one(std::vector<std::string> input)
 
   assert(rules.size() == passwords.size());
 
-  for (size_t index{0}; index < rules.size(); index++)
-  {
+  for (size_t index{0}; index < rules.size(); index++) {
     result += rules[index].accepts(passwords[index]);
   }
   return result;
 }
 
-int part_two(std::vector<std::string> input)
-{
+int part_two(std::vector<std::string> input) {
   auto rules = get_rules(input);
   auto passwords = get_passwords(input);
   bool valid_password;
@@ -108,21 +93,20 @@ int part_two(std::vector<std::string> input)
 
   assert(rules.size() == passwords.size());
 
-  for (size_t index{0}; index < rules.size(); index++)
-  {
+  for (size_t index{0}; index < rules.size(); index++) {
     result += rules[index].new_accepts(passwords[index]);
   }
   return result;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2020/data/input_02.txt"));
   std::vector<std::string> input = reader.get_lines();
-  
+
   std::cout << "The answer to part one is: " << part_one(input) << std::endl;
-  
+
   std::cout << "The answer to part two is: " << part_two(input) << std::endl;
- 
+
   return 0;
 }
+
