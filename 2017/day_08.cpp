e1792b3a1c187f7760b700d8596ed6cae61f1c50
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 04:35:26 +0100

    linting issues

--- 2017/day_08.cpp
+++ 2017/day_08.cpp
@@ -4,8 +4,7 @@
 
 #include "../utils/input.hpp"
 
-enum Comparator
-{
+enum Comparator {
   less,
   less_or_equal,
   equal,
@@ -14,42 +13,26 @@ enum Comparator
   greater
 };
 
-Comparator string_to_comparator(const std::string& input)
-{
-  if (input == "<")
-  {
+Comparator string_to_comparator(const std::string& input) {
+  if (input == "<") {
     return less;
-  }
-  else if (input == "<=")
-  {
+  } else if (input == "<=") {
     return less_or_equal;
-  }
-  else if (input == "==")
-  {
+  } else if (input == "==") {
     return equal;
-  }
-  else if (input == "!=")
-  {
+  } else if (input == "!=") {
     return unequal;
-  }
-  else if (input == ">=")
-  {
+  } else if (input == ">=") {
     return greater_or_equal;
-  }
-  else if (input == ">")
-  {
+  } else if (input == ">") {
     return greater;
-  }
-  else
-  {
+  } else {
     throw std::invalid_argument("Invalid comparator: " + input);
   }
 }
 
-std::string comparator_to_string(const Comparator& comparator)
-{
-  switch (comparator)
-  {
+std::string comparator_to_string(const Comparator& comparator) {
+  switch (comparator) {
     case less: return "<"; break;
     case less_or_equal: return "<="; break;
     case equal: return "=="; break;
@@ -60,42 +43,31 @@ std::string comparator_to_string(const Comparator& comparator)
   return "error";
 }
 
-enum Operator
-{
+enum Operator {
   increment,
   decrement
 };
 
-Operator string_to_operator(const std::string& input)
-{
-  if (input == "inc")
-  {
+Operator string_to_operator(const std::string& input) {
+  if (input == "inc") {
     return increment;
-  }
-  else if (input == "dec")
-  {
+  } else if (input == "dec") {
     return decrement;
-  }
-  else
-  {
+  } else {
     throw std::invalid_argument("Invalid operator: " + input);
   }
 }
 
-std::string operator_to_string(const Operator& op)
-{
-  switch (op)
-  {
+std::string operator_to_string(const Operator& op) {
+  switch (op) {
     case increment: return "inc"; break;
     case decrement: return "dec"; break;
   }
   return "error";
 }
 
-bool compare(const int& lhs, const Comparator& comparator, const int& rhs)
-{
-  switch (comparator)
-  {
+bool compare(const int& lhs, const Comparator& comparator, const int& rhs) {
+  switch (comparator) {
     case less:
       return lhs < rhs;
       break;
@@ -120,25 +92,22 @@ bool compare(const int& lhs, const Comparator& comparator, const int& rhs)
 
 typedef std::tuple<std::string, Operator, int, std::string, Comparator, int> Instruction;
 
-std::ostream& operator<< (std::ostream& os, const Instruction& instruction)
-{
+std::ostream& operator<< (std::ostream& os, const Instruction& instruction) {
   auto [target, op, data, lhs, cmp, rhs] = instruction;
   os << target << " " << operator_to_string(op) << " " << data << " if ";
   os << lhs << " " << comparator_to_string(cmp) << " " << rhs;
   return os;
 }
 
-std::vector<Instruction> prepare_input(const std::vector<std::string>& input)
-{
+std::vector<Instruction> prepare_input(const std::vector<std::string>& input) {
   std::vector<Instruction> instructions;
   std::regex re{"^(\\w+)\\s(inc|dec)\\s(-?\\d+)\\sif\\s(\\w+)\\s(<|<=|==|!=|>=|>)\\s(-?\\d+)$"};
   std::smatch matches;
-  for (auto line: input)
-  {
+  for (auto line: input) {
     std::regex_match(line, matches, re);
     instructions.push_back({
         matches[1].str(),
-        string_to_operator(matches[2].str()), 
+        string_to_operator(matches[2].str()),
         std::stoi(matches[3].str()),
         matches[4].str(),
         string_to_comparator(matches[5].str()),
@@ -148,28 +117,23 @@ std::vector<Instruction> prepare_input(const std::vector<std::string>& input)
   return instructions;
 }
 
-class CPU
-{
-private:
+class CPU {
+ private:
   const std::vector<Instruction> instructions;
   std::map<std::string, int> registers;
   size_t instruction_pointer;
 
-public:
-  explicit CPU (const std::vector<Instruction>& instructions)
-    : instructions(instructions), instruction_pointer(0)
-  {
+ public:
+  explicit CPU(const std::vector<Instruction>& instructions)
+    : instructions(instructions), instruction_pointer(0) {
   }
 
-  bool step ()
-  {
+  bool step() {
     auto [target, op, data, lhs, cmp, rhs] = instructions[instruction_pointer];
     registers.emplace(lhs, 0);
-    if (compare(registers.at(lhs), cmp, rhs))
-    {
+    if (compare(registers.at(lhs), cmp, rhs)) {
       registers.emplace(target, 0);
-      switch (op)
-      {
+      switch (op) {
         case increment: registers.at(target) += data; break;
         case decrement: registers.at(target) -= data; break;
       }
@@ -177,41 +141,34 @@ public:
     return ++instruction_pointer < instructions.size();
   }
 
-  std::map<std::string, int> get_registers () const
-  {
+  std::map<std::string, int> get_registers() const {
     return registers;
   }
 };
 
-int part_one(const std::vector<std::string>& input)
-{
+int part_one(const std::vector<std::string>& input) {
   auto instructions = prepare_input(input);
   CPU cpu(instructions);
   do {} while (cpu.step());
   auto registers = cpu.get_registers();
 
   int result{std::numeric_limits<int>::min()};
-  for (auto [_, value]: registers)
-  {
-    if (value > result)
-    {
+  for (auto [_, value]: registers) {
+    if (value > result) {
       result = value;
     }
   }
   return result;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
   int result{std::numeric_limits<int>::min()};
   auto instructions = prepare_input(input);
   CPU cpu(instructions);
   do {
     auto registers = cpu.get_registers();
-    for (auto [_, value]: registers)
-    {
-      if (value > result)
-      {
+    for (auto [_, value]: registers) {
+      if (value > result) {
         result = value;
       }
     }
@@ -220,14 +177,14 @@ int part_two(const std::vector<std::string>& input)
   return result;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2017/data/input_08.txt"));
   auto input = reader.get_lines();
-  
+
   auto answer_one =  part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
   auto answer_two =  part_two(input);
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
+
