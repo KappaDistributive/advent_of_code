beba87e84b486b0e167932218f00c1bc2f4237df
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 05:03:13 +0100

    linting issues

--- 2020/day_01.cpp
+++ 2020/day_01.cpp
@@ -3,8 +3,8 @@
 #include "../utils/input.hpp"
 
 std::pair<bool, int> part_one(std::vector<std::string> input) {
-  for(size_t index_left{0}; index_left + 1 < input.size(); index_left++) {
-    for(size_t index_right{index_left+1}; index_right < input.size(); index_right++) {
+  for (size_t index_left{0}; index_left + 1 < input.size(); index_left++) {
+    for (size_t index_right{index_left+1}; index_right < input.size(); index_right++) {
       if (std::stoi(input[index_left]) + std::stoi(input[index_right]) == 2020) {
         return std::pair<bool, int>{true, std::stoi(input[index_left]) * std::stoi(input[index_right])};
       }
@@ -14,9 +14,9 @@ std::pair<bool, int> part_one(std::vector<std::string> input) {
 }
 
 std::pair<bool, int> part_two(std::vector<std::string> input) {
-  for(size_t index_left{0}; index_left + 2 < input.size(); index_left++) {
-    for(size_t index_middle{index_left+1}; index_middle + 1 < input.size(); index_middle++) {
-      for(size_t index_right{index_middle+1}; index_right < input.size(); index_right++) {
+  for (size_t index_left{0}; index_left + 2 < input.size(); index_left++) {
+    for (size_t index_middle{index_left+1}; index_middle + 1 < input.size(); index_middle++) {
+      for (size_t index_right{index_middle+1}; index_right < input.size(); index_right++) {
         if (std::stoi(input[index_left]) + std::stoi(input[index_middle]) + std::stoi(input[index_right]) == 2020) {
           return std::pair<bool, int>{true, std::stoi(input[index_left]) * std::stoi(input[index_middle]) * std::stoi(input[index_right])};
         }
@@ -29,7 +29,7 @@ std::pair<bool, int> part_two(std::vector<std::string> input) {
 int main() {
   utils::Reader reader(std::filesystem::path("../2020/data/input_01.txt"));
   std::vector<std::string> input = reader.get_lines();
-  
+
   std::pair<bool, int> answer_one = part_one(input);
   if (std::get<0>(answer_one))
     std::cout << "The answer to part one is: " << std::get<1>(answer_one) << std::endl;
@@ -44,3 +44,4 @@ int main() {
 
   return 0;
 }
+
