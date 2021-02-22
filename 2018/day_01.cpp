6bb4736803067f98955a51f11ceda1d950637247
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 04:36:23 +0100

    linting issues

--- 2018/day_01.cpp
+++ 2018/day_01.cpp
@@ -3,39 +3,31 @@
 
 #include "../utils/input.hpp"
 
-std::vector<int> prepare_input( const std::vector<std::string>& input)
-{
+std::vector<int> prepare_input(const std::vector<std::string>& input) {
   std::vector<int> changes;
-  for (auto line: input)
-  {
+  for (auto line: input) {
     changes.push_back(std::stoi(line));
   }
   return changes;
 }
 
-int part_one(const std::vector<std::string>& input)
-{
+int part_one(const std::vector<std::string>& input) {
   auto changes = prepare_input(input);
   int frequency{0};
-  for (auto change: changes)
-  {
+  for (auto change: changes) {
     frequency += change;
   }
   return frequency;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
   auto changes = prepare_input(input);
   std::set<int> frequencies{0};
   int frequency{0};
-  while (1)
-  {
-    for (auto change: changes)
-    {
+  while (1) {
+    for (auto change: changes) {
       frequency += change;
-      if (frequencies.count(frequency) > 0)
-      {
+      if (frequencies.count(frequency) > 0) {
         return frequency;
       }
       frequencies.insert(frequency);
@@ -44,14 +36,14 @@ int part_two(const std::vector<std::string>& input)
   return std::numeric_limits<int>::min();
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2018/data/input_01.txt"));
   auto input = reader.get_lines();
-  
+
   auto answer_one =  part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
   auto answer_two =  part_two(input);
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
+
