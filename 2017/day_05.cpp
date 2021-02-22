8c268dc1069fb84933a5dec5470b08564222319c
Stefan Mesken <kappadistributive@gmail.com>
2021-02-22 04:30:56 +0100

    linting issues

--- 2017/day_05.cpp
+++ 2017/day_05.cpp
@@ -1,61 +1,50 @@
 #include "../utils/input.hpp"
 
-std::vector<int> prepare_input(const std::vector<std::string>& input)
-{
+std::vector<int> prepare_input(const std::vector<std::string>& input) {
   std::vector<int> jumps;
-  for (auto line: input)
-  {
+  for (auto line: input) {
     jumps.push_back(std::stoi(line));
   }
   return jumps;
 }
 
-long part_one(const std::vector<std::string>& input)
-{
+int64_t part_one(const std::vector<std::string>& input) {
   auto jumps = prepare_input(input);
   int position{0};
-  long step{0};
-  do
-  {
+  int64_t step{0};
+  do {
     position += jumps[position]++;
     step++;
-  }
-  while (0 <= position && position < jumps.size());
+  } while (0 <= position && position < jumps.size());
   return step;
 }
 
-int part_two(const std::vector<std::string>& input)
-{
+int part_two(const std::vector<std::string>& input) {
   auto jumps = prepare_input(input);
   int position{0};
-  long step{0};
+  int64_t step{0};
   int jump_value;
-  do
-  {
+  do {
     jump_value = jumps[position];
-    if (jump_value >= 3)
-    {
+    if (jump_value >= 3) {
       jumps[position]--;
-    }
-    else
-    {
+    } else {
       jumps[position]++;
     }
     position += jump_value;
     step++;
-  }
-  while (0 <= position && position < jumps.size());
+  } while (0 <= position && position < jumps.size());
   return step;
 }
 
-int main()
-{
+int main() {
   utils::Reader reader(std::filesystem::path("../2017/data/input_05.txt"));
   auto input = reader.get_lines();
-  
+
   auto answer_one =  part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
   auto answer_two =  part_two(input);
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
+
