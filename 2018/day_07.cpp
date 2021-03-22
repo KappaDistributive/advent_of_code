0d7e84bb5b8d71ee5fd2ef1ac8bfcd737ce98bfe
Stefan Mesken <kappadistributive@gmail.com>
2021-03-22 19:36:36 +0100

    solve 2018_07

--- 2018/day_07.cpp
+++ 2018/day_07.cpp
@@ -4,6 +4,7 @@
 
 #include "../utils/input.hpp"
 
+
 std::set<std::pair<char, char>> prepare_input(const std::vector<std::string>& input) {
     std::regex re{"^Step (\\w) must be finished before step (\\w) can begin.$"};
     std::smatch matches;
@@ -15,6 +16,7 @@ std::set<std::pair<char, char>> prepare_input(const std::vector<std::string>& in
     }
     return restrictions;
 }
+
 
 std::vector<char> prepare_alphabet(const std::vector<std::string>& input) {
     std::regex re{"^Step (\\w) must be finished before step (\\w) can begin.$"};
@@ -37,6 +39,7 @@ std::vector<char> prepare_alphabet(const std::vector<std::string>& input) {
     return alphabet;
 }
 
+
 size_t fac(size_t n) {
     if (n <= 1) {
         return 1;
@@ -45,23 +48,32 @@ size_t fac(size_t n) {
     }
 }
 
+
+std::set<char> available_tasks(const std::vector<char>& alphabet,
+                               const std::set<std::pair<char, char>>& restrictions,
+                               const std::string& done_tasks,
+                               const std::string& started_tasks="") {
+    std::set<char> candidates;
+    for (auto c: alphabet) {
+        candidates.insert(c);
+    }
+    for (auto c: done_tasks + started_tasks) {
+        candidates.erase(c);
+    }
+    for (auto [before, after]: restrictions) {
+        if (std::find(done_tasks.begin(), done_tasks.end(), before) == done_tasks.end()) {
+            candidates.erase(after);
+        }
+    }
+    return candidates;
+}
+
 std::string part_one(const std::vector<std::string>& input) {
     auto restrictions = prepare_input(input);
     auto alphabet = prepare_alphabet(input);
     std::string result;
     while (result.size() < alphabet.size()) {
-        std::set<char> candidates;
-        for (auto c: alphabet) {
-            candidates.insert(c);
-        }
-        for (auto c: result) {
-            candidates.erase(c);
-        }
-        for (auto [before, after]: restrictions) {
-            if (std::find(result.begin(), result.end(), before) == result.end()) {
-                candidates.erase(after);
-            }
-        }
+        auto candidates = available_tasks(alphabet, restrictions, result);
         result += *std::min_element(candidates.begin(), candidates.end());
     }
     
@@ -69,9 +81,45 @@ std::string part_one(const std::vector<std::string>& input) {
 }
 
 size_t part_two(const std::vector<std::string>& input) {
-    return 9;
-}
+    auto restrictions = prepare_input(input);
+    auto alphabet = prepare_alphabet(input);
+    size_t second{0};
+    std::string result;
+    std::string started;
+    std::vector<std::tuple<char, int>> workers = {
+        {'\0', -1},
+        {'\0', -1},
+        {'\0', -1},
+        {'\0', -1},
+        {'\0', -1},
+    };
+    bool finished_new_work{false};
+    while (result.size() < alphabet.size()) {
+        finished_new_work = false;
+        for (size_t index{0}; index < workers.size(); index++) {
+            auto worker = workers[index];
+            if (std::get<0>(worker) == '\0') { // worker is idle
+                if(started.size() < alphabet.size()) { 
+                    auto candidates = available_tasks(alphabet, restrictions, result, started);
+                    if (candidates.size() > 0) {
+                        auto candidate = *std::min_element(candidates.begin(), candidates.end());
+                        started += candidate;
+                        workers[index] = {candidate, second + static_cast<int>(candidate - 'A') + 61};
+                    }
+                }
+            } else if (std::get<1>(worker) == second) { // worker is done
+                finished_new_work = true;
+                result += std::get<0>(worker);
+                workers[index] = {'\0', -1};
+            }
+        }
+        if (!finished_new_work) {
+            second++;
+        }
+    }
 
+    return second;
+}
 
 
 int main() {
@@ -84,4 +132,3 @@ int main() {
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
-
