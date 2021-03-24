300f486da7c52e79d8ab27d2e212d2c756983b26
Stefan Mesken <kappadistributive@gmail.com>
2021-03-24 23:50:57 +0100

    solve 2018_13

--- 2018/day_13.cpp
+++ 2018/day_13.cpp
@@ -35,6 +35,7 @@ class Cart {
     size_t intersections_seen{0};
     std::pair<size_t, size_t> _position;
     Direction _direction;
+    bool _is_removed{false};
 
     char ahead(const char& up, const char& right, const char& down, const char& left) {
         switch (_direction) {
@@ -58,7 +59,18 @@ class Cart {
         return _direction;
     }
 
+    void remove() {
+        _is_removed = true;
+    }
+
+    bool is_removed() const {
+        return _is_removed;
+    }
+
     void move(const char& up, const char& right, const char& down, const char& left) {
+        if (is_removed()) {
+            return;
+        }
         auto in_front = ahead(up, right, down, left);
         switch (_direction) {
             case north: std::get<1>(_position)--; break;
@@ -219,42 +231,49 @@ class Track {
     }
 
     bool step() {
+        bool crashed{false};
+        crash_site = {_width + 1, _height + 1};
         std::sort(
             carts.begin(),
-             carts.end(),
-             [] (Cart lhs, Cart rhs) {
-                 auto lhs_pos = lhs.position();
-                 auto rhs_pos = rhs.position();
-                 if (std::get<1>(lhs_pos) < std::get<1>(rhs_pos)) {
-                     return true;
-                 } else if (std::get<1>(lhs_pos) == std::get<1>(rhs_pos) && std::get<0>(lhs_pos) < std::get<0>(rhs_pos)) {
-                     return true;
-                 } else {
-                     return false;
-                 }
-             }
+            carts.end(),
+            [] (Cart lhs, Cart rhs) {
+                auto lhs_pos = lhs.position();
+                auto rhs_pos = rhs.position();
+                if (std::get<1>(lhs_pos) < std::get<1>(rhs_pos)) {
+                    return true;
+                } else if (std::get<1>(lhs_pos) == std::get<1>(rhs_pos) && std::get<0>(lhs_pos) < std::get<0>(rhs_pos)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         );
         // std::cout << "Sorted carts:" << std::endl;
         for (auto& cart: carts) {
             auto [x, y] = cart.position();
             try {
                 cart.move(
-                    y > 0            ? map({x, y - 1}) : ' ',
-                    (x + 1) < width  ? map({x + 1, y}) : ' ',
-                    (y + 1) < height ? map({x, y + 1}) : ' ',
-                    x > 0            ? map({x - 1, y}) : ' '
+                    y > 0            ? map({x, y - 1}, false) : ' ',
+                    (x + 1) < width  ? map({x + 1, y}, false) : ' ',
+                    (y + 1) < height ? map({x, y + 1}, false) : ' ',
+                    x > 0            ? map({x - 1, y}, false) : ' '
                 );
             } catch (const std::runtime_error& e) {
+                crashed = true;
                 if (std::string(e.what()) == "Crash!") {
                     crash_site = cart.position();
-                    return false;
+                    for (auto it=carts.begin(); it != carts.end(); it++) {
+                        if (it->position() == crash_site) {
+                            it->remove();
+                        }
+                    }
                 } else {
                     throw e;
                 }
             }
             // std::cout << "(" << std::get<0>(cart.position()) << ", " << std::get<1>(cart.position()) << ")" << std::endl;
         }
-        return true;
+        return !crashed;
     }
 
     std::optional<Cart*> cart(const std::pair<size_t, size_t>& position) {
@@ -266,19 +285,29 @@ class Track {
         return std::nullopt;
     }
 
+    size_t num_carts() const {
+        size_t num_carts{0};
+        for (auto cart: carts) {
+            if (!cart.is_removed()) {
+                num_carts++;
+            }
+        }
+        return num_carts;
+    }
+
     char& track(const std::pair<size_t, size_t>& position) {
         assert (std::get<0>(position) < width);
         assert (std::get<1>(position) < height);
         return _track[std::get<1>(position) * width + std::get<0>(position)];
     }
 
-    char map(const std::pair<size_t, size_t>& position) {
+    char map(const std::pair<size_t, size_t>& position, bool show_crashes = true) {
         assert (std::get<0>(position) < width);
         assert (std::get<1>(position) < height);
         auto potential_cart = cart(position);
-        if (crash_position() == position) {
+        if (show_crashes && crash_position() == position) {
             return 'X';
-        } else if (potential_cart.has_value()) {
+        } else if (potential_cart.has_value() && !potential_cart.value()->is_removed()) {
             return repr(potential_cart.value()->direction());
         } else {
             return _track[std::get<1>(position) * width + std::get<0>(position)];
@@ -287,6 +316,16 @@ class Track {
 
     std::pair<size_t, size_t> crash_position() const {
         return crash_site;
+    }
+
+    std::pair<size_t, size_t> final_cart_position() const {
+        assert(num_carts() == 1);
+        for (auto cart: carts) {
+            if (!cart.is_removed()) {
+                return cart.position();
+            }
+        }
+        throw std::runtime_error("This show never happen.");
     }
 
     friend std::ostream& operator<<(std::ostream& os, Track<_width, _height>& track) {
@@ -303,8 +342,10 @@ class Track {
     }
 };
 
+
 std::string part_one(const std::vector<std::string>& input) {
     Track<150, 150> track(input);
+    // debug track:
     // Track<13, 6> track(std::vector<std::string>({
     //     std::string("/->-\\        "),
     //     std::string("|   |  /----\\"),
@@ -315,12 +356,12 @@ std::string part_one(const std::vector<std::string>& input) {
     // }));
     size_t time{0};
     do {
-        std::cout << "Time: " << time << std::endl;
-        std::cout << track << std::endl;
+        // std::cout << "Time: " << time << std::endl;
+        // std::cout << track << std::endl;
         time++;
     } while (track.step());
-    std::cout << "Time: " << time << std::endl;
-    std::cout << track << std::endl;
+    // std::cout << "Time: " << time << std::endl;
+    // std::cout << track << std::endl;
 
     std::stringstream ss;
     ss << std::get<0>(track.crash_position()) << "," << std::get<1>(track.crash_position());
@@ -328,8 +369,30 @@ std::string part_one(const std::vector<std::string>& input) {
 }
 
 
-int part_two(const std::vector<std::string>& input) {
-    return -1;
+std::string part_two(const std::vector<std::string>& input) {
+    Track<150, 150> track(input);
+    // debug track:
+    // Track<7, 7> track(std::vector<std::string>({
+    //     "/>-<\\  ", 
+    //     "|   |  ",
+    //     "| /<+-\\",
+    //     "| | | v",
+    //     "\\>+</ |",
+    //     "  |   ^",
+    //     "  \\<->/"}));
+    size_t time{0};
+    do {
+        // std::cout << "Time: " << time << std::endl;
+        // std::cout << track << std::endl;
+        time++;
+        track.step();
+    } while (track.num_carts() > 1);
+    // std::cout << "Time: " << time << std::endl;
+    // std::cout << track << std::endl;
+
+    std::stringstream ss;
+    ss << std::get<0>(track.final_cart_position()) << "," << std::get<1>(track.final_cart_position());
+    return ss.str(); 
 }
 
 
