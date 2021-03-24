29811703de23ef80ab542ba32b1ddae4663647c2
Stefan Mesken <kappadistributive@gmail.com>
2021-03-24 19:38:06 +0100

    solve 2018_12

--- 2018/day_12.cpp
+++ 2018/day_12.cpp
@@ -67,6 +67,25 @@ std::pair<std::deque<char>, int> strip(const std::deque<char>& input, char symbo
     return {std::deque<char>(left, right), offset};
 }
 
+void update(std::deque<char>& state, int& offset, const std::map<std::string, char>& transformations) {
+    state = step(state, transformations);
+    offset -= 3;
+    auto stripped_update = strip(state, '.');
+    state = std::get<0>(stripped_update);
+    offset += std::get<1>(stripped_update);
+}
+
+long long score(const std::deque<char>& state, const long& offset) {
+    long long result{0};
+    for (long long index{0}; index < state.size(); index++) {
+        if (state[index] == '#') {
+            result += offset + index;
+        }
+    }
+
+    return result;
+}
+
 int part_one(const std::vector<std::string>& input) {
     std::deque<char> state;
     std::map<std::string, char> transformations;
@@ -77,28 +96,37 @@ int part_one(const std::vector<std::string>& input) {
 
     std::cout << "0: " << utils::stringify(state) << std::endl;
     for (size_t time{1}; time <= 20; time++) {
-        state = step(state, transformations);
-        offset -= 3;
-        auto stripped_update = strip(state, '.');
-        state = std::get<0>(stripped_update);
-        offset += std::get<1>(stripped_update);
+        update(state, offset, transformations);
         std::cout << "Offset: " << offset << std::endl;
         std::cout << time << ": " << utils::stringify(state) << std::endl;
     }
 
-    int result{0};
-    for (int index{0}; index < state.size(); index++) {
-        if (state[index] == '#') {
-            result += offset + index;
-        }
-    }
-
-    return result;
+    return score(state, offset);
 }
 
 
-int part_two(const std::vector<std::string>& input) {
-    return -9;
+long long part_two(const std::vector<std::string>& input) {
+    std::deque<char> state;
+    std::map<std::string, char> transformations;
+    auto prepared_input = prepare_input(input);
+    int offset{0};
+    int transition_offset;
+    state = std::get<0>(prepared_input);
+    transformations = std::get<1>(prepared_input);
+    size_t time{0};
+    while (true) {
+        time++;
+        auto old_offset = offset;
+        auto old_state = state;
+        update(state, offset, transformations);
+        if (old_state == state) {
+            transition_offset = offset - old_offset;
+            std::cout << time << ": Found fixed point! Transition-Offset = " << transition_offset << std::endl;
+            break;
+        }
+    }
+    long long final_offset = static_cast<long long>(offset) + static_cast<long long>(50000000000ll - time) * static_cast<long long>(transition_offset);
+    return score(state, final_offset);
 }
 
 
