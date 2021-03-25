183b2b5336a867e9949345e978271e0a4076d703
Stefan Mesken <kappadistributive@gmail.com>
2021-03-25 13:01:02 +0100

    add SplitString unit tests

--- tests/UtilsUnitTests.cpp
+++ tests/UtilsUnitTests.cpp
@@ -3,9 +3,22 @@
 #include "../utils/input.hpp"
 
 namespace {
-TEST(SplitStringOnSpaces, Positive) {
+TEST(SplitString, EmptyResult) {
+  auto seen = utils::split_string("eeeeeeeee", 'e');
+  std::vector<std::string> expected;
+  EXPECT_EQ(seen, expected);
+}
+
+TEST(SplitString, StripsDelimiter) {
   auto seen = utils::split_string(" this is a test ", ' ');
   std::vector<std::string> expected{{"this", "is", "a", "test"}};
   EXPECT_EQ(seen, expected);
 }
+
+TEST(SplitString, RepeatDelimiters) {
+  auto seen = utils::split_string("..this.is..a.test.", '.');
+  std::vector<std::string> expected{{"this", "is", "a", "test"}};
+  EXPECT_EQ(seen, expected);
+}
+
 }  // namespace
