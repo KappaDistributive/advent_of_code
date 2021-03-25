0ca52e0c5faf1d5c6d8bc90a7e20f0ce504f0927
Stefan Mesken <kappadistributive@gmail.com>
2021-03-25 10:31:10 +0100

    linting

--- 2018/day_04.cpp
+++ 2018/day_04.cpp
@@ -15,13 +15,14 @@ std::vector<std::string> sort_input(const std::vector<std::string>& input) {
             result.end(),
             [] (std::string lhs, std::string rhs) -> bool {
                 return lhs.substr(0, 18) < rhs.substr(0, 18);
-            }
-    );
+            });
 
     return result;
 }
 
-std::map<int, std::set<std::string>> calculate_sleeping_patterns(const std::vector<std::string>& input) {
+
+std::map<int, std::set<std::string>>
+calculate_sleeping_patterns(const std::vector<std::string>& input) {
     auto sorted_input = sort_input(input);
     int active_guard_id{-1};
     bool guard_sleeps{false};
@@ -31,7 +32,7 @@ std::map<int, std::set<std::string>> calculate_sleeping_patterns(const std::vect
     std::regex guard_re{"^.*#(\\d+).*$"};
     std::smatch matches;
 
-    for (auto line: sorted_input) {
+    for (auto line : sorted_input) {
         std::regex_match(line, matches, guard_re);
         if (matches.size() == 2) {
             active_guard_id = std::stoi(matches[1]);
@@ -44,7 +45,8 @@ std::map<int, std::set<std::string>> calculate_sleeping_patterns(const std::vect
 
         } else {
             assert(line.find("wakes up") != std::string::npos);
-            sleeping_patterns.emplace(std::make_pair(active_guard_id, std::set<std::string>()));
+            sleeping_patterns.emplace(
+                std::make_pair(active_guard_id, std::set<std::string>()));
             guard_sleeps = false;
             auto time = line.substr(0, 18);
 
@@ -65,13 +67,15 @@ std::map<int, std::set<std::string>> calculate_sleeping_patterns(const std::vect
     return sleeping_patterns;
 }
 
+
 size_t part_one(const std::vector<std::string>& input) {
     auto sleeping_patterns = calculate_sleeping_patterns(input);
     int sleepy_guard_id{-1};
     size_t sleepy_guard_score{0};
 
-    for (auto [guard_id, sleeping_pattern]: sleeping_patterns) {
-        if (sleepy_guard_id == -1 || sleeping_pattern.size() > sleepy_guard_score) {
+    for (auto [guard_id, sleeping_pattern] : sleeping_patterns) {
+        if (sleepy_guard_id == -1 ||
+            sleeping_pattern.size() > sleepy_guard_score) {
             sleepy_guard_id = guard_id;
             sleepy_guard_score = sleeping_pattern.size();
         }
@@ -79,7 +83,7 @@ size_t part_one(const std::vector<std::string>& input) {
 
     auto sleeping_pattern = sleeping_patterns.at(sleepy_guard_id);
     std::array<int, 60> sleeping_minutes{0};
-    for (auto time: sleeping_pattern) {
+    for (auto time : sleeping_pattern) {
         sleeping_minutes[std::stoi(time.substr(15, 2))]++;
     }
     size_t sleepy_minute_index{0};
@@ -93,15 +97,16 @@ size_t part_one(const std::vector<std::string>& input) {
     return sleepy_guard_id * sleepy_minute_index;
 }
 
+
 size_t part_two(const std::vector<std::string>& input) {
     auto sleeping_patterns = calculate_sleeping_patterns(input);
     int guard_id{-1};
     int guard_score{-1};
     int sleepy_minute{-1};
 
-    for (auto [id, sleeping_pattern]: sleeping_patterns) {
+    for (auto [id, sleeping_pattern] : sleeping_patterns) {
         std::array<int, 60> sleeping_minutes{0};
-        for (auto time: sleeping_pattern) {
+        for (auto time : sleeping_pattern) {
             sleeping_minutes[std::stoi(time.substr(15, 2))]++;
         }
 
@@ -117,7 +122,6 @@ size_t part_two(const std::vector<std::string>& input) {
 }
 
 
-
 int main() {
   utils::Reader reader(std::filesystem::path("../2018/data/input_04.txt"));
   auto input = reader.get_lines();
@@ -128,4 +132,3 @@ int main() {
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
-
