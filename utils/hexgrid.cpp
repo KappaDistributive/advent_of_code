d7e358b1365f3e4a2fc833f7288bb9371491c6fc
Stefan Mesken <kappadistributive@gmail.com>
2021-04-01 12:18:17 +0200

    linting

--- utils/hexgrid.cpp
+++ utils/hexgrid.cpp
@@ -6,87 +6,87 @@ namespace hexgrid {
 
 std::ostream&
 operator<<(std::ostream& os, const Direction& direction) {
-  switch (direction) {
-    case Direction::kNorthWest: os << "northwest"; break;
-    case Direction::kNorth: os << "north"; break;
-    case Direction::kNorthEast: os << "northeast"; break;
-    case Direction::kSouthWest: os << "southwest"; break;
-    case Direction::kSouth: os << "south"; break;
-    case Direction::kSouthEast: os << "southeast"; break;
-  }
+switch (direction) {
+  case Direction::kNorthWest: os << "northwest"; break;
+  case Direction::kNorth: os << "north"; break;
+  case Direction::kNorthEast: os << "northeast"; break;
+  case Direction::kSouthWest: os << "southwest"; break;
+  case Direction::kSouth: os << "south"; break;
+  case Direction::kSouthEast: os << "southeast"; break;
+}
 
-  return os;
+return os;
 }
 
 Point::Point(const int& x, const int& y)
-    : m_x(x), m_y(y) {
+  : m_x(x), m_y(y) {
 }
 
 Point::Point(const Point& point)
-    : m_x(point.m_x), m_y(point.m_y) {
+  : m_x(point.m_x), m_y(point.m_y) {
 }
 
 size_t
 Point::distance(const Point& destination) const {
-  auto delta_x = destination.m_x - this->m_x;
-  auto delta_y = destination.m_y - this->m_y;
+auto delta_x = destination.m_x - this->m_x;
+auto delta_y = destination.m_y - this->m_y;
 
-  if ((delta_x > 0) == (delta_y > 0)) {
-    return std::max(std::abs(delta_x), std::abs(delta_y));
-  } else {
-    return std::abs(delta_x) + std::abs(delta_y);
-  }
+if ((delta_x > 0) == (delta_y > 0)) {
+  return std::max(std::abs(delta_x), std::abs(delta_y));
+} else {
+  return std::abs(delta_x) + std::abs(delta_y);
+}
 }
 
 void
 Point::step(const Direction& direction) {
-  switch (direction) {
-    case Direction::kNorthWest:
-      this->m_x--;
-      break;
-    case Direction::kNorth:
-      this->m_y++;
-      break;
-    case Direction::kNorthEast:
-      this->m_x++;
-      this->m_y++;
-      break;
-    case Direction::kSouthEast:
-      this->m_x++;
-      break;
-    case Direction::kSouth:
-      this->m_y--;
-      break;
-    case Direction::kSouthWest:
-      this->m_x--;
-      this->m_y--;
-      break;
-  }
+switch (direction) {
+  case Direction::kNorthWest:
+    this->m_x--;
+    break;
+  case Direction::kNorth:
+    this->m_y++;
+    break;
+  case Direction::kNorthEast:
+    this->m_x++;
+    this->m_y++;
+    break;
+  case Direction::kSouthEast:
+    this->m_x++;
+    break;
+  case Direction::kSouth:
+    this->m_y--;
+    break;
+  case Direction::kSouthWest:
+    this->m_x--;
+    this->m_y--;
+    break;
+}
 }
 
 Point&
 Point::operator+=(const Point& summand) {
-  this->m_x += summand.m_x;
-  this->m_y += summand.m_y;
+this->m_x += summand.m_x;
+this->m_y += summand.m_y;
 
-  return *this;
+return *this;
 }
 
 bool
 operator==(const Point& lhs, const Point& rhs) {
-  return lhs.m_x == rhs.m_x && lhs.m_y == rhs.m_y;
+return lhs.m_x == rhs.m_x && lhs.m_y == rhs.m_y;
 }
 
 Point
 operator+(const Point& lhs, const Point& rhs) {
-  Point point(lhs.m_x + rhs.m_x, lhs.m_y + rhs.m_y);
+Point point(lhs.m_x + rhs.m_x, lhs.m_y + rhs.m_y);
 
-  return point;
+return point;
 }
 
 std::ostream& operator<<(std::ostream& os, const Point& point) {
-  os << "(" << point.m_x << ", " << point.m_y << ")";
-  return os;
+os << "(" << point.m_x << ", " << point.m_y << ")";
+return os;
 }
 
 }  // namespace hexgrid
