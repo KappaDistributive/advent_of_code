e181f154c2cc7b07c2a0152fdd9d060dc9b57427
Stefan Mesken <kappadistributive@gmail.com>
2021-04-04 17:18:49 +0200

    linting

--- 2017/day_04.cpp
+++ 2017/day_04.cpp
@@ -5,10 +5,10 @@
 
 int part_one(const std::vector<std::string>& input) {
   int result{0};
-  for (auto line: input) {
+  for (auto line : input) {
     auto splits = utils::split_string(line, ' ');
     std::set<std::string> splits_set;
-    for (auto split: splits) {
+    for (auto split : splits) {
       splits_set.insert(split);
     }
     result += splits_set.size() == splits.size();
@@ -18,18 +18,18 @@ int part_one(const std::vector<std::string>& input) {
 
 int part_two(const std::vector<std::string>& input) {
   int result{0};
-  for (auto line: input) {
+  for (auto line : input) {
     auto splits = utils::split_string(line, ' ');
     std::vector<std::string> splits_vector;
     std::set<std::string> splits_set;
-    for (auto split: splits) {
+    for (auto split : splits) {
       std::vector<char> characters;
-      for (auto character: split) {
+      for (auto character : split) {
         characters.push_back(character);
       }
       std::sort(characters.begin(), characters.end());
       std::string word;
-      for (auto character: characters) {
+      for (auto character : characters) {
         word += character;
       }
       splits_set.insert(word);
@@ -49,4 +49,3 @@ int main() {
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
-
