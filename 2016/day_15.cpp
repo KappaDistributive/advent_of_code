5efac3f5ebb557ee6351def1e660f4e93cd6b403
Stefan Mesken <kappadistributive@gmail.com>
2021-04-05 17:02:05 +0200

    refinement

--- 2016/day_15.cpp
+++ 2016/day_15.cpp
@@ -1,5 +1,5 @@
 #include <cassert>
-#include <regex>
+#include <regex>  // NOLINT
 
 #include "../utils/input.hpp"
 
@@ -10,7 +10,9 @@ class Disc {
 
  public:
   Disc(size_t id, size_t size, size_t initial_position)
-    : id(id), size(size), initial_position(initial_position) {
+    : id(id),
+      initial_position(initial_position),
+      size(size) {
   }
 
   size_t get_position(size_t time) const {
@@ -24,16 +26,21 @@ class Disc {
   }
 };
 
+
 std::vector<Disc> prepare_input(const std::vector<std::string>& input) {
-  std::regex re{"^Disc\\s#(\\d+)\\shas\\s(\\d+)\\spositions;\\sat\\stime=0,\\sit\\sis\\sat position\\s(\\d+).$"};
+  std::regex re{"^Disc\\s#(\\d+)\\shas\\s(\\d+)\\spositions;\\sat\\stime=0,\\sit\\sis\\sat position\\s(\\d+).$"};  // NOLINT
   std::smatch matches;
   std::vector<Disc> discs;
-  for (auto line: input) {
+  for (auto line : input) {
     assert(std::regex_match(line, matches, re));
-    discs.push_back(Disc(std::stoi(matches[1].str()), std::stoi(matches[2].str()), std::stoi(matches[3].str())));
+    discs.push_back(Disc(std::stoi(matches[1].str()),
+                         std::stoi(matches[2].str()),
+                         std::stoi(matches[3].str())));
   }
+
   return discs;
 }
+
 
 size_t find_first_opportune_moment(const std::vector<Disc>& discs) {
   size_t global_time{0}, local_time{0};
@@ -41,7 +48,7 @@ size_t find_first_opportune_moment(const std::vector<Disc>& discs) {
   while (searching) {
     local_time = global_time;
     searching = false;
-    for (auto disc: discs) {
+    for (auto disc : discs) {
       local_time++;
       if (disc.get_position(local_time) != 0) {
         searching = true;
@@ -53,16 +60,19 @@ size_t find_first_opportune_moment(const std::vector<Disc>& discs) {
   return global_time - 1;
 }
 
-size_t part_one(const std::vector<std::string>& input) {
+
+auto part_one(const std::vector<std::string>& input) {
   auto discs = prepare_input(input);
   return find_first_opportune_moment(discs);
 }
 
-int part_two(const std::vector<std::string>& input) {
+
+auto part_two(const std::vector<std::string>& input) {
   auto discs = prepare_input(input);
   discs.push_back(Disc(discs.size(), 11, 0));
   return find_first_opportune_moment(discs);
 }
+
 
 int main() {
   utils::Reader reader(std::filesystem::path("../2016/data/input_15.txt"));
@@ -74,4 +84,3 @@ int main() {
   std::cout << "The answer to part two is: " << answer_two << std::endl;
   return 0;
 }
-
