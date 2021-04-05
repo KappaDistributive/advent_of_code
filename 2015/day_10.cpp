2ae430975d3d7cd9f3171ece76550a0945e1e5ec
Stefan Mesken <kappadistributive@gmail.com>
2021-04-05 20:29:31 +0200

    refinement

--- 2015/day_10.cpp
+++ 2015/day_10.cpp
@@ -1,15 +1,15 @@
 #include <algorithm>
 #include <map>
-#include <regex>
+#include <regex>  // NOLINT
 #include <set>
 
 #include "../utils/input.hpp"
+
 
 std::vector<std::string> split_blocks(const std::string& input) {
   std::vector<std::string> blocks;
   std::string block;
 
-  char current;
   if (input.size() > 0) {
     block = input[0];
 
@@ -27,31 +27,34 @@ std::vector<std::string> split_blocks(const std::string& input) {
   return blocks;
 }
 
+
 int part_one(const std::string& input) {
   std::string current_input{input};
   for (size_t index{0}; index < 40; index++) {
     auto blocks = split_blocks(current_input);
     current_input = "";
-    for (auto block: blocks) {
+    for (auto block : blocks) {
       current_input += std::to_string(block.size()) + block[0];
     }
   }
 
   return current_input.size();
 }
+
 
 int part_two(const std::string& input) {
   std::string current_input{input};
   for (size_t index{0}; index < 50; index++) {
     auto blocks = split_blocks(current_input);
     current_input = "";
-    for (auto block: blocks) {
+    for (auto block : blocks) {
       current_input += std::to_string(block.size()) + block[0];
     }
   }
 
   return current_input.size();
 }
+
 
 int main() {
   utils::Reader reader(std::filesystem::path("../2015/data/input_10.txt"));
@@ -62,4 +65,3 @@ int main() {
 
   return 0;
 }
-
