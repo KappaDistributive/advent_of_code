ed2d3ecbe4a7687cb7666c13338b68f5b4fabfd9
Stefan Mesken <kappadistributive@gmail.com>
2021-04-09 21:53:15 +0200

    clean up

--- 2015/day_12.cpp
+++ 2015/day_12.cpp
@@ -16,19 +16,19 @@ int part_one(const std::string& input) {
   return result;
 }
 
-int part_two(const std::string& input) {
-  int result{0};
-  // TODO: solve part_two in C++
-
-  return result;
-}
+// int part_two(const std::string& input) {
+//   int result{0};
+//   // TODO: solve part_two in C++
+// 
+//   return result;
+// }
 
 int main() {
   utils::Reader reader(std::filesystem::path("../2015/data/input_12.txt"));
   auto input = reader.get_lines()[0];
 
   std::cout << "The answer to part one is: " << part_one(input) << std::endl;
-  std::cout << "The answer to part two is: " << part_two(input) << std::endl;
+  // std::cout << "The answer to part two is: " << part_two(input) << std::endl;
 
   return 0;
 }
