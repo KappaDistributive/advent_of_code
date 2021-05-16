6dc2668ecf7a8149ea08d7fcbf77109af14ae992
Stefan Mesken <kappadistributive@gmail.com>
2021-05-16 12:08:28 +0200

    print meta data

--- 2018/day_15.cpp
+++ 2018/day_15.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <map>
+#include <memory>
 #include <optional>
+#include <sstream>
 
 #include "../utils/input.hpp"
 
@@ -17,7 +19,7 @@ struct Point {
 
 
 class Entity {
- private:
+ protected:
   char m_symbol;
 
  public:
@@ -25,10 +27,26 @@ class Entity {
     : m_symbol(symbol) {
   }
 
+  virtual ~Entity() = default;
+
+  virtual bool
+  is_unit() const noexcept {
+    return false;
+  }
+
   friend std::ostream&
   operator<<(std::ostream& os, const Entity& entity) {
     os << entity.m_symbol;
     return os;
+  }
+
+  virtual std::string
+  to_string() const noexcept {
+    std::string representation;
+    representation += m_symbol;
+    representation += "()";
+
+    return representation;
   }
 };
 
@@ -40,7 +58,7 @@ class Wall : public Entity {
 
 
 class Unit : public Entity {
- private:
+ protected:
   int m_hit_points;
 
  public:
@@ -48,18 +66,34 @@ class Unit : public Entity {
     : Entity(symbol),
       m_hit_points(200) {
   }
+
+  bool
+  is_unit() const noexcept {
+    return true;
+  }
+
+  std::string
+  to_string() const noexcept {
+    std::string representation;
+    representation += m_symbol;
+    representation += "(" + std::to_string(m_hit_points) + ")";
+
+    return representation;
+  }
 };
 
 
 class Elf : public Unit {
  public:
   Elf() : Unit('E') {}
+
 };
 
 
 class Goblin : public Unit {
  public:
   Goblin() : Unit('G') {}
+
 };
 
 
@@ -87,11 +121,13 @@ create_entity(char character) {
 
 class Battlefield {
  private:
-  std::map<Point, Entity> m_entities;
+  std::map<Point, std::shared_ptr<Entity>> m_entities;
+  std::shared_ptr<Entity> m_wall;
   size_t m_width{0}, m_height{0};
 
  public:
-  explicit Battlefield(const std::vector<std::string>& input) {
+  explicit Battlefield(const std::vector<std::string>& input)
+    : m_wall(std::make_shared<Entity>('.')) {
     for (size_t y{0}; y < input.size(); ++y) {
       auto line = input[y];
       m_height++;
@@ -102,26 +138,55 @@ class Battlefield {
       }
       for (size_t x{0}; x < line.size(); ++x) {
         auto character = line[x];
-        auto entity = create_entity(character);
-        if (entity.has_value()) {
-          m_entities.insert({Point{x, y}, entity.value()});
+        Point point{x, y};
+
+        switch (character) {
+          case '.':
+            break;
+          case '#':
+            m_entities.insert({point, std::make_unique<Wall>()});
+            break;
+          case 'E':
+            m_entities.insert({point, std::make_unique<Elf>()});
+            break;
+          case 'G':
+            m_entities.insert({point, std::make_unique<Goblin>()});
+            break;
+          default:
+            throw std::invalid_argument("There is no entity of kind: " + std::to_string(character));
+            break;
         }
       }
     }
   }
 
-  Entity
+  std::shared_ptr<Entity>
   operator[](Point point) const noexcept {
-    return m_entities.count(point) > 0 ? m_entities.at(point) : Entity('.');
+    if (m_entities.count(point) > 0) {
+      return m_entities.at(point);
+    } else {
+      return m_wall;
+    }
   }
 
   friend std::ostream&
   operator<<(std::ostream& os, const Battlefield& battlefield) {
 
     for (size_t y{0}; y < battlefield.m_height; ++y) {
+      std::stringstream ss_map, ss_meta;
+      bool has_entered_meta{false};
       for (size_t x{0}; x < battlefield.m_width; ++x) {
-        os << battlefield.operator[](Point{x, y});
+        auto entity = battlefield.operator[](Point{x, y});
+        ss_map << *entity;
+        if (entity->is_unit()) {
+          if (has_entered_meta) {
+            ss_meta << ", ";
+          }
+          ss_meta << entity->to_string();
+          has_entered_meta = true;
+        }
       }
+      os << ss_map.str() << "\t" << ss_meta.str();
       if (y+1 < battlefield.m_height) {
         os << "\n";
       }
@@ -136,6 +201,9 @@ auto
 part_one(const std::vector<std::string>& input) {
   Battlefield battlefield(input);
   std::cout << battlefield << std::endl;
+  // Entity entity{'.'};
+  // Goblin goblin{};
+  // std::cout << entity.is_unit() << " " << goblin.is_unit() << std::endl;
   return "";
 }
 
