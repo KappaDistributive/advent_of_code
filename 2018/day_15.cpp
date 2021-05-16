91443f2199c97d9c105c2f9256b64c8b9384139e
Stefan Mesken <kappadistributive@gmail.com>
2021-05-16 15:53:19 +0200

    find open squares

--- 2018/day_15.cpp
+++ 2018/day_15.cpp
@@ -2,9 +2,38 @@
 #include <map>
 #include <memory>
 #include <optional>
+#include <set>
 #include <sstream>
 
 #include "../utils/input.hpp"
+
+
+enum class Direction: size_t {
+  north,
+  east,
+  south,
+  west,
+};
+
+
+static const std::vector<Direction> DIRECTIONS{
+  Direction::north, Direction::east, Direction::south, Direction::west};
+
+
+std::ostream&
+operator<<(std::ostream& os, Direction direction) {
+  switch (direction) {
+    case Direction::north: os << "Direction::north"; break;
+    case Direction::east:  os << "Direction::east";  break;
+    case Direction::south: os << "Direction::south"; break;
+    case Direction::west:  os << "Direction::west"; break;
+    default:
+      throw std::runtime_error("This should never happen.");
+      break;
+  }
+
+  return os;
+}
 
 
 struct Point {
@@ -16,6 +45,44 @@ struct Point {
     return this->y < other.y || (this->y == other.y && this->x < other.x);
   }
 };
+
+
+std::ostream&
+operator<<(std::ostream& os, const Point& point) noexcept {
+  os << "Point(x=" << point.x << ", y=" << point.y << ")";
+  return os;
+}
+
+
+std::optional<Point>
+operator+(Point point, Direction direction) {
+  switch (direction) {
+    case Direction::north:
+      if (point.y > 0) {
+        return Point{point.x, point.y - 1};
+      } else {
+        return std::nullopt;
+      }
+      break;
+    case Direction::east:
+      return Point{point.x + 1, point.y};
+      break;
+    case Direction::south:
+      return Point{point.x, point.y + 1};
+      break;
+    case Direction::west:
+      if (point.x > 0) {
+        return Point{point.x - 1, point.y};
+      } else {
+        return std::nullopt;
+      }
+      break;
+    default:
+      throw std::runtime_error("This should never happen.");
+      break;
+  }
+}
+
 
 
 class Entity {
@@ -95,14 +162,6 @@ class Goblin : public Unit {
 };
 
 
-enum class Direction: size_t {
-  north,
-  east,
-  south,
-  west,
-};
-
-
 std::optional<Entity>
 create_entity(char character) {
   switch (character) {
@@ -122,6 +181,19 @@ class Battlefield {
   std::map<Point, std::shared_ptr<Entity>> m_entities;
   std::shared_ptr<Entity> m_wall;
   size_t m_width{0}, m_height{0};
+
+  std::set<Point>
+  open_squares(Point point) {
+    std::set<Point> result;
+    for (auto direction : DIRECTIONS) {
+      auto candidate = point + direction;
+      if (candidate.has_value() && m_entities.count(candidate.value()) == 0) {
+        result.insert(candidate.value());
+      }
+    }
+
+    return result;
+  }
 
  public:
   explicit Battlefield(const std::vector<std::string>& input)
@@ -169,7 +241,6 @@ class Battlefield {
 
   friend std::ostream&
   operator<<(std::ostream& os, const Battlefield& battlefield) {
-
     for (size_t y{0}; y < battlefield.m_height; ++y) {
       std::stringstream ss_map, ss_meta;
       bool has_entered_meta{false};
@@ -192,13 +263,21 @@ class Battlefield {
 
     return os;
   }
+
+  void test() {
+    auto open = open_squares(Point{1, 21});
+    for (auto point : open) {
+      std::cout << point << std::endl;
+    }
+  }
 };
 
 
 auto
 part_one(const std::vector<std::string>& input) {
   Battlefield battlefield(input);
-  std::cout << battlefield << std::endl;
+  battlefield.test();
+  // std::cout << battlefield << std::endl;
   return "";
 }
 
@@ -221,3 +300,4 @@ main() {
 
   return 0;
 }
+
