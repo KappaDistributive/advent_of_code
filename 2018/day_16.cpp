935d02ff1fb0c315ec98f28887f55072a36f0a45
Stefan Mesken <kappadistributive@gmail.com>
2021-05-23 09:39:03 +0200

    solve part one of 2018_16

--- 2018/day_16.cpp
+++ 2018/day_16.cpp
@@ -1,16 +1,25 @@
 #include <array>
 #include <cassert>
+#include <sstream>
 
 #include "../utils/input.hpp"
 
+#ifdef DEBUG
+#define DBGVAR(os, var) \
+  (os) << "DBG: " << __FILE__ << "(" << __LINE__ << ") "\
+       << #var << " = [" << (var) << "]" << std::endl
+#else
+#define DBGVAR(os, var) do {}while(0)
+#endif
 
-enum class Mode : int {
+
+enum class Mode : uint {
   val,
   reg
 };
 
 
-enum class Opcode : int {
+enum class Opcode : uint {
   addr,
   addi,
   mulr,
@@ -30,6 +39,48 @@ enum class Opcode : int {
 };
 
 
+std::ostream&
+operator<<(std::ostream& os, const Opcode& opcode) {
+  switch (opcode) {
+    case Opcode::addr: os << "addr"; break;
+    case Opcode::addi: os << "addi"; break;
+    case Opcode::mulr: os << "mulr"; break;
+    case Opcode::muli: os << "muli"; break;
+    case Opcode::banr: os << "banr"; break;
+    case Opcode::bani: os << "bani"; break;
+    case Opcode::borr: os << "borr"; break;
+    case Opcode::bori: os << "bori"; break;
+    case Opcode::setr: os << "setr"; break;
+    case Opcode::seti: os << "seti"; break;
+    case Opcode::gtir: os << "gtir"; break;
+    case Opcode::gtri: os << "gtri"; break;
+    case Opcode::gtrr: os << "gtrr"; break;
+    case Opcode::eqir: os << "eqir"; break;
+    case Opcode::eqri: os << "eqri"; break;
+    case Opcode::eqrr: os << "eqrr"; break;
+    default: throw std::runtime_error("This should never happen."); break;
+  }
+
+  return os;
+}
+
+template<typename T>
+std::ostream&
+operator<<(std::ostream& os, const std::vector<T>& values) {
+  os << "[";
+  for (auto it{values.begin()}; it != values.end(); ++it) {
+    os << *it;
+    if (std::next(it) != values.end()) {
+      os << ", ";
+    } else {
+      os << "]";
+    }
+  }
+
+  return os;
+}
+
+
 static const std::array<Opcode, 16> ALL_OPCODES = {
   Opcode::addr,
   Opcode::addi,
@@ -41,16 +92,17 @@ static const std::array<Opcode, 16> ALL_OPCODES = {
   Opcode::bori,
   Opcode::setr,
   Opcode::seti,
-  Opcode::gtir,
-  Opcode::gtri,
-  Opcode::gtrr,
   Opcode::eqir,
   Opcode::eqri,
-  Opcode::eqrr
+  Opcode::eqrr,
+  Opcode::gtir,
+  Opcode::gtri,
+  Opcode::gtrr
 };
 
 
-std::pair<Mode, Mode> get_modes(Opcode opcode) {
+std::pair<Mode, Mode>
+get_modes(Opcode opcode) {
   switch (opcode) {
     case Opcode::addr: return {Mode::reg, Mode::reg}; break;
     case Opcode::addi: return {Mode::reg, Mode::val}; break;
@@ -61,7 +113,7 @@ std::pair<Mode, Mode> get_modes(Opcode opcode) {
     case Opcode::borr: return {Mode::reg, Mode::reg}; break;
     case Opcode::bori: return {Mode::reg, Mode::val}; break;
     case Opcode::setr: return {Mode::reg, Mode::reg}; break;
-    case Opcode::seti: return {Mode::reg, Mode::val}; break;
+    case Opcode::seti: return {Mode::val, Mode::val}; break;
     case Opcode::gtir: return {Mode::val, Mode::reg}; break;
     case Opcode::gtri: return {Mode::reg, Mode::val}; break;
     case Opcode::gtrr: return {Mode::reg, Mode::reg}; break;
@@ -72,10 +124,21 @@ std::pair<Mode, Mode> get_modes(Opcode opcode) {
   }
 }
 
+std::pair<uint, uint>
+get_values(const Opcode& opcode, const std::vector<uint>& instruction, std::vector<uint>* registers) {
+  auto modes = get_modes(opcode);
+  std::pair<uint, uint> values;
+  values.first = modes.first == Mode::reg ? registers->operator[](instruction[1]) : instruction[1];
+  values.second = modes.second == Mode::reg ? registers->operator[](instruction[2]) : instruction[2];
+
+  return values;
+}
+
+
 struct Example {
-  std::array<int, 4> registers_before;
-  std::array<int, 4> instruction;
-  std::array<int, 4> registers_after;
+  std::vector<uint> registers_before;
+  std::vector<uint> instruction;
+  std::vector<uint> registers_after;
 
   friend std::ostream&
   operator<<(std::ostream& os, const Example& example) {
@@ -110,7 +173,7 @@ struct Example {
       if (std::next(it) != example.registers_after.end()) {
         os << ", ";
       } else {
-        os << "]\n";
+        os << "]";
       }
     }
 
@@ -121,65 +184,91 @@ struct Example {
 
 void
 run_opcode(const Opcode opcode,
-           const std::array<int, 3>& instruction,
-           std::vector<int>* registers) {
-  int a = instruction[0];
-  int b = instruction[1];
-  int c = instruction[2];
+           const std::vector<uint>& instruction,
+           std::vector<uint>* registers) {
+  auto c = instruction[3];
+  auto [val_a, val_b] = get_values(opcode, instruction, registers);
+  DBGVAR(std::cerr, opcode);
+  DBGVAR(std::cerr, instruction);
+  DBGVAR(std::cerr, *registers);
+  DBGVAR(std::cerr, val_a);
+  DBGVAR(std::cerr, val_b);
   switch (opcode) {
   case Opcode::addr:
     // addr (add register) stores into register C the result of adding register A and register B.
-    registers->operator[](c) = registers->operator[](a) + registers->operator[](b);
+    registers->operator[](c) = val_a + val_b;
     break;
   case Opcode::addi:
     // addi (add immediate) stores into register C the result of adding register A and value B.
-    registers->operator[](c) = registers->operator[](a) + b;
+    registers->operator[](c) = val_a + val_b;
     break;
   case Opcode::mulr:
     // mulr (multiply register) stores into register C the result of multiplying register A and register B.
-    registers->operator[](c) = registers->operator[](a) * registers->operator[](b);
+    registers->operator[](c) = val_a * val_b;
     break;
   case Opcode::muli:
     // muli (multiply immediate) stores into register C the result of multiplying register A and value B.
-    registers->operator[](c) = registers->operator[](a) * b;
+    registers->operator[](c) = val_a * val_b;
     break;
   case Opcode::banr:
     // banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
-    registers->operator[](c) = registers->operator[](a) & registers->operator[](b);
+    registers->operator[](c) = val_a & val_b;
     break;
   case Opcode::bani:
     // bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
-    registers->operator[](c) = registers->operator[](a) & b;
+    registers->operator[](c) = val_a & val_b;
     break;
   case Opcode::borr:
     // borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
-    registers->operator[](c) = registers->operator[](a) | registers->operator[](b);
+    registers->operator[](c) = val_a | val_b;
     break;
   case Opcode::bori:
     // bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
-    registers->operator[](c) = registers->operator[](a) | b;
+    registers->operator[](c) = val_a | val_b;
     break;
   case Opcode::setr:
     // setr (set register) copies the contents of register A into register C. (Input B is ignored.)
-    registers->operator[](c) = registers->operator[](a);
+    registers->operator[](c) = val_a;
     break;
   case Opcode::seti:
     // seti (set immediate) stores value A into register C. (Input B is ignored.)
-    registers->operator[](c) = a;
+    registers->operator[](c) = val_a;
     break;
   case Opcode::gtir:
     // gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B.
     // Otherwise, register C is set to 0.
-    registers->operator[](c) = static_cast<int>(a > registers->operator[](b));
+    registers->operator[](c) = static_cast<uint>(val_a > val_b);
     break;
   case Opcode::gtri:
     // gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B.
     // Otherwise, register C is set to 0.
-    registers->operator[](c) = static_cast<int>(registers->operator[](a) > b);
+    registers->operator[](c) = static_cast<uint>(val_a > val_b);
+    break;
+  case Opcode::gtrr:
+    registers->operator[](c) = static_cast<uint>(val_a > val_b);
+    break;
+  case Opcode::eqir:
+    // eqir (equal immediate/register) sets register C to 1 if value A is equal to register B.
+    // Otherwise, register C is set to 0.
+    registers->operator[](c) = static_cast<uint>(val_a == val_b);
+    break;
+  case Opcode::eqri:
+    // eqri (equal register/immediate) sets register C to 1 if register A is equal to value B.
+    // Otherwise, register C is set to 0.
+    registers->operator[](c) = static_cast<uint>(val_a == val_b);
+    break;
+  case Opcode::eqrr:
+    // eqrr (equal register/register) sets register C to 1 if register A is equal to register B.
+    // Otherwise, register C is set to 0.
+    registers->operator[](c) = static_cast<uint>(val_a == val_b);
     break;
   default:
+    std::stringstream ss;
+    ss << "Encountered unknown opcode: " << opcode;
+    throw std::runtime_error(ss.str());
     break;
   }
+  DBGVAR(std::cerr, *registers);
 }
 
 
@@ -189,21 +278,26 @@ matching_opcodes(const Example& example) {
 
   for (auto opcode : ALL_OPCODES) {
     auto registers = example.registers_before;
+    run_opcode(opcode, example.instruction, &registers);
+    if (registers == example.registers_after) {
+      matches.push_back(opcode);
+    }
   }
 
   return matches;
 }
 
 
-std::array<int, 4> extract_registers(std::string input) {
+std::vector<uint>
+extract_registers(std::string input) {
   utils::replace_all_substrings(&input, ", ", ",");
   utils::replace_all_substrings(&input, "]", "");
   auto input_vec = utils::split_string(input, ',');
   assert(input_vec.size() == 4);
 
-  std::array<int, 4> result;
+  std::vector<uint> result;
   for (size_t index{0}; index < 4; index++) {
-    result[index] = std::stoi(input_vec[index]);
+    result.push_back(std::stoi(input_vec[index]));
   }
 
   return result;
@@ -238,17 +332,23 @@ extract_examples(const std::vector<std::string>& input) {
 auto
 part_one(const std::vector<std::string>& input) {
   auto examples = extract_examples(input);
+  size_t result{0};
   for (auto example : examples) {
-    std::cout << example << std::endl;
+    DBGVAR(std::cerr, example);
+    auto opcodes = matching_opcodes(example);
+    DBGVAR(std::cerr, opcodes);
+    if (opcodes.size() >= 3) {
+      result++;
+    }
   }
-  return 0;
+  return result;
 }
 
 
-auto
-part_two(const std::vector<std::string>& input) {
-  return -1;
-}
+// auto
+// part_two(const std::vector<std::string>& input) {
+//   return -1;
+// }
 
 
 int main() {
@@ -257,8 +357,8 @@ int main() {
 
   auto answer_one =  part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
-  auto answer_two =  part_two(input);
-  std::cout << "The answer to part two is: " << answer_two << std::endl;
+  // auto answer_two =  part_two(input);
+  // std::cout << "The answer to part two is: " << answer_two << std::endl;
 
   return 0;
 }
