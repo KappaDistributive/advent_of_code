50e7e625d768ced89e277294352879b0953d9c06
Stefan Mesken <kappadistributive@gmail.com>
2021-05-24 19:24:18 +0200

    linting

--- utils/hexgrid.cpp
+++ utils/hexgrid.cpp
@@ -4,6 +4,7 @@
 namespace utils {
 namespace hexgrid {
 
+
 std::ostream&
 operator<<(std::ostream& os, const Direction& direction) {
 switch (direction) {
@@ -18,13 +19,16 @@ switch (direction) {
 return os;
 }
 
+
 Point::Point(const int& x, const int& y)
   : m_x(x), m_y(y) {
 }
 
+
 Point::Point(const Point& point)
   : m_x(point.m_x), m_y(point.m_y) {
 }
+
 
 size_t
 Point::distance(const Point& destination) const {
@@ -38,31 +42,33 @@ if ((delta_x > 0) == (delta_y > 0)) {
 }
 }
 
+
 void
 Point::step(const Direction& direction) {
-switch (direction) {
-  case Direction::kNorthWest:
-    this->m_x--;
-    break;
-  case Direction::kNorth:
-    this->m_y++;
-    break;
-  case Direction::kNorthEast:
-    this->m_x++;
-    this->m_y++;
-    break;
-  case Direction::kSouthEast:
-    this->m_x++;
-    break;
-  case Direction::kSouth:
-    this->m_y--;
-    break;
-  case Direction::kSouthWest:
-    this->m_x--;
-    this->m_y--;
-    break;
+  switch (direction) {
+    case Direction::kNorthWest:
+      this->m_x--;
+      break;
+    case Direction::kNorth:
+      this->m_y++;
+      break;
+    case Direction::kNorthEast:
+      this->m_x++;
+      this->m_y++;
+      break;
+    case Direction::kSouthEast:
+      this->m_x++;
+      break;
+    case Direction::kSouth:
+      this->m_y--;
+      break;
+    case Direction::kSouthWest:
+      this->m_x--;
+      this->m_y--;
+      break;
+  }
 }
-}
+
 
 Point&
 Point::operator+=(const Point& summand) {
@@ -72,10 +78,12 @@ this->m_y += summand.m_y;
 return *this;
 }
 
+
 bool
 operator==(const Point& lhs, const Point& rhs) {
 return lhs.m_x == rhs.m_x && lhs.m_y == rhs.m_y;
 }
+
 
 Point
 operator+(const Point& lhs, const Point& rhs) {
@@ -84,7 +92,9 @@ Point point(lhs.m_x + rhs.m_x, lhs.m_y + rhs.m_y);
 return point;
 }
 
-std::ostream& operator<<(std::ostream& os, const Point& point) {
+
+std::ostream&
+operator<<(std::ostream& os, const Point& point) {
 os << "(" << point.m_x << ", " << point.m_y << ")";
 return os;
 }
