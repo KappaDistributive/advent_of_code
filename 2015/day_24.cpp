c8631e4a2c3266a549a0e1f3b5d38c173ce8bfb3
Stefan Mesken <kappadistributive@gmail.com>
2021-05-30 10:11:05 +0200

    solve 2015_24

--- 2015/day_24.cpp
+++ 2015/day_24.cpp
@@ -23,7 +23,8 @@ prepare_input(const std::vector<std::string>& input) {
 bool
 is_valid(const std::vector<size_t>& candidate,
          const std::vector<size_t>& weights,
-         size_t target_weight) {
+         size_t target_weight,
+         bool part_two = false) {
   std::vector<size_t> remaining_weights;
   for (auto weight : weights) {
     if (std::find(candidate.begin(), candidate.end(), weight) == candidate.end()) {
@@ -32,9 +33,29 @@ is_valid(const std::vector<size_t>& candidate,
   }
 
   Powerset powerset{remaining_weights};
+  if (!part_two) {
+    for (auto subvector : powerset) {
+      if (std::accumulate(subvector.begin(), subvector.end(), size_t{0}) == target_weight) {
+          return true;
+      }
+    }
+    return false;
+  }
+
   for (auto subvector : powerset) {
     if (std::accumulate(subvector.begin(), subvector.end(), size_t{0}) == target_weight) {
-      return true;
+      std::vector<size_t> inner_remaining_weights;
+      for (auto weight : remaining_weights) {
+        if (std::find(subvector.begin(), subvector.end(), weight) == subvector.end()) {
+          inner_remaining_weights.push_back(weight);
+        }
+      }
+      Powerset inner_powerset{inner_remaining_weights};
+      for (auto inner_subvector : inner_powerset) {
+        if (std::accumulate(inner_subvector.begin(), inner_subvector.end(), size_t{0}) == target_weight) {
+          return true;
+        }
+      }
     }
   }
   return false;
@@ -67,7 +88,6 @@ part_one(const std::vector<std::string>& input) {
       } else {
         candidates.insert(subvector);
       }
-      std::cout << max_size << ": " << candidates.size() << std::endl;
     }
   }
 
@@ -86,13 +106,42 @@ part_one(const std::vector<std::string>& input) {
 }
 
 
-// auto
-// part_two(const std::vector<std::string>& input) {
-//   CPU cpu(input);
-//   cpu.set_memory('a', 1);
-//   cpu.run();
-//   return cpu.get_memory().at('b');
-// }
+auto
+part_two(const std::vector<std::string>& input) {
+  auto weights = prepare_input(input);
+  size_t target_weight = std::accumulate(weights.begin(), weights.end(), size_t{0}) / 4;
+  Powerset powerset(weights);
+  std::set<std::vector<size_t>> candidates;
+  size_t max_size{weights.size()};
+
+  for (auto subvector : powerset) {
+    if (subvector.size() > max_size ||
+        std::accumulate(subvector.begin(), subvector.end(), size_t{0}) != target_weight) {
+      continue;
+    }
+    if (is_valid(subvector, weights, target_weight, true)) {
+      if (subvector.size() < max_size) {
+        candidates = std::set<std::vector<size_t>>{{subvector}};
+        max_size = subvector.size();
+      } else {
+        candidates.insert(subvector);
+      }
+    }
+  }
+
+  size_t min_entanglement{quantum_entanglement(weights)};
+  for (auto candidate : candidates) {
+    if (candidate.size() > max_size) {
+      continue;
+    }
+    auto entanglement = quantum_entanglement(candidate);
+    if (entanglement < min_entanglement) {
+      min_entanglement = entanglement;
+    }
+  }
+
+  return min_entanglement;
+}
 
 
 int
@@ -100,10 +149,12 @@ main() {
   utils::Reader reader(std::filesystem::path("../2015/data/input_24.txt"));
   const auto input = reader.get_lines();
 
+  std::cout << "This will take a few minutes..." << std::endl;
   auto answer_one = part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
-  // auto answer_two = part_two(input);
-  // std::cout << "The answer to part two is: " << answer_two << std::endl;
+  std::cout << "Bear with me..." << std::endl;
+  auto answer_two = part_two(input);
+  std::cout << "The answer to part two is: " << answer_two << std::endl;
 
   return 0;
 }
