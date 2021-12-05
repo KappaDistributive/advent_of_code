a11cd7818694326821cf5c3e9299640176248387
Stefan Mesken <kappadistributive@gmail.com>
2021-12-05 10:08:50 +0100

    [2021_05][cpp] solve part two

--- 2021/day_05.cpp
+++ 2021/day_05.cpp
@@ -19,31 +19,25 @@ auto prepare_input(const std::vector<std::string>& input, bool no_diagonals) {
       if (no_diagonals && x_0 != x_1 && y_0 != y_1) {
         continue;
       }
+      size_t num_steps = std::max(std::abs(x_0 - x_1), std::abs(y_0 - y_1)) + 1;
+      int x_step{0}, y_step{0};
+      if (x_0 < x_1) {
+        x_step = 1;
+      } else if (x_1 < x_0) {
+        x_step = -1;
+      }
+      if (y_0 < y_1) {
+        y_step = 1;
+      } else if (y_1 < y_0) {
+        y_step = -1;
+      }
 
-      if (y_0 <= y_1) {
-        for (auto y{y_0}; y <= y_1; ++y) {
-          if (x_0 <= x_1) {
-            for (auto x{x_0}; x <= x_1; ++x) {
-              map[y * width + x] += 1;
-            }
-          } else {
-            for (auto x{x_0}; x >= x_1; --x) {
-              map[y * width + x] += 1;
-            }
-          }
-        }
-      } else {
-        for (auto y{y_0}; y >= y_1; --y) {
-          if (x_0 <= x_1) {
-            for (auto x{x_0}; x <= x_1; ++x) {
-              map[y * width + x] += 1;
-            }
-          } else {
-            for (auto x{x_0}; x >= x_1; --x) {
-              map[y * width + x] += 1;
-            }
-          }
-        }
+      int x = x_0;
+      int y = y_0;
+      for (size_t step{0}; step < num_steps; ++step) {
+        map[y * width + x] += 1;
+        x += x_step;
+        y += y_step;
       }
     }
   }
@@ -60,9 +54,14 @@ auto part_one(const std::vector<std::string>& input) {
   return result;
 }
 
-// auto part_two(const std::vector<std::string>& input) {
-//   return 1;
-// }
+auto part_two(const std::vector<std::string>& input) {
+  auto map = prepare_input(input, false);
+  size_t result{0};
+  for (auto marker : map) {
+    result += marker > 1;
+  }
+  return result;
+}
 
 int main() {
   // std::filesystem::path input_path{"../2021/data/input_05_mock.txt"};
@@ -73,8 +72,8 @@ int main() {
   auto answer_one = part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
 
-  // auto answer_two = part_two(input);
-  // std::cout << "The answer to part two is: " << answer_two << std::endl;
+  auto answer_two = part_two(input);
+  std::cout << "The answer to part two is: " << answer_two << std::endl;
 
   return 0;
 }
