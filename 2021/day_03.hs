9a6dcd6c5828a773a992061553eadd04aa9424be
Stefan Mesken <kappadistributive@gmail.com>
2021-12-05 15:56:18 +0100

    [2021_03][hs] solve part two

--- 2021/day_03.hs
+++ 2021/day_03.hs
@@ -29,9 +29,49 @@ gamma x = decodeBinary $ map (\n -> if 2 * n > length x then 1 else 0) (countAll
 epislon :: [[Bool]] -> Int
 epislon x = decodeBinary $ map (\n -> if 2 * n < length x then 1 else 0) (countAllOnes x)
  
+digitsCounts :: Int -> [[Bool]] -> (Int, Int)
+digitsCounts n x = (length x - countOnes n x, countOnes n x)
+
+mostCommonDigit n x d = if uncurry (==) counts then d else uncurry (<) counts
+  where counts = digitsCounts n x
+
+leastCommonDigit n x d = if uncurry (==) counts then d else uncurry (>) counts
+  where counts = digitsCounts n x
+
+oxygenGeneratorRating' :: Int -> [[Bool]] -> (Int, [[Bool]], Bool)
+oxygenGeneratorRating' pos x = do
+  (pos + 1, filter (\d -> (d !! pos) == digit) x , digit)
+  where
+    digit = mostCommonDigit pos x True
+
+oxygenGeneratorRating :: Int -> [[Bool]] -> [Bool]
+oxygenGeneratorRating pos x = if pos == length (head x) then [] else digit : oxygenGeneratorRating next_pos output
+  where
+    (next_pos, output, digit) = oxygenGeneratorRating' pos x
+
+scrubberRating' :: Int -> [[Bool]] -> (Int, [[Bool]], Bool)
+scrubberRating' pos x = do
+  (pos + 1, filter (\d -> (d !! pos) == digit) x , digit)
+  where
+    digit = leastCommonDigit pos x False
+
+scrubberRating :: Int -> [[Bool]] -> [Bool]
+scrubberRating pos x
+  | pos == length (head x) = []
+  | length x == 1 = head x !! pos : scrubberRating next_pos x
+  | otherwise = digit : scrubberRating next_pos output
+  where (next_pos, output, digit) = scrubberRating' pos x
+
+
+partOne :: [[Bool]] -> Int
 partOne x = gamma x * epislon x
+
+partTwo:: [[Bool]] -> Int
+partTwo x = (* ) (decodeBinary $ map fromEnum (oxygenGeneratorRating 0 x)) (decodeBinary $ map fromEnum (scrubberRating 0 x))
+
 
 main = do
   contents <- readFile "../2021/data/input_03.txt"
   let input = parse contents
   print $ partOne input
+  print $ partTwo input
