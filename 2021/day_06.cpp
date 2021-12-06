571fadc3fbe76ac5f5bef5cdc902007953b421d5
Stefan Mesken <kappadistributive@gmail.com>
2021-12-06 06:44:40 +0100

    [2021_06][cpp] solve part two

--- 2021/day_06.cpp
+++ 2021/day_06.cpp
@@ -1,33 +1,27 @@
+#include <cassert>
+
 #include "../utils/input.hpp"
 
-struct Lanternfish {
-  int counter;
-};
-
 auto prepare_input(const std::vector<std::string>& input) {
-  std::vector<Lanternfish> fish;
+  std::array<size_t, 9> fish{0};
+
   for (auto timer : input) {
-    fish.push_back(Lanternfish{std::stoi(timer)});
+    auto counter = std::stoi(timer);
+    assert(0 <= counter && counter <= 8);
+    ++fish[counter];
   }
   return fish;
 }
 
-auto step(const std::vector<Lanternfish>& fish) {
-  std::vector<Lanternfish> new_fish;
-  std::vector<Lanternfish> children;
-
-  for (auto f : fish) {
-    if (f.counter == 0) {
-      new_fish.push_back(Lanternfish{6});
-      children.push_back(
-          Lanternfish{8});
+auto step(const std::array<size_t, 9>& fish) {
+  std::array<size_t, 9> new_fish{0};
+  for (size_t counter{0}; counter < 9; ++counter) {
+    if (counter == 0) {
+      new_fish[8] += fish[0];
+      new_fish[6] += fish[0];
     } else {
-      new_fish.push_back(Lanternfish{f.counter - 1});
+      new_fish[counter - 1] += fish[counter];
     }
-  }
-
-  for (auto child : children) {
-    new_fish.push_back(child);
   }
 
   return new_fish;
@@ -37,16 +31,17 @@ auto part_one(const std::vector<std::string>& input) {
   auto fish = prepare_input(input);
   for (size_t index{0}; index < 80; ++index) {
     fish = step(fish);
-    // for (auto f : fish) {
-    //   std::cout << f.counter << ", ";
-    // }
-    // std::cout << std::endl;
   }
-  return fish.size();
+  return std::accumulate(fish.begin(), fish.end(), 0ull);
 }
 
-// auto part_two(const std::vector<std::string>& input) {
-// }
+auto part_two(const std::vector<std::string>& input) {
+  auto fish = prepare_input(input);
+  for (size_t index{0}; index < 256; ++index) {
+    fish = step(fish);
+  }
+  return std::accumulate(fish.begin(), fish.end(), 0ull);
+}
 
 int main() {
   // std::filesystem::path input_path{"../2021/data/input_06_mock.txt"};
@@ -57,8 +52,8 @@ int main() {
   auto answer_one = part_one(input);
   std::cout << "The answer to part one is: " << answer_one << std::endl;
 
-  // auto answer_two = part_two(input);
-  // std::cout << "The answer to part two is: " << answer_two << std::endl;
+  auto answer_two = part_two(input);
+  std::cout << "The answer to part two is: " << answer_two << std::endl;
 
   return 0;
 }
