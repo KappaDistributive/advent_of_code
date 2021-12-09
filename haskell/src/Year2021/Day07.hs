33a5366a17e101b429840c3da6fb969b5a01afc8
Stefan Mesken <kappadistributive@gmail.com>
2021-12-09 09:46:30 +0100

    [2021_07][hs] solve part two

--- haskell/src/Year2021/Day07.hs
+++ haskell/src/Year2021/Day07.hs
@@ -6,18 +6,33 @@ import qualified Data.Text as T
 fuelCost :: Int -> Int -> Int
 fuelCost destination position = abs (destination - position)
 
+accumulatedFuelCost :: Int -> Int -> Int
+accumulatedFuelCost destination position = fst $ (n * (n + 1)) `divMod` 2
+  where
+    n = abs (destination - position)
+
 parse :: String -> [Int]
-parse x = map ((read) . T.unpack) $ T.splitOn (T.pack ",") (T.pack x)
+parse x = map (read . T.unpack) $ T.splitOn (T.pack ",") (T.pack x)
 
 partOne :: [Int] -> Int
 partOne x =
-  snd . head $
-  L.sortBy
+  snd $
+  L.minimumBy
     (\(_, a) (_, b) -> compare a b)
     [ (destination, sum $ map (fuelCost destination) x)
+    | destination <- [(minimum x) .. (maximum x)]
+    ]
+
+partTwo :: [Int] -> Int
+partTwo x =
+  snd $
+  L.minimumBy
+    (\(_, a) (_, b) -> compare a b)
+    [ (destination, sum $ map (accumulatedFuelCost destination) x)
     | destination <- [(minimum x) .. (maximum x)]
     ]
 
 run contents = do
   let input = parse contents
   print $ partOne input
+  print $ partTwo input
