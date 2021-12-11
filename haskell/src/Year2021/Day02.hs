58c3fbb81afb89af63dedfed58021c06e0522546
Stefan Mesken <kappadistributive@gmail.com>
2021-12-11 09:46:04 +0100

    [2021_02][hs] clean up

--- haskell/src/Year2021/Day02.hs
+++ haskell/src/Year2021/Day02.hs
@@ -9,30 +9,35 @@ parse' :: [String] -> Maybe (String, Int)
 parse' [x, y] = Just (x, readInt y)
 parse' _ = Nothing
 
-skipNothing = filter
-
 parse :: String -> [(String, Int)]
 parse x = mapMaybe (parse' . words) (lines x)
 
+horizontal :: (String, Int) -> Int
 horizontal (x, y) =
   if x == "forward"
     then y
     else 0
 
+depth :: (String, Int) -> Int
 depth (x, y) =
   case x of
     "down" -> y
     "up" -> -y
     _ -> 0
 
+aim :: [(String, Int)] -> [Int]
 aim x = scanl1 (+) (map depth x)
 
+multiply :: (Int, Int) -> Int
 multiply (x, y) = x * y
 
+depthTwo :: [(String, Int)] -> [Int]
 depthTwo x = zipWith (curry multiply) (map horizontal x) (aim x)
 
+partOne :: [(String, Int)] -> Int
 partOne x = (*) (sum $ map horizontal x) (sum $ map depth x)
 
+partTwo :: [(String, Int)] -> Int
 partTwo x = (*) (sum $ map horizontal x) ((sum . depthTwo) x)
 
 run contents = do
