a2d27a286c14f052a478bdf6cb40249f46847b9d
Stefan Mesken <kappadistributive@gmail.com>
2021-12-14 11:57:40 +0100

    [2021_12][cpp] solve part two

--- cpp/src/2021/day_12.cpp
+++ cpp/src/2021/day_12.cpp
@@ -2,17 +2,22 @@
 
 class Map {
  private:
-  const size_t m_curiosity;
+  const bool m_allow_revisit;
   std::set<std::pair<std::string, std::string>> m_adjacency_list;
 
  public:
-  explicit Map(const std::vector<std::string>& input, size_t curiosity = 1)
-      : m_curiosity(curiosity) {
+  explicit Map(const std::vector<std::string>& input,
+               bool allow_revisit = false)
+      : m_allow_revisit(allow_revisit) {
     for (auto line : input) {
       auto splits = utils::split_string(line, '-');
-      this->m_adjacency_list.insert(std::make_pair(splits[0], splits[1]));
-      this->m_adjacency_list.insert(
-          std::make_pair(splits[1], splits[0]));  // undirected graph
+      if (splits[0] != "end" && splits[1] != "start") {
+        this->m_adjacency_list.insert(std::make_pair(splits[0], splits[1]));
+      }
+      if (splits[0] != "start" && splits[1] != "end") {
+        this->m_adjacency_list.insert(
+            std::make_pair(splits[1], splits[0]));  // undirected graph
+      }
     }
   }
 
@@ -21,13 +26,26 @@ class Map {
     if (std::isupper(node[0])) {
       return true;
     }
-    size_t num_visits{0};
+
+    std::map<std::string, size_t> counts;
+    size_t max_visits{1};
     for (auto position : path) {
-      if (position == node) {
-        ++num_visits;
+      if (std::isupper(position[0])) {
+        continue;
+      } else if (counts.count(position) == 0) {
+        counts.insert(std::make_pair(position, 1));
+      } else {
+        ++counts.at(position);
       }
+      max_visits = std::max(max_visits, counts.at(position));
     }
-    return num_visits < this->m_curiosity;
+
+    if (max_visits > 1) {
+      return counts.count(node) == 0;
+    }
+
+    return counts.count(node) == 0 ||
+           (this->m_allow_revisit && counts.at(node) == 1);
   }
 
   std::set<std::vector<std::string>> paths(std::string source,
@@ -42,8 +60,8 @@ class Map {
 
       for (auto path : current_paths) {
         for (auto [s, d] : this->m_adjacency_list) {
-          auto new_path = path;
           if (s == path.back() && this->may_visit(path, d)) {
+            auto new_path = path;
             new_path.push_back(d);
             if (current_paths.count(new_path) == 0) {
               searching = true;
@@ -62,6 +80,8 @@ class Map {
           }
         }
       }
+
+      std::cout << final_paths.size() << std::endl;
     }
 
     return final_paths;
@@ -78,21 +98,24 @@ class Map {
 auto part_one(const std::vector<std::string>& input) {
   Map map(input);
   // std::cout << map << std::endl;
-
   auto all_paths = map.paths("start", "end");
 
   return all_paths.size();
 }
 
-// auto part_two(const std::vector<std::string>& input) {
-//   return 2;
-// }
+auto part_two(const std::vector<std::string>& input) {
+  Map map(input, true);
+  // std::cout << map << std::endl;
+  auto all_paths = map.paths("start", "end");
+
+  return all_paths.size();
+}
 
 int main(int argc, char* argv[]) {
   std::string extension{""};
 
-  if (argc > 1 && std::string(argv[1]) == "mock") {
-    extension = "_mock";
+  if (argc > 1) {
+    extension = "_" + std::string(argv[1]);
   }
   std::filesystem::path input_path{
       fmt::format("../../data/2021/input_12{}.txt", extension)};
@@ -101,8 +124,8 @@ int main(int argc, char* argv[]) {
 
   auto answer_one = part_one(input);
   fmt::print("The answer to part one is: {}\n", answer_one);
-  // auto answer_two = part_two(input);
-  // fmt::print("The answer to part two is: {}\n", answer_two);
+  auto answer_two = part_two(input);
+  fmt::print("The answer to part two is: {}\n", answer_two);
 
   return 0;
 }
