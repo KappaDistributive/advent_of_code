82ba234e7754d9017878d0516460ed672d482b67
Stefan Mesken <kappadistributive@gmail.com>
2021-12-25 22:23:03 +0100

    [2016_04][hs] improve formatting

--- haskell/src/Year2016/Day04.hs
+++ haskell/src/Year2016/Day04.hs
@@ -1,8 +1,8 @@
 module Year2016.Day04 where
 
-import Data.Map (fromListWith, toList)
 import Data.Char (chr, ord)
 import Data.List (sortBy)
+import Data.Map (fromListWith, toList)
 import Data.Ord (Ordering(..))
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
@@ -17,17 +17,17 @@ data Room =
 roomRegex = "^([a-z\\-]+)\\-([0-9]+)\\[([a-z]+)\\]$"
 
 compareFunction :: (Char, Int) -> (Char, Int) -> Ordering
-compareFunction (a,b) (c, d) 
+compareFunction (a, b) (c, d)
   | (a == c) && (b == d) = EQ
   | b > d = LT
   | (b == d) && (a < c) = LT
   | otherwise = GT
 
 checksum :: Room -> String
-checksum (Room name _ _ ) = take 5 $ map fst frequencies
+checksum (Room name _ _) = take 5 $ map fst frequencies
   where
-  letters = filter (\x -> x `elem` ['a'..'z']) name
-  frequencies = sortBy compareFunction $ frequency letters
+    letters = filter (\x -> x `elem` ['a' .. 'z']) name
+    frequencies = sortBy compareFunction $ frequency letters
 
 char2int :: Char -> Int
 char2int x = ord x - ord 'a'
@@ -56,11 +56,20 @@ parse :: [String] -> Maybe [Room]
 parse = mapM parseRoom
 
 partOne :: Maybe [Room] -> Maybe Int
-partOne (Just rooms) = Just $ sum $ map (\(_,_,a) -> a) $ filter (\(a,b,_) -> a == b) $ map (\room@(Room _ sector_id c) -> (c, checksum room, sector_id)) rooms
+partOne (Just rooms) =
+  Just $
+  sum $
+  map (\(_, _, a) -> a) $
+  filter (\(a, b, _) -> a == b) $
+  map (\room@(Room _ sector_id c) -> (c, checksum room, sector_id)) rooms
 partOne Nothing = Nothing
 
 partTwo :: Maybe [Room] -> Maybe Int
-partTwo (Just rooms) = Just $ snd . head $ filter (\ (a, b) -> a == "northpole-object-storage") $ map (\(Room name sectorId _) -> (decrypt sectorId name, sectorId)) $ rooms
+partTwo (Just rooms) =
+  Just $
+  snd . head $
+  filter (\(a, b) -> a == "northpole-object-storage") $
+  map (\(Room name sectorId _) -> (decrypt sectorId name, sectorId)) $ rooms
 partTwo Nothing = Nothing
 
 run :: String -> IO ()
