c1153644a5b1721d45a65211d4993f72c1f01954
Stefan Mesken <kappadistributive@gmail.com>
2022-01-07 18:41:31 +0100

    [2019_01][hs] solve part two

--- haskell/src/Year2019/Day01.hs
+++ haskell/src/Year2019/Day01.hs
@@ -2,9 +2,20 @@ module Year2019.Day01
   ( run
   ) where
 
+fuel :: Bool -> Int -> Int
+fuel False mass = max 0 ((mass `div` 3) - 2)
+fuel True 0 = 0
+fuel True mass = value + fuel True value
+  where
+    value = max 0 ((mass `div` 3) - 2)
+
 partOne :: [Int] -> Int
-partOne modules = sum $ map (\x -> max 0 ((x `div` 3) - 2)) modules
+partOne modules = sum $ map (fuel False) modules
+
+partTwo :: [Int] -> Int
+partTwo modules = sum $ map (fuel True) modules
 
 run contents = do
   let input = map (read :: String -> Int) $ lines contents
   print $ partOne input
+  print $ partTwo input
