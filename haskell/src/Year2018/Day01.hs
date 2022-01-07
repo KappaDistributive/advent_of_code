c3ec716290348f6df7a7b78515e6e378885a3cf2
Stefan Mesken <kappadistributive@gmail.com>
2022-01-07 17:47:01 +0100

    [2018_01][hs] solve part two

--- haskell/src/Year2018/Day01.hs
+++ haskell/src/Year2018/Day01.hs
@@ -8,14 +8,32 @@ parse :: String -> [Int]
 parse contents =
   map (read . unpack . replace (pack "+") (pack "") . pack) $ lines contents
 
+rotate :: [a] -> [a]
+rotate [] = []
+rotate (x:xs) = xs ++ [x]
+
 partOne' :: [Int] -> [Int] -> Int -> Int
 partOne' [] _ frequency = frequency
 partOne' changes frequencies frequency =
   partOne' (tail changes) (frequency : frequencies) (frequency + head changes)
 
+partTwo' :: [Int] -> [Int] -> Int -> Int
+partTwo' [] _ frequency = frequency
+partTwo' changes frequencies frequency =
+  if frequency `elem` frequencies
+    then frequency
+    else partTwo'
+           (rotate changes)
+           (frequency : frequencies)
+           (frequency + head changes)
+
 partOne :: [Int] -> Int
 partOne input = partOne' input [] 0
+
+partTwo :: [Int] -> Int
+partTwo input = partTwo' input [] 0
 
 run contents = do
   let input = parse contents
   print $ partOne input
+  print $ partTwo input
