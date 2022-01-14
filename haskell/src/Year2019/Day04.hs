d6c6025b263a3f7171296e2354bbd39416f46b32
Stefan Mesken <kappadistributive@gmail.com>
2022-01-14 16:29:59 +0100

    [2019_04][hs] solve part two

--- haskell/src/Year2019/Day04.hs
+++ haskell/src/Year2019/Day04.hs
@@ -18,19 +18,43 @@ isIncreasing number = L.sort digits == digits
   where
     digits = show number
 
-hasPair :: Int -> Bool
-hasPair number = any (uncurry (==)) pairs
+chains' :: String -> String -> [String]
+chains' (x:xs) buffer =
+  if null buffer || x == head buffer
+    then chains' xs (x : buffer)
+    else buffer : chains' xs [x]
+chains' [] buffer = [buffer]
+
+chains :: String -> [String]
+chains word = chains' word []
+
+chainLengths :: String -> [Int]
+chainLengths word = map length $ chains word
+
+hasPair :: Bool -> Int -> Bool
+hasPair False number = any (uncurry (==)) pairs
   where
     digits = show number
     pairs = zip digits (tail digits)
+hasPair True number = 2 `elem` chainLengths digits
+  where
+    digits = show number
 
 partOne :: (Int, Int) -> Int
 partOne (lower, upper) =
   length $
   filter
     (== True)
-    [hasPair number && isIncreasing number | number <- [lower .. upper]]
+    [hasPair False number && isIncreasing number | number <- [lower .. upper]]
+
+partTwo :: (Int, Int) -> Int
+partTwo (lower, upper) =
+  length $
+  filter
+    (== True)
+    [hasPair True number && isIncreasing number | number <- [lower .. upper]]
 
 run contents = do
   let input = parse contents
   print $ partOne input
+  print $ partTwo input
