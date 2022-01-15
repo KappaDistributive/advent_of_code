afb4b9f3c278ad594cd2a503262ea40dfb6f4871
Stefan Mesken <kappadistributive@gmail.com>
2022-01-15 19:55:52 +0100

    [2020_01][hs] solve part two

--- haskell/src/Year2020/Day01.hs
+++ haskell/src/Year2020/Day01.hs
@@ -10,6 +10,10 @@ parse contents = map (read :: String -> Int) $ lines contents
 pairs :: [Int] -> [(Int, Int)]
 pairs numbers = [(x, y) | (x:ys) <- L.tails numbers, y <- ys]
 
+triples :: [Int] -> [(Int, Int, Int)]
+triples numbers =
+  [(x, y, z) | (x:ys) <- L.tails numbers, (y:zs) <- L.tails ys, z <- zs]
+
 partOne :: [(Int, Int)] -> Maybe Int
 partOne (x:xs) =
   if a + b == 2020
@@ -19,6 +23,16 @@ partOne (x:xs) =
     (a, b) = x
 partOne [] = Nothing
 
+partTwo :: [(Int, Int, Int)] -> Maybe Int
+partTwo (x:xs) =
+  if a + b + c == 2020
+    then Just (a * b * c)
+    else partTwo xs
+  where
+    (a, b, c) = x
+partTwo [] = Nothing
+
 run contents = do
   let input = parse contents
   print $ partOne $ pairs input
+  print $ partTwo $ triples input
