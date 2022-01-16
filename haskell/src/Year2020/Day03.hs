ae21d9c5301a20dd5125ebfc811d6db09d14b715
Stefan Mesken <kappadistributive@gmail.com>
2022-01-16 09:01:56 +0100

    [2020_03][hs] solve part two

--- haskell/src/Year2020/Day03.hs
+++ haskell/src/Year2020/Day03.hs
@@ -17,11 +17,20 @@ trace' map vel_x vel_y x y =
 trace :: [String] -> Int -> Int -> String
 trace map vel_x vel_y = trace' map vel_x vel_y 0 0
 
+numTrees :: [String] -> Int -> Int -> Int
+numTrees m vel_x vel_y = length $ filter (== '#') $ trace m vel_x vel_y
+
 partOne :: [String] -> Int
-partOne map = length $ filter (== '#') path
-  where
-    path = trace map 3 1
+partOne m = numTrees m 3 1
+
+partTwo :: [String] -> Int
+partTwo m =
+  foldl
+    (*)
+    1
+    [numTrees m (fst x) (snd x) | x <- [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]
 
 run contents = do
   let input = parse contents
   print $ partOne input
+  print $ partTwo input
