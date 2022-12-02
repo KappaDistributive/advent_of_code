1fd01b0499bd087dd9f7506e09e5c950242e5066
Stefan Mesken <kappadistributive@gmail.com>
2022-12-02 18:06:49 +0100

    [2022_01][cpp] solve part two

--- cpp/src/2022/day_01.cpp
+++ cpp/src/2022/day_01.cpp
@@ -1,6 +1,6 @@
-#include "../utils/input.hpp"
-
 #include <algorithm>
+
+#include "../utils/input.hpp"
 
 auto prepare_input(const std::vector<std::string>& input) {
   std::vector<std::vector<int>> result;
@@ -36,22 +36,25 @@ auto part_one(const std::vector<std::vector<int>>& input) {
   return answer;
 }
 
-// auto part_two(const std::vector<std::vector<int>>& input) {
-//   for (auto line : input) {
-//     fmt::print("{}\n", line);
-//   }
-// 
-//   return 2;
-// }
+auto part_two(const std::vector<std::vector<int>>& input) {
+  std::vector<int> calories;
+  for (const auto& inventory: input) {
+    calories.push_back(std::accumulate(inventory.begin(), inventory.end(), 0));
+  }
+
+  std::sort(calories.begin(), calories.end(), [](int a, int b) { return a > b; });
+
+  return std::accumulate(calories.begin(), calories.begin() + 3, 0);
+}
 
 int main() {
-  // std::filesystem::path input_path{"../../data/2021/input_01_mock.txt"};
+  // std::filesystem::path input_path{"../../data/2022/input_01_mock.txt"};
   std::filesystem::path input_path{"../../data/2022/input_01.txt"};
   utils::Reader reader(input_path);
   auto input = prepare_input(reader.get_lines());
 
   fmt::print("The answer to part one is: {}\n", part_one(input));
-  // fmt::print("The answer to part two is: {}\n", part_two(input));
+  fmt::print("The answer to part two is: {}\n", part_two(input));
 
   return 0;
 }
