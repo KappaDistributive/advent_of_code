c2e195b22240f8226b034e0b5ba05112e930f1ff
Stefan Mesken <kappadistributive@gmail.com>
2022-12-12 21:20:29 +0100

    [2022_02][hs] solve part two

--- haskell/src/Year2022/Day02.hs
+++ haskell/src/Year2022/Day02.hs
@@ -1,4 +1,7 @@
-module Year2022.Day02 ( run ) where
+module Year2022.Day02
+  ( run
+  ) where
+
 import Text.Read (readMaybe)
 
 reveal :: Char -> Char
@@ -12,10 +15,9 @@ reveal _ = '_'
 
 parse :: String -> [(Char, Char)]
 parse [] = []
-parse x = map (\y -> (reveal . head $ y, reveal . last $ y )) xs
+parse x = map (\y -> (reveal . head $ y, reveal . last $ y)) xs
   where
     xs = lines x
-
 
 value :: Char -> Int
 value 'R' = 1
@@ -23,6 +25,17 @@ value 'P' = 2
 value 'S' = 3
 value _ = 0
 
+undo :: (Char, Char) -> Char
+undo ('R', 'R') = 'S'
+undo ('R', 'P') = 'R'
+undo ('R', 'S') = 'P'
+undo ('P', 'R') = 'R'
+undo ('P', 'P') = 'P'
+undo ('P', 'S') = 'S'
+undo ('S', 'R') = 'P'
+undo ('S', 'P') = 'S'
+undo ('S', 'S') = 'R'
+undo _ = '_'
 
 score :: (Char, Char) -> Int
 score ('R', 'R') = 3
@@ -37,9 +50,11 @@ score ('S', 'S') = 3
 score _ = -1
 
 partOne :: [(Char, Char)] -> Int
-partOne moves = sum $ map (\(a, b) -> a + b) $ zip (map score moves) (map (value . snd) moves)
+partOne moves =
+  sum $ map (\(a, b) -> a + b) $ zip (map score moves) (map (value . snd) moves)
 
-partTwo _ = 2
+partTwo :: [(Char, Char)] -> Int
+partTwo moves = partOne $ map (\(a, b) -> (a, undo (a, b))) moves
 
 run contents = do
   let input = parse contents
