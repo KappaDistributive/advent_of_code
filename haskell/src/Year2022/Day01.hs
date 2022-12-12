d1813a90db55cfa634594b3a310b7ed41df90998
Stefan Mesken <kappadistributive@gmail.com>
2022-12-12 21:20:49 +0100

    [2022_01][hs] clean up

--- haskell/src/Year2022/Day01.hs
+++ haskell/src/Year2022/Day01.hs
@@ -1,23 +1,26 @@
 module Year2022.Day01
   ( run
   ) where
+
 import qualified Data.List as L
 import qualified Data.Text as T
 import Text.Read (readMaybe)
 
 parse :: String -> Maybe [[Int]]
-parse x = mapM sequence calories where
-    groups = filter (/=[]) $ map (lines . T.unpack) $ T.splitOn (T.pack "\n\n") $ T.pack x
-    calories = [ [readMaybe a | a <- group ] | group <- groups ]
+parse x = mapM sequence calories
+  where
+    groups =
+      filter (/= []) $
+      map (lines . T.unpack) $ T.splitOn (T.pack "\n\n") $ T.pack x
+    calories = [[readMaybe a | a <- group] | group <- groups]
 
 partOne :: Maybe [[Int]] -> Int
 partOne (Just groups) = head . reverse . L.sort $ map sum groups
 partOne Nothing = -1
 
-partTwo:: Maybe [[Int]] -> Int
+partTwo :: Maybe [[Int]] -> Int
 partTwo (Just groups) = sum . (take 3) . reverse . L.sort $ map sum groups
 partTwo Nothing = -1
-
 
 run contents = do
   let input = parse contents
