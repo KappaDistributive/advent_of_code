52c0958a22c177ff2e9c6830596b6cfcf1a47c9a
Stefan Mesken <kappadistributive@gmail.com>
2022-12-16 12:57:09 +0100

    [2022_15][cpp] solve part two

--- cpp/src/2022/day_15.cpp
+++ cpp/src/2022/day_15.cpp
@@ -26,6 +26,41 @@ struct Sensor {
            this->position.manhatten_distance(this->closest_beacon);
   }
 
+  std::vector<Point> border() const {
+    int radius{this->position.manhatten_distance(this->closest_beacon)};
+    Point pos{{this->position[0], this->position[1] - (radius + 1)}};
+    std::vector<Point> result;
+    for (int step{0}; step < radius + 1; ++step) {
+      pos += Point{{1, 1}};
+      result.push_back(pos);
+    }
+    assert(pos[1] == this->position[1]);
+    for (int step{0}; step < radius + 1; ++step) {
+      pos += Point{{-1, 1}};
+      result.push_back(pos);
+    }
+    assert(pos[0] == this->position[0]);
+    for (int step{0}; step < radius + 1; ++step) {
+      pos += Point{{-1, -1}};
+      result.push_back(pos);
+    }
+    assert(pos[1] == this->position[1]);
+    for (int step{0}; step < radius + 1; ++step) {
+      pos += Point{{1, -1}};
+      result.push_back(pos);
+    }
+    assert(pos[0] == this->position[0]);
+    assert(pos[1] == this->position[1] - radius + 1);
+
+    auto center = this->position;
+    assert(std::all_of(result.cbegin(), result.cend(),
+                       [center, radius](const Point pos) {
+                         return pos.manhatten_distance(center) == radius + 1;
+                       }));
+
+    return result;
+  }
+
   friend std::ostream &operator<<(std::ostream &os, const Sensor &sensor) {
     os << fmt::format("Sensor at x={}, y={}: closest beacon is at x={}, y={}",
                       sensor.position[0], sensor.position[1],
@@ -34,15 +69,31 @@ struct Sensor {
   }
 };
 
-auto part_one(const std::vector<std::string> &input) {
+auto parse_input(const std::vector<std::string> &input) {
   std::vector<Sensor> sensors;
   for (const auto &line : input) {
     sensors.push_back(Sensor{line});
   }
 
+  return sensors;
+}
+
+auto part_one(const std::vector<Sensor> &sensors) {
+  int min_x{std::numeric_limits<int>::max()},
+      max_x{std::numeric_limits<int>::min()};
+  for (const auto &sensor : sensors) {
+    min_x = std::min(
+        min_x, sensor.position[0] -
+                   sensor.position.manhatten_distance(sensor.closest_beacon) -
+                   1);
+    max_x = std::max(
+        max_x, sensor.position[0] +
+                   sensor.position.manhatten_distance(sensor.closest_beacon) +
+                   1);
+  }
   Point pos{{0, 2000000}};
   size_t result{0};
-  for (int x{-100000000}; x < 100000000; ++x) {
+  for (int x{min_x}; x <= max_x; ++x) {
     pos[0] = x;
     for (const auto &sensor : sensors) {
       if (pos != sensor.closest_beacon && sensor.in_reach(pos)) {
@@ -54,13 +105,27 @@ auto part_one(const std::vector<std::string> &input) {
   return result;
 }
 
-auto part_two(const std::vector<std::string> &input) { return 2; }
+auto part_two(const std::vector<Sensor> &sensors) {
+  for (const auto &sensor : sensors) {
+    for (const auto &pos : sensor.border()) {
+      if (pos[0] < 0 || pos[0] > 4000000 || pos[1] < 0 || pos[1] > 4000000) {
+        continue;
+      }
+      if (!std::any_of(sensors.cbegin(), sensors.cend(),
+                       [&pos](const Sensor s) { return s.in_reach(pos); })) {
+        return static_cast<int64_t>(pos[0]) * int64_t{4000000} +
+               static_cast<int64_t>(pos[1]);
+      }
+    }
+  }
+  return int64_t{-1};
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2022/input_15_mock.txt"};
   std::filesystem::path input_path{"../../data/2022/input_15.txt"};
   utils::Reader reader(input_path);
-  auto input = reader.get_lines();
+  auto input = parse_input(reader.get_lines());
 
   fmt::print("The answer to part one is: {}\n", part_one(input));
   fmt::print("The answer to part two is: {}\n", part_two(input));
