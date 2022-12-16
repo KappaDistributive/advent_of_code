d3714f85303c9eeda6f2a12f6b2845a4e87e8109
Stefan Mesken <kappadistributive@gmail.com>
2022-12-16 19:29:17 +0100

    [2022_16][cpp] solve part two

--- cpp/src/2022/day_16.cpp
+++ cpp/src/2022/day_16.cpp
@@ -1,5 +1,6 @@
 #include <regex>
 
+#include "../utils/combinatorics.hpp"
 #include "../utils/input.hpp"
 
 static const std::regex valve_regex(
@@ -54,11 +55,11 @@ struct Network {
 };
 
 size_t best_flow(const std::vector<std::string> path, const Network &network,
-                 size_t time_left) {
+                 size_t time_left, const std::set<std::string> &allowed_nodes) {
   size_t max_flow{0};
 
   for (auto node : network.valves) {
-    if (network.flows.at(node) == 0 ||
+    if (allowed_nodes.count(node) == 0 || network.flows.at(node) == 0 ||
         std::find(path.cbegin(), path.cend(), node) != path.cend()) {
       continue;
     }
@@ -70,25 +71,71 @@ size_t best_flow(const std::vector<std::string> path, const Network &network,
         time_left - network.distances.at(std::make_pair(path.back(), node)) - 1;
     auto new_path = path;
     new_path.push_back(node);
-    max_flow = std::max(max_flow, network.flows.at(node) * time +
-                                      best_flow(new_path, network, time));
+    max_flow = std::max(max_flow,
+                        network.flows.at(node) * time +
+                            best_flow(new_path, network, time, allowed_nodes));
   }
 
   return max_flow;
 }
 
-auto part_one(const std::vector<std::string> &input) {
-  Network network(input);
-  return best_flow({"AA"}, network, 30);
+auto parse_input(const std::vector<std::string> &input) {
+  return Network(input);
 }
 
-auto part_two(const std::vector<std::string> &input) { return 1; }
+auto part_one(const Network &network) {
+  std::set<std::string> allowed_nodes;
+  for (auto node : network.valves) {
+    allowed_nodes.insert(node);
+  }
+  return best_flow({"AA"}, network, 30, allowed_nodes);
+}
+
+auto part_two(const Network &network) {
+  std::vector<std::string> key_nodes;
+  for (auto node : network.valves) {
+    if (node == "AA" || network.flows.at(node) > 0) {
+      key_nodes.push_back(node);
+    }
+  }
+  utils::combinatorics::Powerset<std::string> subsets(key_nodes);
+  std::map<std::set<std::string>, size_t> scores;
+  for (auto subset : subsets) {
+    std::set<std::string> nodes;
+    for (auto node : subset) {
+      nodes.insert(node);
+    }
+    nodes.insert("AA");
+    if (scores.count(nodes) == 0) {
+      scores.emplace(
+          std::make_pair(nodes, best_flow({"AA"}, network, 26, nodes)));
+    }
+  }
+
+  size_t result{0};
+  for (auto [used_nodes, score] : scores) {
+    std::set<std::string> remaining_nodes;
+    remaining_nodes.insert("AA");
+    for (auto node : key_nodes) {
+      if (used_nodes.count(node) == 0) {
+        remaining_nodes.insert(node);
+      }
+    }
+    result = std::max(result, score + scores.at(remaining_nodes));
+  }
+  return result;
+}
+
+auto part_two(const std::vector<std::string> &input) {
+  std::map<std::vector<std::string>, size_t> best_flows;
+  return 1;
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2022/input_16_mock.txt"};
   std::filesystem::path input_path{"../../data/2022/input_16.txt"};
   utils::Reader reader(input_path);
-  auto input = reader.get_lines();
+  auto input = parse_input(reader.get_lines());
 
   fmt::print("The answer to part one is: {}\n", part_one(input));
   fmt::print("The answer to part two is: {}\n", part_two(input));
