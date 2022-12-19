15a1087325f193b69042769e3b13a1b2da05e754
Stefan Mesken <kappadistributive@gmail.com>
2022-12-19 10:17:55 +0100

    [2022_17][cpp] solve part two

--- cpp/src/2022/day_17.cpp
+++ cpp/src/2022/day_17.cpp
@@ -92,7 +92,8 @@ struct Chamber {
     return true;
   }
 
-  void step() {
+  bool step() {
+    bool new_rock{false};
     if (this->tick % 2 == 0) {
       // jet of gas pushes rock
       Point push{{1, 0}};
@@ -115,30 +116,18 @@ struct Chamber {
           this->max_y = std::max(this->max_y, unit[1]);
         }
         this->current_rock =
-            Rock(this->settled_rocks.size() % 5, Point{{2, max_y + 4}});
+            Rock(this->settled_rocks.size() % 5, Point{{2, this->max_y + 4}});
+        new_rock = true;
       }
     }
     ++this->tick;
+    return new_rock;
   }
 
   std::string str() const {
-    int max_y{0};
-    for (const auto &unit : this->current_rock.units()) {
-      max_y = std::max(max_y, unit[1]);
-    }
-    std::set<Point> units;
-    for (const auto &rock : this->settled_rocks) {
-      for (auto unit : rock.units()) {
-        units.insert(unit);
-      }
-    }
-    for (const auto &unit : units) {
-      max_y = std::max(max_y, unit[1]);
-    }
-
     std::string result{};
     Point pos{{0, 0}};
-    for (int y{max_y}; y >= -1; --y) {
+    for (int y{this->max_y}; y >= -1; --y) {
       pos[1] = y;
       for (int x{-1}; x < 8; ++x) {
         pos[0] = x;
@@ -149,7 +138,7 @@ struct Chamber {
         } else if (this->current_rock.units().count(pos) > 0) {
           result.push_back('@');
         } else {
-          result.push_back(units.count(Point{{x, y}}) > 0 ? '#' : '.');
+          result.push_back(this->occupied.count(Point{{x, y}}) > 0 ? '#' : '.');
         }
       }
       result.push_back('\n');
@@ -174,7 +163,59 @@ auto part_one(const std::string &input) {
   return result + 1;
 }
 
-auto part_two(const std::string &input) { return 2; }
+auto part_two(const std::string &input) {
+  Chamber chamber{input};
+  std::vector<int> height_gains{};
+  int max_y{0};
+  while (chamber.settled_rocks.size() < 10000) {
+    if (chamber.step()) {
+      height_gains.push_back(chamber.max_y - max_y);
+      max_y = chamber.max_y;
+    }
+  }
+  const size_t total_length{1000000000000};
+
+  // find cycle
+  size_t best_offset{0};
+  size_t best_period{std::numeric_limits<size_t>::max()};
+  for (size_t offset{0}; offset < 1000; ++offset) {
+    for (size_t period{1}; period < height_gains.size() / 4; ++period) {
+      bool match{true};
+      for (size_t t{1};
+           match && offset + t * period < height_gains.size() - period; ++t) {
+        for (size_t i{0}; i < period; ++i) {
+          if (height_gains[offset + i] !=
+              height_gains[offset + t * period + i]) {
+            match = false;
+            break;
+          }
+        }
+      }
+      if (match && period < best_period) {
+        best_offset = offset;
+        best_period = period;
+      }
+    }
+  }
+
+  size_t result{0};
+  for (size_t index{0}; index < best_offset; ++index) {
+    result += height_gains[index];
+  }
+
+  size_t period_sum{0};
+  for (size_t index{0}; index < best_period; ++index) {
+    period_sum += height_gains[best_offset + index];
+  }
+
+  result += ((total_length - best_offset) / best_period) * period_sum;
+  for (size_t index{0}; index < (total_length - best_offset) % best_period;
+       ++index) {
+    result += height_gains[best_offset + index];
+  }
+
+  return result + 1;
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2022/input_17_mock.txt"};
