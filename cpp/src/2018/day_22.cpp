b1984e64c35542540903dc5d1695421cde5d9a30
Stefan Mesken <kappadistributive@gmail.com>
2023-04-03 21:45:42 +0200

    [2018_22][cpp] parse input

--- cpp/src/2018/day_22.cpp
+++ cpp/src/2018/day_22.cpp
@@ -1,20 +1,25 @@
-#include "../utils/input.hpp"
 #include "../utils/geometry.hpp"
+#include "../utils/input.hpp"
 
 using Point = utils::geometry::Point<size_t, 2>;
 
 class Cave {
- private:
-  Point target;
+private:
+  size_t m_depth;
+  Point m_target;
 
- public:
-
-  Cave(const std::vector<std::string>& input) {
-
+public:
+  Cave(const std::vector<std::string> &input) {
+    std::string temp;
+    this->m_depth = std::stoull(input[0].substr(7, std::string::npos));
+    auto splits = utils::split_string(input[1], ',');
+    assert(splits.size() == 2);
+    this->m_target = Point{{std::stoull(splits[0].substr(8, std::string::npos)),
+                            std::stoull(splits[1])}};
   }
 
-  size_t geological_index(const Point& position) {
-    if ((position[0] == 0 && position[1] == 0) || position == this->target) {
+  size_t geological_index(const Point &position) {
+    if ((position[0] == 0 && position[1] == 0) || position == this->m_target) {
       return 0;
     }
     if (position[1] == 0) {
@@ -24,24 +29,25 @@ class Cave {
       return position[1] * 48271;
     }
 
-    Point lhs{{position[0] - 1, position[1]}}, rhs{{position[0], position[1] - 1}};
+    Point lhs{{position[0] - 1, position[1]}},
+        rhs{{position[0], position[1] - 1}};
 
     return geological_index(lhs) * geological_index(rhs);
-
   }
 
+  friend std::ostream &operator<<(std::ostream &os, const Cave &cave) {
+    os << cave.m_depth << '\t' << cave.m_target;
+    return os;
+  }
 };
 
 auto part_one(const std::vector<std::string> &input) {
-  for (auto& line : input) {
-    fmt::print("{}\n", line);
-  }
+  Cave cave{input};
+  std::cout << cave << std::endl;
   return 1;
 }
 
-auto part_two(const std::vector<std::string> &input) {
-  return 2;
-}
+auto part_two(const std::vector<std::string> &input) { return 2; }
 
 int main() {
   std::filesystem::path input_path{"../../data/2018/input_22_mock.txt"};
