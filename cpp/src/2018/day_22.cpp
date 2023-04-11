b3e904f6518e4697a4536569c332ccdb12bd2096
Stefan Mesken <kappadistributive@gmail.com>
2023-04-11 18:30:01 +0200

    [2018_22][cpp] refactor

--- cpp/src/2018/day_22.cpp
+++ cpp/src/2018/day_22.cpp
@@ -1,6 +1,6 @@
+#include "../../vendors/BigInt/release/BigInt.hpp"
 #include "../utils/geometry.hpp"
 #include "../utils/input.hpp"
-#include "../../vendors/BigInt/release/BigInt.hpp"
 
 using Point = utils::geometry::Point<size_t, 2>;
 
@@ -12,7 +12,28 @@ Point extract_target(const std::vector<std::string> &input) {
   auto splits = utils::split_string(input[1], ',');
   assert(splits.size() == 2);
   return Point{{std::stoull(splits[0].substr(8, std::string::npos)),
-                          std::stoull(splits[1])}};
+                std::stoull(splits[1])}};
+}
+
+enum class GroundType {
+  Rocky,
+  Wet,
+  Narrow,
+};
+
+std::ostream &operator<<(std::ostream &os, const GroundType &ground_type) {
+  switch (ground_type) {
+  case GroundType::Rocky:
+    os << '.';
+    break;
+  case GroundType::Wet:
+    os << '=';
+    break;
+  case GroundType::Narrow:
+    os << '=';
+    break;
+  }
+  return os;
 }
 
 class Cave {
@@ -23,8 +44,9 @@ private:
   std::map<Point, size_t> m_geological_indeces;
 
 public:
-  Cave(const std::vector<std::string> &input, size_t divisor = 20183) : m_divisor(divisor), m_depth(extract_depth(input)), m_target(extract_target(input)) {
-      }
+  Cave(const std::vector<std::string> &input, size_t divisor = 20183)
+      : m_divisor(divisor), m_depth(extract_depth(input)),
+        m_target(extract_target(input)) {}
 
   size_t geological_index(const Point &position) {
     if (this->m_geological_indeces.count(position)) {
@@ -50,18 +72,44 @@ public:
     return (geological_index(position) + this->m_depth) % this->m_divisor;
   }
 
+  GroundType ground_type(const Point &position) {
+    size_t ground_value = this->erosion_level(position) % 3;
+
+    switch (ground_value) {
+    case 0:
+      return GroundType::Rocky;
+      break;
+    case 1:
+      return GroundType::Wet;
+      break;
+    case 2:
+      return GroundType::Narrow;
+      break;
+    default:
+      throw std::runtime_error("This should never happen!");
+      break;
+    }
+  }
+
   size_t risk_level() {
     Point pos{{0, 0}};
     size_t result{0};
     for (size_t y{0}; y <= this->m_target.coordinates()[1]; ++y) {
       for (size_t x{0}; x <= this->m_target.coordinates()[0]; ++x) {
         pos = Point{{x, y}};
-        auto ground_type = this->erosion_level(pos) % 3;
+        auto ground_type = this->ground_type(pos);
         switch (ground_type) {
-          case 0: break;
-          case 1: ++result; break;
-          case 2: result += 2; break;
-          default: throw std::runtime_error("This should never happen!"); break;
+        case GroundType::Rocky:
+          break;
+        case GroundType::Wet:
+          ++result;
+          break;
+        case GroundType::Narrow:
+          result += 2;
+          break;
+        default:
+          throw std::runtime_error("This should never happen!");
+          break;
         }
       }
     }
@@ -73,13 +121,7 @@ public:
     for (size_t y{0}; y <= cave.m_target.coordinates()[1]; ++y) {
       for (size_t x{0}; x <= cave.m_target.coordinates()[0]; ++x) {
         pos = Point{{x, y}};
-        auto ground_type = cave.erosion_level(pos) % 3;
-        switch (ground_type) {
-          case 0: os << '.'; break;
-          case 1: os << '='; break;
-          case 2: os << '|'; break;
-          default: throw std::runtime_error("This should never happen!"); break;
-        }
+        os << cave.ground_type(pos);
       }
       os << '\n';
     }
@@ -100,7 +142,6 @@ int main() {
   std::filesystem::path input_path{"../../data/2018/input_22.txt"};
   utils::Reader reader(input_path);
   auto input = reader.get_lines();
-
 
   fmt::print("The answer to part one is: {}\n", part_one(input));
   fmt::print("The answer to part two is: {}\n", part_two(input));
