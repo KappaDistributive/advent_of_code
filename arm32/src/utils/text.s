493e2dd73a33eb9836b7a7adad0840309d182cb6
Stefan Mesken <kappadistributive@gmail.com>
2023-06-26 18:23:22 +0200

    [arm32][utils] implement my_parse_next_number

--- arm32/src/utils/text.s
+++ arm32/src/utils/text.s
@@ -1,5 +1,40 @@
 .text
 .global my_strlen
+.global my_parse_next_number
+
+my_parse_next_number:
+/*
+input:
+    r0 = address of 8 byte output buffer.
+    r1 = address to first byte of null-terminated string.
+
+return:
+    [r0] = value of next number; 0 if none was found.
+    [r0, #4] = address of first byte after next number in input.
+
+remarks:
+    - Only supports non-negative integers < 2**32.
+*/
+    push {r4}
+    mov r4, #10
+    mov r3, #0
+my_parse_next_number_loop:
+    ldrb r2, [r1]
+    cmp r2, #'0'
+    blt my_parse_next_number_end
+    cmp r2, #'9'
+    bgt my_parse_next_number
+    mul r3, r3, r4
+    add r3, r2
+    sub r3, #'0'
+    add r1, #1
+    b my_parse_next_number_loop
+
+my_parse_next_number_end:
+    str r3, [r0]
+    str r1, [r0, #4]
+    pop {r4}
+    bx lr
 
 my_strlen:
 /*
