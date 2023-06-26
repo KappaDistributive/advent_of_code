0055bc1c895d7e867ca81c823ffdbae15647419f
Stefan Mesken <kappadistributive@gmail.com>
2023-06-26 22:50:33 +0200

    [arm32] write tests for my_parse_next_number

---
+++ arm32/tests/text.s
@@ -0,0 +1,74 @@
+.section .rodata
+msg_success: .asciz "Test `%s`: ✓\n"
+msg_failure: .asciz "Test `%s`: ✘\n"
+
+name_my_parse_next_number_empty: .asciz "my_parse_next_number_empty"
+data_my_parse_next_number_empty: .asciz ""
+
+name_my_parse_next_number_simple: .asciz "my_parse_next_number_simple"
+data_my_parse_next_number_simple: .asciz "123"
+
+
+.balign 8
+.text
+.global main
+
+test_my_parse_next_number_empty:
+    push {r4, lr}
+    mov r4, #0
+    sub sp, #8
+    mov r0, sp
+    ldr r1, =data_my_parse_next_number_empty
+    bl my_parse_next_number
+    ldr r0, [sp]
+    add sp, #8
+    cmp r0, #0
+    ldreq r0, =msg_success
+    movne r4, #1
+    ldrne r0, =msg_failure
+    
+    ldr r1, =name_my_parse_next_number_empty
+    bl printf
+
+    mov r0, r4
+    pop {r4, lr}
+    bx lr
+
+test_my_parse_next_number_simple:
+    push {r4, lr}
+    mov r4, #0
+    sub sp, #8
+    mov r0, sp
+    ldr r1, =data_my_parse_next_number_simple
+    bl my_parse_next_number
+    ldr r0, [sp]
+    add sp, #8
+    cmp r0, #123
+    ldreq r0, =msg_success
+    movne r4, #1
+    ldrne r0, =msg_failure
+    
+    ldr r1, =name_my_parse_next_number_simple
+    bl printf
+
+    mov r0, r4
+    pop {r4, lr}
+    bx lr
+
+
+main:
+    push {r4, lr}
+    mov r4, #0
+
+    bl test_my_parse_next_number_empty
+    orr r4, r0
+
+    bl test_my_parse_next_number_simple
+    orr r4, r0
+
+    mov r0, r4
+    pop {r4, lr}
+    bx lr
+
+.global my_parse_next_number
+.global printf
