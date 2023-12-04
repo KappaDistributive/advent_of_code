f49651b97bd21bd4422b4fe18a9e4c2cb8c03244
Stefan Mesken <kappadistributive@gmail.com>
2023-12-04 21:58:13 +0100

    [2023_04][cpp] solve part two

--- cpp/src/2023/day_04.cpp
+++ cpp/src/2023/day_04.cpp
@@ -1,16 +1,17 @@
 #include "../utils/input.hpp"
 #include <algorithm>
+#include <numeric>
 #include <regex> //NOLINT
 #include <string>
 #include <vector>
 
 class Card {
- public:
+public:
   int id;
   std::vector<int> winners;
   std::vector<int> numbers;
 
-  Card(const std::string& description) {
+  Card(const std::string &description) {
     std::regex regex{"^Card\\s+(\\d+)\\:\\s([\\s\\d]+)\\s\\|\\s([\\s\\d]+)$"};
     std::smatch matches;
     std::regex_match(description, matches, regex);
@@ -24,10 +25,22 @@ class Card {
     }
   }
 
-  int worth() const {
+  int matching_numbers() const {
     int result{0};
     for (auto number : this->numbers) {
-      if (std::find(this->winners.cbegin(), this->winners.cend(), number) != this->winners.cend()) {
+      if (std::find(this->winners.cbegin(), this->winners.cend(), number) !=
+          this->winners.cend()) {
+        ++result;
+      }
+    }
+    return result;
+  }
+
+  int worth() const {
+    int result{this->matching_numbers()};
+    for (auto number : this->numbers) {
+      if (std::find(this->winners.cbegin(), this->winners.cend(), number) !=
+          this->winners.cend()) {
         ++result;
       }
     }
@@ -39,25 +52,44 @@ class Card {
   }
 };
 
-auto parse_input(const std::vector<std::string>& input) {
+auto parse_input(const std::vector<std::string> &input) {
   std::vector<Card> cards;
-  for (auto line: input) {
+  for (auto line : input) {
     cards.push_back(Card(line));
   }
 
   return cards;
 }
 
-auto part_one(const std::vector<Card>& cards) {
+auto part_one(const std::vector<Card> &cards) {
   int result{0};
-  for (const auto& card: cards) {
+  for (const auto &card : cards) {
     result += card.worth();
   }
   return result;
 }
 
-auto part_two() {
-  return 0;
+auto part_two(const std::vector<Card> &cards) {
+  std::vector<size_t> amount;
+  for (auto _ : cards) {
+    amount.push_back(1);
+  }
+  for (size_t index{0}; index < cards.size(); ++index) {
+    if (cards[index].matching_numbers() == 0) {
+      continue;
+    }
+    for (size_t offset{0};
+         offset < static_cast<size_t>(cards[index].matching_numbers());
+         ++offset) {
+      fmt::print("Increasing {} by {} from {}\n", index + 2 + offset,
+                 amount[index], index + 1);
+      amount[index + 1 + offset] += amount[index];
+    }
+  }
+  for (size_t index{0}; index < amount.size(); ++index) {
+    fmt::print("{}: {}\n", index + 1, amount[index]);
+  }
+  return std::accumulate(amount.cbegin(), amount.cend(), 0ull);
 }
 
 int main() {
@@ -67,7 +99,7 @@ int main() {
   auto cards = parse_input(reader.get_lines());
 
   fmt::print("The answer to part one is: {}\n", part_one(cards));
-  fmt::print("The answer to part two is: {}\n", part_two());
+  fmt::print("The answer to part two is: {}\n", part_two(cards));
 
   return 0;
 }
