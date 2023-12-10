14dc8f24e897e46233fe9eaf27895a6dc4c4974d
Stefan Mesken <kappadistributive@gmail.com>
2023-12-10 23:17:34 +0100

    [2023_06][cpp] solve part two

--- cpp/src/2023/day_06.cpp
+++ cpp/src/2023/day_06.cpp
@@ -1,27 +1,28 @@
 #include "../utils/input.hpp"
 #include <ostream>
 #include <regex>
+#include <string>
 #include <utility>
 #include <vector>
 
-std::vector<std::pair<int, int>> parse(const std::vector<std::string> &input) {
-  std::vector<int> time;
+auto parse(const std::vector<std::string> &input) {
+  std::vector<size_t> time;
   auto splits = utils::split_string(input[0], ' ');
   for (auto split : splits) {
     if (split.size() > 0 && split != "Time:")
-      time.push_back(std::stoi(split));
+      time.push_back(std::stoull(split));
   }
 
-  std::vector<int> distance;
+  std::vector<size_t> distance;
   splits = utils::split_string(input[1], ' ');
   for (auto split : splits) {
     if (split.size() > 0 && split != "Distance:")
-      distance.push_back(std::stoi(split));
+      distance.push_back(std::stoull(split));
   }
 
   assert(time.size() == distance.size());
 
-  std::vector<std::pair<int, int>> result;
+  std::vector<std::pair<size_t, size_t>> result;
 
   for (size_t index{0}; index < time.size(); ++index) {
     result.push_back(std::make_pair(time[index], distance[index]));
@@ -30,9 +31,9 @@ std::vector<std::pair<int, int>> parse(const std::vector<std::string> &input) {
   return result;
 }
 
-auto winning_combinations(int time, int distance) {
-  std::vector<int> charges;
-  for (int charge{0}; charge <= time; ++charge) {
+auto winning_combinations(size_t time, size_t distance) {
+  std::vector<size_t> charges;
+  for (size_t charge{0}; charge <= time; ++charge) {
     if ((time - charge) * charge > distance) {
       charges.push_back(charge);
     }
@@ -40,7 +41,7 @@ auto winning_combinations(int time, int distance) {
   return charges;
 }
 
-auto part_one(std::vector<std::pair<int, int>> &input) {
+auto part_one(std::vector<std::pair<size_t, size_t>> &input) {
   size_t result{1};
   for (auto [time, distance] : input) {
     result *= winning_combinations(time, distance).size();
@@ -49,7 +50,19 @@ auto part_one(std::vector<std::pair<int, int>> &input) {
   return result;
 }
 
-auto part_two(std::vector<std::pair<int, int>> &input) { return 1; }
+auto part_two(std::vector<std::pair<size_t, size_t>> &input) {
+  std::string time_buffer;
+  std::string distance_buffer;
+
+  for (auto [time, distance] : input) {
+    time_buffer += std::to_string(time);
+    distance_buffer += std::to_string(distance);
+  }
+  size_t time{std::stoull(time_buffer)};
+  size_t distance{std::stoull(distance_buffer)};
+
+  return winning_combinations(time, distance).size();
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2023/input_06_mock.txt"};
