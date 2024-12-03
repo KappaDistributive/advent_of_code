eded6783bd4807a00623ef83c3d6649362f125a6
Stefan Mesken <kappadistributive@gmail.com>
2024-12-03 08:46:47 +0100

    [2024_03][cpp] solve part one

---
+++ cpp/src/2024/day_03.cpp
@@ -0,0 +1,59 @@
+#include "../utils/input.hpp"
+
+std::pair<int, size_t> get_number(const std::string &line, size_t pos) {
+  int result{0};
+  for (size_t i{0}; i < 4; ++i) {
+    if (line[pos] >= '0' && line[pos] <= '9') {
+      result = result * 10 + (line[pos] - '0');
+    } else {
+      break;
+    }
+    ++pos;
+  }
+  return {result, pos};
+}
+
+auto part_one(const std::vector<std::string> &input) {
+  size_t result{0};
+  for (const auto &line : input) {
+    size_t pos{0};
+
+    while (true) {
+      pos = line.find("mul(", pos);
+      if (pos == std::string::npos || pos + 8 >= line.size()) {
+        break;
+      }
+      pos += 4;
+      auto left = get_number(line, pos);
+      if (left.second == pos || line[left.second] != ',') {
+        ++pos;
+        continue;
+      }
+      pos = left.second + 1;
+      auto right = get_number(line, pos);
+      if (right.second == pos || line[right.second] != ')') {
+        ++pos;
+        continue;
+      }
+      pos = right.second + 1;
+      result += left.first * right.first;
+    }
+  }
+
+  return result;
+}
+
+auto part_two() { return 2; }
+
+int main() {
+  // std::filesystem::path input_path{"../../data/2024/input_03_mock.txt"};
+  std::filesystem::path input_path{"../../data/2024/input_03.txt"};
+  utils::Reader reader(input_path);
+  auto input = reader.get_lines();
+
+  std::cout << std::format("The answer to part one is: {}", part_one(input))
+            << std::endl;
+  std::cout << std::format("The answer to part two is: {}", part_two())
+            << std::endl;
+  return 0;
+}
