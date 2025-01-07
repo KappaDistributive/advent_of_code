6ef60409d21526a9797714afe36cfda134ad38be
Stefan Mesken <kappadistributive@gmail.com>
2025-01-07 14:24:02 +0100

    [2024_12][cpp] solve part two

--- cpp/src/2024/day_12.cpp
+++ cpp/src/2024/day_12.cpp
@@ -15,7 +15,13 @@ public:
       : m_map(map), m_height(static_cast<int>(map.size())),
         m_width(static_cast<int>(map[0].size())) {}
 
-  char at(const Point &pos) const { return this->m_map[pos[1]][pos[0]]; }
+  char at(const Point &pos) const {
+    if (pos[0] < 0 || pos[0] >= this->m_width || pos[1] < 0 ||
+        pos[1] >= this->m_height) {
+      return ' ';
+    }
+    return this->m_map[pos[1]][pos[0]];
+  }
 
   int num_neighbors(const Point &pos) const {
     auto symbol = this->at(pos);
@@ -76,6 +82,77 @@ public:
     }
     return result;
   }
+
+  int interior_corners(const Point &pos) const {
+    int count{0};
+    // xX
+    // .x
+    if (this->at(pos) == this->at(pos + Direction::Left) &&
+        this->at(pos) == this->at(pos + Direction::Down) &&
+        this->at(pos) != this->at(pos + Direction::Left + Direction::Down)) {
+      ++count;
+    }
+    // Xx
+    // x.
+    if (this->at(pos) == this->at(pos + Direction::Down) &&
+        this->at(pos) == this->at(pos + Direction::Right) &&
+        this->at(pos) != this->at(pos + Direction::Down + Direction::Right)) {
+      ++count;
+    }
+    // x.
+    // Xx
+    if (this->at(pos) == this->at(pos + Direction::Up) &&
+        this->at(pos) == this->at(pos + Direction::Right) &&
+        this->at(pos) != this->at(pos + Direction::Up + Direction::Right)) {
+      ++count;
+    }
+    // .x
+    // xX
+    if (this->at(pos) == this->at(pos + Direction::Up) &&
+        this->at(pos) == this->at(pos + Direction::Left) &&
+        this->at(pos) != this->at(pos + Direction::Up + Direction::Left)) {
+      ++count;
+    }
+    return count;
+  }
+
+  int exterior_corners(const Point &pos) const {
+    int count{0};
+    // -+
+    //  |
+    if (this->at(pos) != this->at(pos + Direction::Left) &&
+        this->at(pos) != this->at(pos + Direction::Down)) {
+      ++count;
+    }
+    // +-
+    // |
+    if (this->at(pos) != this->at(pos + Direction::Down) &&
+        this->at(pos) != this->at(pos + Direction::Right)) {
+      ++count;
+    }
+    //  |
+    // -+
+    if (this->at(pos) != this->at(pos + Direction::Left) &&
+        this->at(pos) != this->at(pos + Direction::Up)) {
+      ++count;
+    }
+    // |
+    // +-
+    if (this->at(pos) != this->at(pos + Direction::Up) &&
+        this->at(pos) != this->at(pos + Direction::Right)) {
+      ++count;
+    }
+
+    return count;
+  }
+
+  int sides(const std::set<std::pair<Point, int>> &region) {
+    int count{0};
+    for (const auto &[point, _] : region) {
+      count += interior_corners(point) + exterior_corners(point);
+    }
+    return count;
+  }
 };
 
 int perimiter(const std::set<std::pair<Point, int>> &region) {
@@ -97,7 +174,16 @@ auto part_one(const std::vector<std::string> &input) {
   return result;
 }
 
-auto part_two() { return 2; }
+auto part_two(const std::vector<std::string> &input) {
+  Map map(input);
+  auto regions = map.regions();
+  int result{0};
+  for (const auto &region : regions) {
+    result += map.sides(region) * region.size();
+  }
+
+  return result;
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2024/input_12_mock.txt"};
@@ -107,7 +193,7 @@ int main() {
 
   std::cout << std::format("The answer to part one is: {}", part_one(input))
             << std::endl;
-  std::cout << std::format("The answer to part two is: {}", part_two())
+  std::cout << std::format("The answer to part two is: {}", part_two(input))
             << std::endl;
 
   return EXIT_SUCCESS;
