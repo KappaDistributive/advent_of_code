fa63c6eeb69e226605cb25dabb0b1e6a80e288e5
Stefan Mesken <kappadistributive@gmail.com>
2025-01-07 12:09:57 +0100

    [2024_12][cpp] solve part one

--- cpp/src/2024/day_12.cpp
+++ cpp/src/2024/day_12.cpp
@@ -1,32 +1,107 @@
+#include "../utils/geometry.hpp"
 #include "../utils/input.hpp"
 
+using Point = utils::geometry::Point<int, 2>;
+using utils::geometry::ALL_DIRECTIONS;
+using utils::geometry::Direction;
 
 class Map {
- private:
-  const std::vector<std::string>& m_map;
-  
- public:
-  Map (const std::vector<std::string>& map) : m_map(map) {}
+private:
+  const std::vector<std::string> &m_map;
+  int m_height, m_width;
 
-  char cat(int x, int y) {
-    return m_map[y][x];
+public:
+  Map(const std::vector<std::string> &map)
+      : m_map(map), m_height(static_cast<int>(map.size())),
+        m_width(static_cast<int>(map[0].size())) {}
+
+  char at(const Point &pos) const { return this->m_map[pos[1]][pos[0]]; }
+
+  int num_neighbors(const Point &pos) const {
+    auto symbol = this->at(pos);
+    int count{0};
+    for (auto direction : ALL_DIRECTIONS) {
+      if (this->at(pos + direction) == symbol) {
+        ++count;
+      }
+    }
+    return count;
+  }
+
+  int height() const { return this->m_height; }
+
+  int width() const { return this->m_width; }
+
+  std::set<Point> flood_fill(const Point &pos) const {
+    std::set<Point> visited;
+    std::queue<Point> queue;
+    queue.push(pos);
+    visited.insert(pos);
+    while (!queue.empty()) {
+      auto current = queue.front();
+      queue.pop();
+      for (auto direction : ALL_DIRECTIONS) {
+        auto neighbor = current + direction;
+        if (this->at(neighbor) == this->at(pos) &&
+            visited.find(neighbor) == visited.end()) {
+          queue.push(neighbor);
+          visited.insert(neighbor);
+        }
+      }
+    }
+    return visited;
+  }
+
+  std::vector<std::set<std::pair<Point, int>>> regions() const {
+    std::vector<std::set<std::pair<Point, int>>> regions;
+    std::set<Point> visited;
+    for (int y{0}; y < this->height(); ++y) {
+      for (int x{0}; x < this->width(); ++x) {
+        Point pos{{x, y}};
+        if (visited.find(pos) == visited.end()) {
+          auto region = this->flood_fill(pos);
+          regions.push_back(enhance_region(region));
+          visited.insert(region.begin(), region.end());
+        }
+      }
+    }
+    return regions;
+  }
+
+  std::set<std::pair<Point, int>>
+  enhance_region(const std::set<Point> &region) const {
+    std::set<std::pair<Point, int>> result;
+    for (const auto &point : region) {
+      result.insert({point, this->num_neighbors(point)});
+    }
+    return result;
   }
 };
 
-auto part_one(const std::vector<std::string>& input) {
-  for (const auto& line : input) {
-    std::cout << line << std::endl;
+int perimiter(const std::set<std::pair<Point, int>> &region) {
+  int count{0};
+  for (const auto &[point, neighbors] : region) {
+    count += 4 - neighbors;
   }
-  return 1;
+  return count;
 }
 
-auto part_two() {
-  return 2;
+auto part_one(const std::vector<std::string> &input) {
+  Map map(input);
+  auto regions = map.regions();
+  int result{0};
+  for (const auto &region : regions) {
+    result += perimiter(region) * region.size();
+  }
+
+  return result;
 }
+
+auto part_two() { return 2; }
 
 int main() {
-  std::filesystem::path input_path{"../../data/2024/input_12_mock.txt"};
-  // std::filesystem::path input_path{"../../data/2024/input_12.txt"};
+  // std::filesystem::path input_path{"../../data/2024/input_12_mock.txt"};
+  std::filesystem::path input_path{"../../data/2024/input_12.txt"};
   utils::Reader reader(input_path);
   auto input = reader.get_lines();
 
@@ -35,5 +110,5 @@ int main() {
   std::cout << std::format("The answer to part two is: {}", part_two())
             << std::endl;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
