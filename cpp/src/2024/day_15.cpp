10c93f67befdacfb7591c3da43ee0157b7872de0
Stefan Mesken <kappadistributive@gmail.com>
2025-03-16 21:32:25 +0100

    [2024_15][cpp] refactor

--- cpp/src/2024/day_15.cpp
+++ cpp/src/2024/day_15.cpp
@@ -6,9 +6,7 @@ using Direction = utils::geometry::Direction;
 
 class Grid {
 private:
-  Point m_robot;
-  std::vector<Point> m_walls;
-  std::vector<Point> m_boxes;
+  std::vector<std::string> m_grid;
   std::vector<Direction> m_instructions;
   size_t m_instruction_index;
 
@@ -22,15 +20,7 @@ public:
         ++index;
         break;
       }
-      for (int x{0}; x < static_cast<int>(line.size()); ++x) {
-        if (line[x] == '#') {
-          this->m_walls.push_back(Point{{x, index}});
-        } else if (line[x] == 'O') {
-          this->m_boxes.push_back(Point{{x, index}});
-        } else if (line[x] == '@') {
-          this->m_robot = Point{{x, index}};
-        }
-      }
+      this->m_grid.push_back(line);
     }
     // parse instructions
     for (; index < static_cast<int>(input.size()); ++index) {
@@ -63,70 +53,53 @@ public:
 
   int64_t score() const {
     int64_t result{0};
-    for (auto const &box : this->m_boxes) {
-      result += this->gps(box);
-    }
     return result;
+  }
+
+  Point robot() const {
+    for (int y{0}; y < static_cast<int>(this->m_grid.size()); ++y) {
+      for (int x{0}; x < static_cast<int>(this->m_grid[0].size()); ++x) {
+        if (this->m_grid[y][x] == '@') {
+          return Point{{x, y}};
+        }
+      }
+    }
+    throw std::runtime_error("Unable to find robot!");
   }
 
   bool step() {
     if (this->m_instruction_index >= this->m_instructions.size()) {
       return false;
     }
-    auto instruction = this->m_instructions[this->m_instruction_index];
-
-    auto new_pos = this->m_robot + instruction;
-    if (std::find(this->m_walls.cbegin(), this->m_walls.cend(), new_pos) !=
-        this->m_walls.cend()) {
-      // robot attempts to move into wall
-    } else if (std::find(this->m_boxes.cbegin(), this->m_boxes.cend(),
-                         new_pos) != this->m_boxes.cend()) {
-      // robot is moving into box
-      auto trail = new_pos;
-      while (std::find(this->m_boxes.cbegin(), this->m_boxes.cend(), trail) !=
-             this->m_boxes.cend()) {
-        trail += instruction;
-      }
-      if (std::find(this->m_walls.cbegin(), this->m_walls.cend(), trail) ==
-          this->m_walls.cend()) {
-        this->m_robot += instruction;
-        do {
-          trail -= instruction;
-          auto it =
-              std::find(this->m_boxes.begin(), this->m_boxes.end(), trail);
-          *it += instruction;
-        } while (trail != new_pos);
-      }
-    } else {
-      this->m_robot = new_pos;
+    auto instruction = this->m_instructions[this->m_instruction_index++];
+    auto robot = this->robot();
+    auto pos = robot + instruction;
+    if (this->at(pos) == '#') {
+      return true;
     }
-    ++this->m_instruction_index;
+    while (this->at(pos) == 'O') {
+      pos += instruction;
+    }
+    if (this->at(pos) == '#') {
+      return true;
+    }
+    this->m_grid[pos.coordinates()[1]][pos.coordinates()[0]] = 'O';
+    this->m_grid[robot.coordinates()[1]][robot.coordinates()[0]] = '.';
+    robot += instruction;
+    this->m_grid[robot.coordinates()[1]][robot.coordinates()[0]] = '@';
     return true;
   }
 
+  char at(const Point &pos) const {
+    auto coordinates = pos.coordinates();
+    return this->m_grid[coordinates[1]][coordinates[0]];
+  }
+
   friend std::ostream &operator<<(std::ostream &os, const Grid &grid) {
-    int max_x{0}, max_y{0};
-    for (const auto &point : grid.m_walls) {
-      auto coords = point.coordinates();
-      max_x = std::max(max_x, coords[0]);
-      max_y = std::max(max_y, coords[1]);
-    }
-    for (int y{0}; y <= max_y; ++y) {
-      for (int x{0}; x <= max_x; ++x) {
+    for (int y{0}; y < static_cast<int>(grid.m_grid.size()); ++y) {
+      for (int x{0}; x < static_cast<int>(grid.m_grid[0].size()); ++x) {
         Point pos{{x, y}};
-        if (grid.m_robot == pos) {
-          os << "\033[36m";
-          os << '@';
-          os << "\033[39m";
-        } else if (std::find(grid.m_walls.cbegin(), grid.m_walls.cend(), pos) !=
-                   grid.m_walls.cend()) {
-          os << '#';
-        } else if (std::find(grid.m_boxes.cbegin(), grid.m_boxes.cend(), pos) !=
-                   grid.m_boxes.cend()) {
-          os << 'O';
-        } else {
-          os << ' ';
-        }
+        os << grid.m_grid[y][x];
       }
       os << '\n';
     }
@@ -157,27 +130,9 @@ public:
   }
 };
 
-void parse(const std::vector<std::string> &data) {
-  size_t index{0};
-  // parse grid
-  std::cout << "Grid" << std::endl;
-  for (; index < data.size(); ++index) {
-    auto line = data[index];
-    std::cout << line << std::endl;
-    if (line.size() == 0) {
-      break;
-    }
-  }
-  // parse instructions
-  std::cout << "Instructions" << std::endl;
-  for (; index < data.size(); ++index) {
-    auto line = data[index];
-    std::cout << line << std::endl;
-  }
-}
-
 auto part_one(Grid grid) {
   do {
+    std::cout << grid << std::endl;
   } while (grid.step());
   return grid.score();
 }
@@ -185,8 +140,8 @@ auto part_one(Grid grid) {
 auto part_two() { return 2; }
 
 int main() {
-  // std::filesystem::path input_path{"../../data/2024/input_15_mock2.txt"};
-  std::filesystem::path input_path{"../../data/2024/input_15.txt"};
+  std::filesystem::path input_path{"../../data/2024/input_15_mock2.txt"};
+  // std::filesystem::path input_path{"../../data/2024/input_15.txt"};
   utils::Reader reader(input_path);
   Grid grid(reader.get_lines());
 
