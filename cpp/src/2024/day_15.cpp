3fda37ae75cee534f385fd0d60d2f31409e83d8d
Stefan Mesken <kappadistributive@gmail.com>
2025-03-16 20:15:18 +0100

    [2024_15][cpp] implement movement

--- cpp/src/2024/day_15.cpp
+++ cpp/src/2024/day_15.cpp
@@ -7,8 +7,8 @@ using Direction = utils::geometry::Direction;
 class Grid {
 private:
   Point m_robot;
-  std::set<Point> m_walls;
-  std::set<Point> m_boxes;
+  std::vector<Point> m_walls;
+  std::vector<Point> m_boxes;
   std::vector<Direction> m_instructions;
   size_t m_instruction_index;
 
@@ -24,35 +24,71 @@ public:
       }
       for (int x{0}; x < static_cast<int>(line.size()); ++x) {
         if (line[x] == '#') {
-          this->m_walls.insert(Point{{x, index}});
+          this->m_walls.push_back(Point{{x, index}});
         } else if (line[x] == 'O') {
-          this->m_boxes.insert(Point{{x, index}});
+          this->m_boxes.push_back(Point{{x, index}});
         } else if (line[x] == '@') {
           this->m_robot = Point{{x, index}};
         }
       }
     }
     // parse instructions
-    assert(index + 1 == static_cast<int>(input.size()));
-    for (const auto direction : input[index]) {
-      switch (direction) {
-      case '^':
-        this->m_instructions.push_back(Direction::Up);
-        break;
-      case '>':
-        this->m_instructions.push_back(Direction::Right);
-        break;
-      case 'v':
-        this->m_instructions.push_back(Direction::Down);
-        break;
-      case '<':
-        this->m_instructions.push_back(Direction::Down);
-        break;
-      default:
-        throw std::runtime_error(
-            std::format("Illegal direction: {}", direction));
+    for (; index < static_cast<int>(input.size()); ++index) {
+      for (const auto direction : input[index]) {
+        switch (direction) {
+        case '^':
+          this->m_instructions.push_back(Direction::Up);
+          break;
+        case '>':
+          this->m_instructions.push_back(Direction::Right);
+          break;
+        case 'v':
+          this->m_instructions.push_back(Direction::Down);
+          break;
+        case '<':
+          this->m_instructions.push_back(Direction::Left);
+          break;
+        default:
+          throw std::runtime_error(
+              std::format("Illegal direction: {}", direction));
+        }
       }
     }
+  }
+
+  bool step() {
+    if (this->m_instruction_index >= this->m_instructions.size()) {
+      return false;
+    }
+    auto instruction = this->m_instructions[this->m_instruction_index];
+
+    auto new_pos = this->m_robot + instruction;
+    if (std::find(this->m_walls.cbegin(), this->m_walls.cend(), new_pos) !=
+        this->m_walls.cend()) {
+      // robot attempts to move into wall
+    } else if (std::find(this->m_boxes.cbegin(), this->m_boxes.cend(),
+                         new_pos) != this->m_boxes.cend()) {
+      // robot is moving into box
+      auto trail = new_pos;
+      while (std::find(this->m_boxes.cbegin(), this->m_boxes.cend(), trail) !=
+             this->m_boxes.cend()) {
+        trail += instruction;
+      }
+      if (std::find(this->m_walls.cbegin(), this->m_walls.cend(), trail) ==
+          this->m_walls.cend()) {
+        this->m_robot += instruction;
+        do {
+          trail -= instruction;
+          auto it =
+              std::find(this->m_boxes.begin(), this->m_boxes.end(), trail);
+          *it += instruction;
+        } while (trail != new_pos);
+      }
+    } else {
+      this->m_robot = new_pos;
+    }
+    ++this->m_instruction_index;
+    return true;
   }
 
   friend std::ostream &operator<<(std::ostream &os, const Grid &grid) {
@@ -69,9 +105,11 @@ public:
           os << "\033[36m";
           os << '@';
           os << "\033[39m";
-        } else if (grid.m_walls.count(pos)) {
+        } else if (std::find(grid.m_walls.cbegin(), grid.m_walls.cend(), pos) !=
+                   grid.m_walls.cend()) {
           os << '#';
-        } else if (grid.m_boxes.count(pos)) {
+        } else if (std::find(grid.m_boxes.cbegin(), grid.m_boxes.cend(), pos) !=
+                   grid.m_boxes.cend()) {
           os << 'O';
         } else {
           os << ' ';
@@ -130,11 +168,13 @@ auto part_one() { return 1; }
 auto part_two() { return 2; }
 
 int main() {
-  std::filesystem::path input_path{"../../data/2024/input_15_mock.txt"};
+  std::filesystem::path input_path{"../../data/2024/input_15_mock2.txt"};
   // std::filesystem::path input_path{"../../data/2024/input_15.txt"};
   utils::Reader reader(input_path);
   Grid grid(reader.get_lines());
-  std::cout << grid << std::endl;
+  do {
+    std::cout << grid << std::endl;
+  } while (grid.step());
 
   std::cout << std::format("The answer to part one is: {}", part_one())
             << std::endl;
