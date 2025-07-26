a1bd1440ecfdd965c45e4ecc5522e9854e35bc0d
Stefan Mesken <kappadistributive@gmail.com>
2025-07-26 13:05:51 +0200

    [arm32] create tests for my_strlen

--- arm32/tests/text.s
+++ arm32/tests/text.s
@@ -2,6 +2,17 @@
 msg_success: .asciz "Test `%s`: ✓\n"
 msg_failure: .asciz "Test `%s`: ✘\n"
 
+msg_bla: .asciz "bla"
+
+name_my_strlen_empty: .asciz "my_strlen_empty"
+data_my_strlen_empty: .asciz ""
+
+name_my_strlen_simple: .asciz "my_strlen_simple"
+data_my_strlen_simple: .asciz "ab cd"
+
+name_my_strlen_simple2: .asciz "my_strlen_simple2"
+data_my_strlen_simple2: .asciz "ab cd\n"
+
 name_my_parse_next_number_empty: .asciz "my_parse_next_number_empty"
 data_my_parse_next_number_empty: .asciz ""
 
@@ -20,6 +31,28 @@ data_my_parse_next_number_simple4: .asciz "123:456"
 .balign 8
 .text
 .global main
+
+test_my_strlen:
+    push {r4-r8,  lr }
+    mov r5, r0
+    mov r6, r1
+    mov r7, r2
+
+    mov r0, r1
+    bl my_strlen
+
+    cmp r0, r7
+    movne r4, #1
+    ldr r0, =msg_success
+    ldrne r0, =msg_failure
+
+    mov r1, r5
+    bl printf
+    
+    mov r0, r4
+    pop {r4-r8, lr}
+    bx lr
+
 
 test_my_parse_next_number:
     push {r4-r8, lr}
@@ -54,7 +87,25 @@ test_my_parse_next_number:
 
 main:
     push {r4, lr}
-    mov r4, #0
+    mov r4, #0 // r4 will hold the error code of all tests
+
+    ldr r0, =name_my_strlen_empty
+    ldr r1, =data_my_strlen_empty
+    mov r2, #0
+    bl test_my_strlen
+    orr r4, r0
+
+    ldr r0, =name_my_strlen_simple
+    ldr r1, =data_my_strlen_simple
+    mov r2, #5
+    bl test_my_strlen
+    orr r4, r0
+
+    ldr r0, =name_my_strlen_simple2
+    ldr r1, =data_my_strlen_simple2
+    mov r2, #6
+    bl test_my_strlen
+    orr r4, r0
 
     ldr r0, =name_my_parse_next_number_empty 
     ldr r1, =data_my_parse_next_number_empty
