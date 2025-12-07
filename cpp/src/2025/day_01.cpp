c78e076e10e4e5f5c514e82d2feeb6e7611baecb
Stefan Mesken <kappadistributive@gmail.com>
2025-12-07 20:36:31 +0100

    [2025_01][cpp] solve part two

--- cpp/src/2025/day_01.cpp
+++ cpp/src/2025/day_01.cpp
@@ -2,20 +2,31 @@
 
 auto part_one(const std::vector<std::string> &data) {
   int result{0}, position{50};
-  std::cout << position << std::endl;
   for (const auto &line : data) {
     int rotation = line[0] == 'R' ? 1 : -1;
     position += rotation * std::stoi(line.substr(1));
     position += 100000;
     position = position % 100;
-    std::cout << position << std::endl;
     if (position == 0)
       ++result;
   }
   return result;
 }
 
-auto part_two() { return 2; }
+auto part_two(const std::vector<std::string> &data) {
+  int result{0}, position{50};
+  for (const auto &line : data) {
+    int rotation = line[0] == 'R' ? 1 : -1;
+    for (int click{0}; click < std::stoi(line.substr(1)); ++click) {
+      position += rotation;
+      position += 100000;
+      position = position % 100;
+      if (position == 0)
+        ++result;
+    }
+  }
+  return result;
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2025/input_01_mock.txt"};
@@ -24,7 +35,7 @@ int main() {
   auto data = reader.get_lines();
   std::cout << std::format("The answer to part one is: {}", part_one(data))
             << std::endl;
-  std::cout << std::format("The answer to part two is: {}", part_two())
+  std::cout << std::format("The answer to part two is: {}", part_two(data))
             << std::endl;
 
   return EXIT_SUCCESS;
