7d27be2d0f44aee8c43b03f8c32d381fb9939b82
Stefan Mesken <kappadistributive@gmail.com>
2025-12-07 20:33:43 +0100

    [2025_01][cpp] solve part one

---
+++ cpp/src/2025/day_01.cpp
@@ -0,0 +1,31 @@
+#include "../utils/input.hpp"
+
+auto part_one(const std::vector<std::string> &data) {
+  int result{0}, position{50};
+  std::cout << position << std::endl;
+  for (const auto &line : data) {
+    int rotation = line[0] == 'R' ? 1 : -1;
+    position += rotation * std::stoi(line.substr(1));
+    position += 100000;
+    position = position % 100;
+    std::cout << position << std::endl;
+    if (position == 0)
+      ++result;
+  }
+  return result;
+}
+
+auto part_two() { return 2; }
+
+int main() {
+  // std::filesystem::path input_path{"../../data/2025/input_01_mock.txt"};
+  std::filesystem::path input_path{"../../data/2025/input_01.txt"};
+  utils::Reader reader(input_path);
+  auto data = reader.get_lines();
+  std::cout << std::format("The answer to part one is: {}", part_one(data))
+            << std::endl;
+  std::cout << std::format("The answer to part two is: {}", part_two())
+            << std::endl;
+
+  return EXIT_SUCCESS;
+}
