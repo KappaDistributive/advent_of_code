fc39e9e1eb74cf767afcf1b4836a922860abb45e
Stefan Mesken <kappadistributive@gmail.com>
2025-12-25 18:48:36 +0100

    [2025_05][cpp] solve part two

--- cpp/src/2025/day_05.cpp
+++ cpp/src/2025/day_05.cpp
@@ -36,7 +36,23 @@ auto part_one(const std::vector<std::pair<int64_t, int64_t>> &ranges,
   return result;
 }
 
-auto part_two() { return 2; }
+auto part_two(std::vector<std::pair<int64_t, int64_t>> ranges) {
+  // Merge overlapping ranges
+  std::vector<std::pair<int64_t, int64_t>> merged;
+  std::sort(ranges.begin(), ranges.end());
+  for (const auto &[low, high] : ranges) {
+    if (merged.empty() || merged.back().second < low) {
+      merged.emplace_back(low, high);
+    } else {
+      merged.back().second = std::max(merged.back().second, high);
+    }
+  }
+  int64_t result{0};
+  for (const auto &[low, high] : merged) {
+    result += high - low + 1;
+  }
+  return result;
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2025/input_05_mock.txt"};
@@ -46,7 +62,7 @@ int main() {
   std::cout << std::format("The answer to part one is: {}",
                            part_one(ranges, ids))
             << std::endl;
-  std::cout << std::format("The answer to part two is: {}", part_two())
+  std::cout << std::format("The answer to part two is: {}", part_two(ranges))
             << std::endl;
 
   return EXIT_SUCCESS;
