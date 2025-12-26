998c0159a94eb44667a615b71e801f5f15eba8e6
Stefan Mesken <kappadistributive@gmail.com>
2025-12-26 15:07:26 +0100

    [2025_06][cpp] solve part two

--- cpp/src/2025/day_06.cpp
+++ cpp/src/2025/day_06.cpp
@@ -4,7 +4,6 @@ std::vector<std::vector<int64_t>>
 parse_input(const std::vector<std::string> &data) {
   std::vector<std::vector<int64_t>> parsed_data;
   for (const auto &line : data) {
-    std::cout << std::format("Parsing line: {}", line) << std::endl;
     std::vector<int64_t> row;
     int64_t curr{0};
     int i{0};
@@ -56,13 +55,53 @@ auto part_one(const std::vector<std::vector<int64_t>> &data) {
   return result;
 }
 
-auto part_two() { return 2; }
+auto part_two(std::vector<std::string> &data) {
+  int64_t result{0};
+  std::vector<int64_t> numbers;
+  for (int col{static_cast<int>(data[0].size()) - 1}; col >= 0; --col) {
+    int64_t tmp{0};
+    for (int row{0}; row < static_cast<int>(data.size()); ++row) {
+      char ch = data[row][col];
+      if (ch == ' ') {
+        if (tmp != 0) {
+          numbers.push_back(tmp);
+        }
+        tmp = 0;
+      } else if (ch == '+') {
+        numbers.push_back(tmp);
+        tmp = std::transform_reduce(numbers.rbegin(), numbers.rend(), 0LL,
+                                    std::plus<>(), [](int64_t n) { return n; });
+        numbers.clear();
+        result += tmp;
+        tmp = 0;
+        continue;
+      } else if (ch == '*') {
+        numbers.push_back(tmp);
+        tmp = std::transform_reduce(numbers.rbegin(), numbers.rend(), 1LL,
+                                    std::multiplies<>(),
+                                    [](int64_t n) { return n == 0 ? 1 : n; });
+        numbers.clear();
+        result += tmp;
+        tmp = 0;
+        continue;
+      } else {
+        tmp = tmp * 10 + (ch - '0');
+      }
+    }
+  }
+  return result;
+}
 
 int main() {
   // std::filesystem::path input_path{"../../data/2025/input_06_mock.txt"};
   std::filesystem::path input_path{"../../data/2025/input_06.txt"};
   utils::Reader reader(input_path);
-  auto data = parse_input(reader.get_lines());
+  auto data = reader.get_lines();
+  std::cout << std::format("The answer to part one is: {}",
+                           part_one(parse_input(data)))
+            << std::endl;
+  std::cout << std::format("The answer to part two is: {}", part_two(data))
+            << std::endl;
 
   return EXIT_SUCCESS;
 }
