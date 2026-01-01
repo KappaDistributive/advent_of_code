5937f14b4f0448e1f89fe7ab9c1497f63065cf6b
Stefan Mesken <kappadistributive@gmail.com>
2026-01-01 21:29:57 +0100

    [2023_16][py] solve part one

---
+++ python/2023/day_16.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+
+def step(position: tuple[int, int, str], data: list[str]) -> list[tuple[int, int, str]]:
+    (x, y, direction) = position
+    nx, ny = x, y
+    result = []
+    match direction:
+        case ">":
+            nx += 1
+        case "<":
+            nx -= 1
+        case "^":
+            ny -= 1
+        case "v":
+            ny += 1
+    if not (0 <= nx < len(data[0]) and 0 <= ny < len(data)):
+        return []
+    match data[ny][nx]:
+        case ".":
+            result = [(nx, ny, direction)]
+        case "/":
+            match direction:
+                case ">":
+                    direction = "^"
+                case "<":
+                    direction = "v"
+                case "^":
+                    direction = ">"
+                case "v":
+                    direction = "<"
+            result = [(nx, ny, direction)]
+        case "\\":
+            match direction:
+                case ">":
+                    direction = "v"
+                case "<":
+                    direction = "^"
+                case "^":
+                    direction = "<"
+                case "v":
+                    direction = ">"
+            result = [(nx, ny, direction)]
+        case "-":
+            if (direction == ">" or direction == "<"):
+                result =[(nx, ny, direction)]
+            result = [(nx, ny, "<"), (nx, ny, ">")]
+        case "|":
+            if (direction == "^" or direction == "v"):
+                result = [(nx, ny, direction)]
+            result = [(nx, ny, "^"), (nx, ny, "v")]
+    return result
+
+
+def display(positions: list[tuple[int, int, str]], data: list[str]) -> None:
+    for y in range(len(data)):
+        row = ""
+        for x in range(len(data[0])):
+            found = False
+            for pos in positions:
+                if pos[0] == x and pos[1] == y:
+                    row += pos[2]
+                    found = True
+                    break
+            if not found:
+                row += data[y][x]
+        print(row)
+
+
+def part_one(data: list[str]) -> int:
+    visited = set()
+    active_positions = [(-1, 0, ">")]
+    n = 0
+    while active_positions:
+        new_positons = []
+        for pos in active_positions:
+            new_pos = step(pos, data)
+            # print(f"{pos} -> {new_pos}")
+            new_positons.extend([x for x in new_pos if x not in visited])
+            visited.update(new_pos)
+        active_positions = new_positons
+        if n == len(visited):
+            break
+        n = len(visited)
+    return len({pos[:2] for pos in visited})
+
+
+if __name__ == "__main__":
+    path = Path(__file__).parent.parent.parent / "data/2023/input_16.txt"
+    with open(path, "r") as f:
+        data = [line.strip() for line in f.readlines()]
+    print(f"Part One: {part_one(data)}")
