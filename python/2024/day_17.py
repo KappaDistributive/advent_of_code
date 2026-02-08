0aa8b276bfed5c45fd57376624dd0c8ea987b582
Stefan Mesken <kappadistributive@gmail.com>
2026-02-08 21:42:49 +0100

    [2024_17][py] solve part two

--- python/2024/day_17.py
+++ python/2024/day_17.py
@@ -9,6 +9,7 @@ class Computer:
         self.c: int = 0
         self.ip: int = 0
         self.program: list[int] = []
+        self.output: list[int] = []
 
         for line in data:
             if line.startswith("Register A:"):
@@ -37,7 +38,7 @@ class Computer:
 
     def step(self) -> bool:
         if self.ip >= len(self.program):
-            print("")
+            # print(",".join(map(str, self.output)))
             return True
         opcode = self.program[self.ip]
         match opcode:
@@ -59,7 +60,7 @@ class Computer:
                 self.b = self.c ^ self.b
                 self.ip += 2
             case 5:  # out
-                print(self.combo(self.program[self.ip + 1]) % 8, end=",")
+                self.output.append(self.combo(self.program[self.ip + 1]) % 8)
                 self.ip += 2
             case 6:  # bdv
                 self.b = self.a // 2 ** self.combo(self.program[self.ip + 1])
@@ -70,10 +71,43 @@ class Computer:
         return False
 
 
+def run(data: list[str], a: int) -> list[int]:
+    computer = Computer(data)
+    computer.a = a
+    while not computer.step():
+        pass
+    return computer.output
+
+
+def part_one(data: list[str]) -> str:
+    computer = Computer(data)
+    while not computer.step():
+        pass
+    return ",".join(map(str, computer.output))
+
+
+def part_two(data: list[str]) -> int:
+    """
+    By decoding the program we see that for every iteration in-between outputs, a is divided by 8.
+    So we can find a value of a that produces only the last entry of the wanted program. Then we multiply that value by 8 and add [0-7] that produces the two last entries of the wanted program. We repeat this process until we have the full wanted program.
+    """
+    a = 0
+    want = Computer(data).program
+    matching = 1
+    while matching < len(want):
+        while run(data, a) != want[-matching:]:
+            a += 1
+        matching += 1
+        a = a * 8
+    if run(data, a) != want:
+        while run(data, a) != want:
+            a += 1
+    return a
+
+
 if __name__ == "__main__":
     path = Path(__file__).parent.parent.parent / "data/2024/input_17.txt"
     with open(path, "r") as f:
         data = [lines.strip() for lines in f.readlines()]
-    computer = Computer(data)
-    while not computer.step():
-        pass
+    print(f"Part one: {part_one(data)}")
+    print(f"Part two: {part_two(data)}")
